caacf1d2a272e7fdf838910c62e07499c902e5de
gal <gal@gal-PC.mshome.net>
2013-08-21 14:51:09 +0300

    now looks for folder in parent dirs as well

--- app/models/Poster.scala
+++ app/models/Poster.scala
@@ -1,11 +1,17 @@
 package models
 
-import common.path.Path.richPath
+import common.path.Path._
+import common.path.Directory
+import java.io.File
 /**
  * Handles cover art
  */
 object Poster {
-	def getCoverArt(s: Song) = {
-		s.file.parent.files.filter(_.name.matches("[fF]older.jpg"))(0)
+	private def getCoverArt(dir: Directory): File = {
+		val f = dir.files.find(_.name.matches("[fF]older.jpg"))
+		f.getOrElse(getCoverArt(dir.parent))
+	}
+	def getCoverArt(s: Song): File = {
+		getCoverArt(s.file.parent)
 	}
 }
