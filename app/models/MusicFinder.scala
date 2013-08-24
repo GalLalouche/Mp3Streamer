03b337c0a066b49904ffdcc002956947bcba312e
gal <gal@gal-PC.mshome.net>
2013-08-25 00:29:57 +0300

    changed sorting method to sorted

--- app/models/MusicFinder.scala
+++ app/models/MusicFinder.scala
@@ -8,9 +8,9 @@ trait MusicFinder {
 	val dir: Directory
 	val subDirs: List[String]
 	val extensions: List[String]
-
-	lazy val genreDirs = subDirs.sortBy(x => x).map(x => Directory(dir / x))
-	def getSongs: Seq[String] = {
+	
+	lazy val genreDirs = subDirs.sorted.map(x => Directory(dir / x))
+	def getSongs: IndexedSeq[String] = {
 		(genreDirs.flatMap(_.files) ++ (genreDirs.flatMap(_.dirs).par.flatMap(_.deepFiles)))
 				.filter(x => extensions.contains(x.extension)).map(_.path).toVector
 	}
