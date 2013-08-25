d74f1ea719190b86395a80d29e5c3c05b85b52a9
gal <gal@gal-PC.mshome.net>
2013-08-25 13:05:10 +0300

    added path tests

--- test/dirwatch/DirectoryWatcherTest.scala
+++ test/dirwatch/DirectoryWatcherTest.scala
@@ -2,20 +2,18 @@ package dirwatch
 
 import java.io.File
 import java.util.concurrent.TimeUnit
-
 import scala.concurrent.duration.FiniteDuration
-
 import org.junit.runner.RunWith
 import org.specs2.mock.Mockito
 import org.specs2.runner.JUnitRunner
 import org.specs2.time.Duration
-
 import akka.actor.{ActorDSL, ActorSystem}
 import akka.testkit.{DefaultTimeout, ImplicitSender, TestProbe}
 import akka.util.Timeout._
 import akka.util.Timeout
 import dirwatch.DirectoryWatcher._
 import models.TempDirTest
+import java.nio.file.Paths
 
 @RunWith(classOf[JUnitRunner])
 class DirectoryWatcherTest extends TempDirTest with Mockito {
@@ -70,6 +68,15 @@ class DirectoryWatcherTest extends TempDirTest with Mockito {
 				probe expectMsg DirectoryDeleted(tempDir \ "foo")
 				probe expectMsg DirectoryCreated(tempDir / "bar" /)
 			}
+		}
+	}
+	"Path" >> {
+		"override hashcode and equals" >> new TempDir {
+			val p1 = Paths.get(tempDir.path)
+			val p2 = Paths.get(tempDir.path)
+			p1 should not be p2
+			p1.hashCode === p2.hashCode
+			p1 === p2
 		}
 	}
 }
