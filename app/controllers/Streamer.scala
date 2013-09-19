32532a4209372f3abf5e7d2cf751a0de915ba34c
Gal <Gal@Gal-PC>
2013-09-19 17:42:11 +0300

    Changed path of dBpowerAmp

--- app/controllers/Streamer.scala
+++ app/controllers/Streamer.scala
@@ -14,18 +14,24 @@ import play.api.mvc.Action
 import play.api.mvc.Controller
 
 object Streamer extends Controller with Decoder with DbPowerampCodec {
-	val codecPath = "c:/program files (x86)/Illustrate/dBpoweramp/CoreConverter.exe"
+	val codecPath = "D:/Media/Tools/dBpoweramp/CoreConverter.exe"
 	val outputDir = Directory("D:/media/streamer/musicOutput")
-	
+
 	def download(s: String) = Action {
-		val futureFile = Future { decodeFileIfNeeded(new File(URLDecoder.decode(s, "UTF-8"))) }  // this ensures this call isn't blocking
+		val futureFile = Future { decodeFileIfNeeded(new File(URLDecoder.decode(s, "UTF-8"))) } // this ensures this call isn't blocking
 		Async {
 			futureFile.map { file =>
 				loggers.CompositeLogger.trace("Sending file " + file.getAbsolutePath)
-				Status(200).sendFile(file).withHeaders(("Content-length", file.length.toString), ("Accept-Ranges", "bytes"),
-					("X-Pad", "avoid browser bug"), ("Content-Transfer-Encoding", "binary"), ("Cache-Control", "no-cache"),
+				Status(200).sendFile(file).withHeaders(
+					("Content-length", file.length.toString),
+					("Accept-Ranges", "bytes"),
+					("X-Pad", "avoid browser bug"),
+					("Content-Transfer-Encoding", "binary"),
+					("Cache-Control", "no-cache"),
 					("Content-Disposition", "attachment; filename=" + file.getName().replaceAll(",", "%2c")),
-					("Content-Range", "byte %d/%d".format(file.length, file.length)), ("Content", "audio/mp3"))
+					("Content-Range", "byte %d/%d".format(file.length, file.length)),
+					("Content", "audio/mp3")
+				)
 
 			}
 		}
