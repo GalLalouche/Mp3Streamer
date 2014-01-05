ceeeb0c29a610559bf7337fa1616eb9c3c9f3063
gal_lalouche <glpkmtg@gmail.com>
2014-01-05 17:35:23 +0200

    fixed trailing ,

--- project/Build.scala
+++ project/Build.scala
@@ -17,7 +17,7 @@ object ApplicationBuild extends Build {
 		"org.mockito" % "mockito-all" % "1.9.5" % "test",
 		"com.typesafe.akka" %% "akka-actor" % "[2.0,)",
 		"com.typesafe.akka" %% "akka-testkit" % "[2.0,)" % "test",
-		"org.apache.commons" % "commons-io" % "[1.3.2, )",
+		"org.apache.commons" % "commons-io" % "[1.3.2, )"
 	)
 
 	val main = play.Project(appName, appVersion, appDependencies).settings(
