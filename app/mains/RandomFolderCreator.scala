936f298840215f1e863666e1a9cb8ecbf4f71f4a
gal_lalouche <glpkmtg@gmail.com>
2014-02-09 13:08:21 +0200

    refactoring

--- app/mains/RandomFolderCreator.scala
+++ app/mains/RandomFolderCreator.scala
@@ -1,21 +1,19 @@
 package mains
 
 import java.io.File
-import common.path.Directory
-import common.path.RichFile._
-import models.MusicFinder
-import controllers.MusicLocations
-import models.Song
+
 import scala.util.Random
-import org.apache.commons.io.IOUtils
+import scala.util.control.Breaks.{ break, breakable }
+
 import org.apache.commons.io.FileUtils
 import org.jaudiotagger.audio.AudioFileIO
+import org.jaudiotagger.audio.exceptions.{ CannotWriteException, UnableToRenameFileException }
 import org.jaudiotagger.tag.images.StandardArtwork
-import models.Album
-import models.Poster
-import org.jaudiotagger.audio.exceptions.CannotWriteException
-import scala.util.control.Breaks._
-import org.jaudiotagger.audio.exceptions.UnableToRenameFileException
+
+import common.path.Directory
+import common.path.RichFile.richFile
+import controllers.MusicLocations
+import models.{ MusicFinder, Poster, Song }
 
 /**
   * Selects n random songs and puts them in a folder on D
@@ -37,37 +35,33 @@ object RandomFolderCreator extends App {
 		files.map(_.name).foreach($.appendLine(_))
 		$
 	}
-	
+
 	val random = new Random
 	val n = 100
 	(1 to n)
 		.map(index => (index, songs(random nextInt (songs length))))
-		.foreach {
-			case (index, file) =>
-				try {
-
-					val newFile = new File(outputDir.dir, file.name)
-					FileUtils.copyFile(file, newFile)
-					val x = (AudioFileIO.read(newFile))
-					x.getTag.setField(StandardArtwork.createArtworkFromFile(Poster.getCoverArt(Song(file))))
-					breakable {
-						while (true) {
-							try {
-								x.commit
-								break
-							} catch {
-								case _: CannotWriteException => ()
-								case _: UnableToRenameFileException => ()
-							}
-						}
+		.foreach { case (index, file) =>
+			try {
+				val newFile = new File(outputDir.dir, file.name)
+				FileUtils.copyFile(file, newFile)
+				val x = (AudioFileIO.read(newFile))
+				x.getTag.setField(StandardArtwork.createArtworkFromFile(Poster.getCoverArt(Song(file))))
+				breakable { while (true) {
+					try {
+						x.commit
+						break
+					} catch {
+						case _: CannotWriteException => ()
+						case _: UnableToRenameFileException => ()
 					}
+				} }
 
-					Thread.sleep(100)
-					newFile.renameTo(new File(outputDir.dir, "%02d.%s".format(index, file.extension)))
-					println(s"${100 * index / n}%% done".format())
-				} catch {
-					case e: MatchError => println("Failed @ " + file); e.printStackTrace; throw e
-				}
+				Thread.sleep(100) // why is this here?
+				newFile.renameTo(new File(outputDir.dir, "%02d.%s".format(index, file.extension)))
+				println(s"${100 * index / n}%% done".format())
+			} catch {
+				case e: Exception => println("Failed @ " + file); e.printStackTrace; throw e
+			}
 		}
 	createPlaylistFile
 	println("Done!")
