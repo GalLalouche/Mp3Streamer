5a5ed89aab09b199f1f5de7be120b50c09f5af0c
gal_lalouche <glpkmtg@gmail.com>
2014-03-06 17:21:30 +0200

    fixed prininting (missing s

--- app/mains/FindSongsNotInPlaylist.scala
+++ app/mains/FindSongsNotInPlaylist.scala
@@ -28,13 +28,13 @@ object FindSongsNotInPlaylist extends App with Debug {
 		val realSongs = real.getSongs
 			.map(_.toLowerCase.replaceAll("\\\\", "/"))
 			.toSet
-		println("actual songs |${realSongs.size}|")
+		println(s"actual songs |${realSongs.size}|")
 
 		val playlistMissing = realSongs.diff(playlistSongs).toList.sorted
 		val serverMissing = playlistSongs.diff(realSongs).toList.sorted
-		println("Server is missing ${serverMissing.size} songs")
+		println(s"Server is missing ${serverMissing.size} songs")
 		println(serverMissing.mkString("\n"))
-		println("Playlist is missing ${playlistMissing.size} songs")
+		println(s"Playlist is missing ${playlistMissing.size} songs")
 		println(playlistMissing.mkString("\n"))
 	}
 }
