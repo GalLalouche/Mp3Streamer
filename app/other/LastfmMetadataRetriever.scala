61113f9a53af1455b3768aa9c845bebee076950a
gal_lalouche <glpkmtg@gmail.com>
2014-09-13 16:27:40 +0300

    moved some stuff from lastfm reader to parent

--- app/other/LastfmMetadataRetriever.scala
+++ app/other/LastfmMetadataRetriever.scala
@@ -1,32 +1,28 @@
 package other
 
+import java.util.concurrent.TimeoutException
+
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
-import common.RichString.richString
-import models.Album
-import play.api.libs.json.{ JsArray, JsString, JsValue }
-import play.api.libs.ws.WS
-import java.util.concurrent.TimeoutException
 import scala.util.Try
 
+import common.RichString.richString
+import models.Album
+import play.api.libs.json.{ JsArray, JsValue }
+import play.api.libs.ws.WS
+
 class LastfmMetadataRetriever(apiKey: String = "d74d6d3b7e8fbfa6b733d376c4d41e73") extends MetadataRetriever {
-	private implicit def richJson(js: JsValue) = new {
-		def asString: String = js.asInstanceOf[JsString].value
-	}
-	private def jsonToAlbum(js: JsValue): Option[Album] = {
+	override protected def jsonToAlbum(js: JsValue): Option[Album] = {
+		def getAlbumJson(artist: String, album: String) = getJson("album.getinfo", "artist" -> artist, "album" -> album)
 		val artist = js \ "artist" \ "name" asString
 		val albumName = js \ "name" asString
 		val albumJson = getAlbumJson(artist, albumName) \ "album"
 		Try(Album(artist, (albumJson \ "releasedate" asString).trim.captureWith(".*?(\\d{4}).*".r).toInt, albumName)).toOption
 	}
-	override def getAlbums(artist: String): Iterator[Album] =
-		try {
-			getArtistJson(artist) \ "topalbums" \ "album" match {
-				case a: JsArray => a.value.iterator.map(jsonToAlbum).filter(_.isDefined).map(_.get)
-				case _ => throw new MatchError // removes warning
-			}
-		} finally {
-			WS.resetClient // this is needed for the application to die
+	override protected def getArtistJson(artist: String) =
+		getJson("artist.gettopalbums", "artist" -> artist) \ "topalbums" \ "album" match {
+			case a: JsArray => a
+			case _ => throw new AssertionError("received json wasn't a JsArray")
 		}
 
 	private def getJson(method: String, other: (String, String)*): JsValue = {
@@ -45,9 +41,6 @@ class LastfmMetadataRetriever(apiKey: String = "d74d6d3b7e8fbfa6b733d376c4d41e73
 			case e: TimeoutException => println("Failed to retrieve data for " + other.toSeq); throw e
 		}
 	}
-
-	private def getArtistJson(artist: String) = getJson("artist.gettopalbums", "artist" -> artist)
-	private def getAlbumJson(artist: String, album: String) = getJson("album.getinfo", "artist" -> artist, "album" -> album)
 
 }
 
