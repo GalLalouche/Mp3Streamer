b25f3c33dcf3e37191a9667abb9b690e54ea6896
gal_lalouche <glpkmtg@gmail.com>
2014-11-17 08:30:07 +0200

    Now catches throwable

--- app/mains/FolderFixer.scala
+++ app/mains/FolderFixer.scala
@@ -8,18 +8,16 @@ object FolderFixer extends App {
 	try {
 		val folder: String = args(0)
 		val newFolder = FixLabels.fix(folder)
-		try {
+		try
 			DownloadCover.main(List(newFolder).toArray)
-			println("Done!")
-			readLine
-		} catch {
+		catch {
 			case CoverException(text) =>
 				println("Could not auto-download picture :( press any key to open browser")
 				readLine
 				Process("""C:\Users\Gal\AppData\Local\Google\Chrome\Application\chrome.exe "https://www.google.com/search?espv=2&biw=1920&bih=955&tbs=isz%3Aex%2Ciszw%3A500%2Ciszh%3A500&tbm=isch&sa=1&q=lastfm """ + text).!!
 		}
-	} catch { 
-		case e: Exception => 
+	} catch {
+		case e: Throwable =>
 			e.printStackTrace()
 			readLine
 	}
