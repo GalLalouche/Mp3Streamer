783e1c7861cbd2354f805919370062402972f330
gal_lalouche <glpkmtg@gmail.com>
2015-01-27 12:02:52 +0200

    fixed a bug, where sometimes the albums wouldn't be ordered by year (how did this EVER work?)

--- app/other/NewAlbumsRetriever.scala
+++ app/other/NewAlbumsRetriever.scala
@@ -2,18 +2,30 @@ package other
 
 import models.MusicFinder
 import models.Album
+import common.rich.RichAll._
 
 class NewAlbumsRetriever(meta: MetadataRetriever, music: MusicFinder, ignoredArtists: Seq[String]) {
-
+	var lastArtist: Option[String] = None
 	def findNewAlbums: Iterator[Album] = {
 		val lastAlbums = music.getAlbums
 			.toSeq
 			.groupBy(_.artist)
-			.map(e => e._1.toLowerCase -> e._2.map(_.year).last) // take last album
-			.toMap
+			.map(e => e._1.toLowerCase -> e._2.toVector
+				.sortBy(_.year)
+				.map(_.year)
+				.last // finds the last album
+			).toMap
 		lastAlbums.keys.iterator
 			.filterNot(ignoredArtists.contains)
 			.flatMap(meta.getAlbums)
-			.filter(e => lastAlbums(e.artist.toLowerCase) < e.year)
+			.filter(e =>
+				if (lastAlbums(e.artist.toLowerCase) < e.year)
+					true
+				else {
+					for (a <- lastArtist)
+						if (a != e.artist) println("Finished " + a)
+					lastArtist = Some(e.artist)
+					false
+				})
 	}
 }
