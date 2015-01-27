9cb32a3632c890548b15316633eee2c7768b757d
gal_lalouche <glpkmtg@gmail.com>
2015-01-27 11:57:29 +0200

    waits one second between each successful query

--- app/other/MetadataRetriever.scala
+++ app/other/MetadataRetriever.scala
@@ -24,6 +24,10 @@ trait MetadataRetriever {
 			.iterator
 			.map(jsonToAlbum(artist, _))
 			.collect { case Some(a) => a }
-		catch { case e: Exception => println("Could not get data for artist: " + artist); Thread sleep 10000; Iterator.empty }
-		finally WS.resetClient // this is needed for the application to die
+		catch {
+		case e: Exception => println("Could not get data for artist: " + artist); Thread sleep 10000; Iterator.empty }
+		} finally {
+			WS.resetClient // this is needed for the application to die
+			Thread sleep 1000 // doesn't overload the server
+		}
 }
