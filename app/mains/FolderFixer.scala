7a31f92da2ac7827ec8695e55dd942bac50f8b98
Gal PC <glpkmtg@gmail.com>
2015-02-15 14:33:05 +0200

    if the artist folder isnt found, it asks for the artists genre and creates it

--- app/mains/FolderFixer.scala
+++ app/mains/FolderFixer.scala
@@ -17,24 +17,29 @@ import models.Song
 object FolderFixer extends App {
 	private def findArtistFolder(folder: Directory): Option[Directory] = {
 		println("Searching for artist folder")
-		val artist = folder
-			.files
-			.filter(f => Set("mp3", "flac").contains(f.extension))
-			.head
-			.mapTo(Song.apply)
-			.artist
+		val artist = findArtist(folder)
 		Directory("d:/media/music")
 			.deepDirs
 			.find(_.name.toLowerCase == artist.toLowerCase)
 	}
-	private def moveDirectory(location: java.util.concurrent.Future[Option[common.rich.path.Directory]], newPath: String) {
-		if (location.isDone() == false)
+	private def moveDirectory(artist: String,
+		future: java.util.concurrent.Future[Option[common.rich.path.Directory]],
+		newPath: String) {
+		if (future.isDone() == false)
 			println("Waiting on artist find...")
-		for (d <- location.get()) {
-			val newFile = new File(newPath)
-			val moved = Files.move(newFile.toPath, new File(d, newFile.name).toPath)
-			new ProcessBuilder("explorer.exe", d.getAbsolutePath).start
+		val d: Directory = future.get().getOrElse {
+			val genre = readLine("Could not find artist directory... what is the artist's genre?\n").toLowerCase
+			Directory("d:/media/music")
+				.dirs
+				.view
+				.flatMap(_.dirs)
+				.find(_.name.toLowerCase == genre)
+				.get
+				.addSubDir(artist)
 		}
+		val newFile = new File(newPath)
+		val moved = Files.move(newFile.toPath, new File(d, newFile.name).toPath)
+		new ProcessBuilder("explorer.exe", d.getAbsolutePath).start
 	}
 
 	private def downloadCover(newPath: String) {
@@ -43,7 +48,7 @@ object FolderFixer extends App {
 		catch {
 			case CoverException(text) =>
 				println("Could not auto-download picture :( opening browser")
-				Process("""C:\Users\Gal\AppData\Local\Google\Chrome\Application\chrome.exe "https://www.google.com/search?espv=2&biw=1920&bih=955&tbs=isz%3Aex%2Ciszw%3A500%2Ciszh%3A500&tbm=isch&sa=1&q=lastfm """ + text).!!
+				Process("""C:\Users\Gal\AppData\Local\Google\Chrome\Application\chrome.exe "https://www.google.com/search?espv=2&biw=1920&bih=955&tbs=isz%3Aex%2Ciszw%3A500%2Ciszh%3A500&tbm=isch&sa=1&q=png """ + text).!!
 		}
 	}
 	private implicit def richCall[T](f: () => T): Callable[T] = new Callable[T] { override def call: T = f() }
@@ -54,7 +59,7 @@ object FolderFixer extends App {
 		println("fixing directory")
 		val newPath = FixLabels fix folder.cloneDir()
 		downloadCover(newPath)
-		moveDirectory(location, newPath)
+		moveDirectory(findArtist(folder), location, newPath)
 		println("--Done!--")
 	} catch {
 		case e: Throwable =>
@@ -62,5 +67,13 @@ object FolderFixer extends App {
 			readLine
 	} finally
 		executors.shutdownNow
+
+	private def findArtist(folder: common.rich.path.Directory): String =
+		folder
+			.files
+			.filter(Set("mp3", "flac") contains _.extension)
+			.head
+			.mapTo(Song.apply)
+			.artist
 
 }
