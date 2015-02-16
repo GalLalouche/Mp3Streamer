02630a39fbd85be4acc87a92300b8e34ed8ff0da
Gal PC <glpkmtg@gmail.com>
2015-02-17 00:18:55 +0200

    unflacer java main method

--- app/mains/FlacGenerator.scala
+++ app/mains/FlacGenerator.scala
@@ -5,7 +5,11 @@ import decoders.CueTools
 //splits cue file and fixes the flac output
 object FlacGenerator {
 	def main(args: Array[String]) {
-		val splitter = CueTools
-		splitter.split("""D:\Incoming\Bittorrent\Completed\Music\Rotting Christ LOSSLESS\[1994] [CD] Non Serviam [EAC-FLAC]\Rotting Christ - (1994) Non Serviam.flac.cue""")
+		try {
+			CueTools.split(args(0))
+		} catch {
+			case e: Exception => e.printStackTrace()
+		}
+		readLine
 	}
 }
