265ebc1e57fb21f2e7d7603c39d678e4628b9798
Gal PC <glpkmtg@gmail.com>
2016-02-13 15:19:59 +0200

    auto add folder if popup blocker is on

--- public/javascripts/ws/newFolder.js
+++ public/javascripts/ws/newFolder.js
@@ -1,6 +1,7 @@
 $(function() {
 	openConnection("newFolder", function(msg) {
-		if (window.confirm("Found new folder(" + msg.data + "), would you like to add it to the playlist?"))
+		// in case pop blocker is disabled, fuck that and just add the folder
+		if (!window.confirm || window.confirm("Found new folder(" + msg.data + "), would you like to add it to the playlist?"))
 			$.get("/music/albums/" + msg.data, function(data) {
 				playlist.add(data, false);
 			});
