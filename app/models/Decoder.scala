c3e03d516669da50aeb078745f06cc3f01331ce9
gal_lalouche <glpkmtg@gmail.com>
2016-02-19 10:19:23 +0200

    decoder is not long being extended

--- app/models/Decoder.scala
+++ app/models/Decoder.scala
@@ -2,11 +2,9 @@ package models
 
 import java.io.File
 
-import common.rich.path.RichFile._
+import common.rich.path.RichFile.richFile
 import decoders.Mp3Decoder
 
 trait Decoder extends Mp3Decoder {
-	def decodeFileIfNeeded(f: File) = {
-		if (f.extension == "flac") decode(f) else f
-	}
+  def decodeFileIfNeeded(f: File) = if (f.extension.toLowerCase == "flac") decode(f) else f
 }
