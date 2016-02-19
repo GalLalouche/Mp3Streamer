64196efde6e7269918488e327afab97c9faf4edd
gal_lalouche <glpkmtg@gmail.com>
2016-02-19 10:02:11 +0200

    Flac albums are now predecoded.

--- app/controllers/Player.scala
+++ app/controllers/Player.scala
@@ -5,13 +5,13 @@ import java.net.{ URLDecoder, URLEncoder }
 
 import scala.util.Random
 
-import akka.actor.ActorDSL
-import akka.actor.ActorDSL.Act
-import akka.actor.actorRef2Scala
-import common.{ Debug, LazyActor }
+import akka.actor.{ ActorDSL, actorRef2Scala }
+import common.{ DaemonRunner, Debug, LazyActor }
+import common.rich.path.Directory
 import common.rich.path.RichPath.richPath
+import decoders.DbPowerampCodec
 import dirwatch.DirectoryWatcher
-import models.{ AlbumDirectory, MusicFinder, Poster, Song }
+import models._
 import play.api.libs.json.{ JsArray, JsString }
 import play.api.mvc.{ Action, Controller }
 import websockets.{ NewFolderSocket, TreeSocket }
@@ -20,48 +20,51 @@ import websockets.{ NewFolderSocket, TreeSocket }
   * Handles fetch requests of JSON information
   */
 object Player extends Controller with MusicFinder with MusicLocations with Debug {
-	private val random = new Random
-	private var songs: Seq[File] = null
+  private val random = new Random
+  private var songs: Seq[File] = null
 
-	private def songJsonInformation(song: models.Song): play.api.libs.json.JsObject = {
-		song.jsonify + (("mp3", JsString("/music/songs/" + URLEncoder.encode(song.file.path, "UTF-8")))) +
-			(("poster", JsString("/posters/" + Poster.getCoverArt(song).path)))
-	}
+  private def songJsonInformation(song: models.Song): play.api.libs.json.JsObject = {
+    song.jsonify + (("mp3", JsString("/music/songs/" + URLEncoder.encode(song.file.path, "UTF-8")))) +
+      (("poster", JsString("/posters/" + Poster.getCoverArt(song).path)))
+  }
 
-	private implicit val system = models.KillableActors.system
-	import akka.actor.ActorDSL._
-	private val lazyActor = ActorDSL.actor(new LazyActor(1000))
+  private implicit val system = models.KillableActors.system
+  import akka.actor.ActorDSL._
+  private val lazyActor = ActorDSL.actor(new LazyActor(1000))
 
-	private val updatingMusic = () => timed("Updating music") {
-		// this cannot be inlined, as it has to be the same function for LazyActor
-		songs = getSongFilePaths.map(new File(_))
-		TreeSocket.actor ! TreeSocket.Update
-	}
+  private val updatingMusic = () => timed("Updating music") {
+    // this cannot be inlined, as it has to be the same function for LazyActor
+    songs = getSongFilePaths.map(new File(_))
+    TreeSocket.actor ! TreeSocket.Update
+  }
 
-	private val watcher = ActorDSL.actor(new DirectoryWatcher(ActorDSL.actor(new Act {
-		become {
-			case DirectoryWatcher.DirectoryCreated(d) =>
-				lazyActor ! updatingMusic
-				NewFolderSocket.actor ! d
-			case DirectoryWatcher.DirectoryDeleted(_) => lazyActor ! updatingMusic
-		}
-	}), genreDirs))
-	updatingMusic()
+  private val watcher = ActorDSL.actor(new DirectoryWatcher(ActorDSL.actor(new Act {
+    become {
+      case DirectoryWatcher.DirectoryCreated(d) =>
+        lazyActor ! updatingMusic
+        NewFolderSocket.actor ! d
+      case DirectoryWatcher.DirectoryDeleted(_) => lazyActor ! updatingMusic
+    }
+  }), genreDirs))
+  updatingMusic()
 
-	def randomSong = {
-		val song = songs(random nextInt (songs length))
-		Action {
-			try {
-				Ok(songJsonInformation(Song(song)))
-			} catch {
-				case e: Exception =>
-					loggers.CompositeLogger.error("Failed to parse " + song.path, e)
-					InternalServerError
-			}
-		}
-	}
+  def randomSong = {
+    val song = songs(random nextInt (songs length))
+    Action {
+      try {
+        Ok(songJsonInformation(Song(song)))
+      } catch {
+        case e: Exception =>
+          loggers.CompositeLogger.error("Failed to parse " + song.path, e)
+          InternalServerError
+      }
+    }
+  }
 
-	def album(path: String) = Action {
-		Ok(JsArray(AlbumDirectory(new File(URLDecoder.decode(path, "UTF-8"))).songs.map(songJsonInformation)))
-	}
+  def album(path: String) = Action {
+    val songs = AlbumDirectory(new File(URLDecoder.decode(path, "UTF-8"))).songs
+    def decode(f: File) { Streamer.decoder.decodeFileIfNeeded(f) } // returns a unit as required by DaemonRunner
+    songs.map(_.file).map(e => () => decode(e)).foreach(DaemonRunner.run)
+    Ok(JsArray(songs.map(songJsonInformation)))
+  }
 }
