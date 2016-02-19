92dad8c679b447b411b826031384d5ee5b32f22c
gal_lalouche <glpkmtg@gmail.com>
2016-02-19 10:38:51 +0200

    clean old flac files

--- app/decoders/Mp3Encoder.scala
+++ app/decoders/Mp3Encoder.scala
@@ -1,31 +1,35 @@
 package decoders;
 
 import java.io.File
-
 import common.rich.path.Directory
-import common.rich.path.RichFile._
+import common.rich.path.RichFile.{ poorFile, richFile }
+import java.nio.file.Files
+import java.nio.file.attribute.BasicFileAttributes
+import org.joda.time.DateTime
+import common.DaemonRunner
 
-/**
-  * Encodes audio files files to mp3.
-  * Also handles caching
-  *
-  * @author Gal Lalouche
-  */
+/** Encodes audio files files to mp3. Also handles caching */
 trait Mp3Encoder extends Codec {
 	val outputDir: Directory
-
+	
+	private def cleanOldFiles() {
+	  def getCreationTime(f: File) = Files.readAttributes(f.toPath, classOf[BasicFileAttributes]).creationTime().toMillis()
+	  val minimumCreationTime = DateTime.now.minusWeeks(1).getMillis
+	  outputDir.files.filter(getCreationTime(_) < minimumCreationTime).foreach(_.delete)
+	}
 	/**
 	  * Encode the file to an mp3 format
 	  *
 	  * @param originalFile The file to decode
-	  * @return The new mp3 file created; The file will be created in the {@code outputDir}, and will be the absolute
-	  *         path of the file (with no space) with .mp3
+	  * @return The (possibly new) mp3 file created; The file will be created in the {@code outputDir}, and will 
+	  * 				be the absolute path of the file (with no space) with .mp3
 	  * @throws IOException
 	  */
 	def decode(file: File): File = {
 		require(file != null)
 		require(file exists)
 		require(file.isDirectory == false)
+		DaemonRunner run cleanOldFiles
 		val outputFileName = file.path.replaceAll("[\\s\\/\\\\\\-\\:]", "").toLowerCase + ".mp3";
 		outputDir.files.find(_.name == outputFileName).getOrElse({
 			val outputFile = outputDir.addFile(outputFileName)
