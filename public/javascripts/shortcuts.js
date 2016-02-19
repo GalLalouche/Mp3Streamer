38b3b65f0d7537b68f9a454b90dd4d917a598feb
gal_lalouche <glpkmtg@gmail.com>
2016-02-19 18:15:09 +0200

    Shortcuts no longer triggers in input boxes
    This is a prequisite to using the search box.

--- public/javascripts/shortcuts.js
+++ public/javascripts/shortcuts.js
@@ -1,51 +1,43 @@
 $(function() {
-	jPlayer.togglePlay = function() {
-		var jPlayer = $('#jquery_jplayer_1').data().jPlayer;
-		if (jPlayer.status.paused)
-			click("play");
-		else
-			click("pause");
-	}
-	$(document).keyup(function(e) {
-		var jPlayer = $('#jquery_jplayer_1').data().jPlayer;
-		var code = e.keyCode || e.which;
-		var letter = String.fromCharCode(e.which);
-		switch (letter) {
-		case 'Z':
-			if (playlist.current) {
-				click("previous");
-			} else {
-				click("stop");
-				click("play");
-			}
-			break;
-		case 'X':
-			if (jPlayer.status.paused) {
-				click("play");
-			} else {
-				click("stop");
-				click("play");
-			}
-			break;
-		case 32: // Space key
-		case 'K': // fucking youtube :\
-		case 'C':
-			if (jPlayer.status.paused) {
-				click("play");
-			} else {
-				click("pause");
-			}
-			break;
-		case 'V':
-			click("stop");
-			break;
-		case 'B':
-			click("next");
-			break;
-		}
-	});
+  const click = what => $(".jp-" + what).click()
+  const isFromInput = tag => tag == 'input' || tag == 'textarea'
 
-	function click(what) {
-		$(".jp-" + what).click();
-	}
+  $(document).keyup(function(e) {
+    if (isFromInput(e.target.tagName.toLowerCase()))
+      return
+    const jPlayer = $('#jquery_jplayer_1').data().jPlayer;
+    const letter = String.fromCharCode(e.which);
+    switch (letter) {
+    case 'Z':
+      if (playlist.current > 0) // if not first song, go to previous song
+        click("previous");
+      else { // otherwise, restart song
+        click("stop");
+        click("play");
+      }
+      break;
+    case 'X': // restart song if playing, otherwise unpause
+      if (jPlayer.status.paused) {
+        click("play");
+      } else {
+        click("stop");
+        click("play");
+      }
+      break;
+    case 32: // Space key
+    case 'K': // fucking youtube :\
+    case 'C':
+      if (jPlayer.status.paused)
+        click("play");
+      else
+        click("pause");
+      break;
+    case 'V':
+      click("stop");
+      break;
+    case 'B':
+      click("next");
+      break;
+    }
+  });
 });
