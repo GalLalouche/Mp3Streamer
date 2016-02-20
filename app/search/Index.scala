7264432a83627ec9272f10b2abe9c441947b10dd
gal_lalouche <glpkmtg@gmail.com>
2016-02-20 15:16:42 +0200

    some functional refactoring

--- app/search/Index.scala
+++ app/search/Index.scala
@@ -4,29 +4,18 @@ import models.Song
 import scala.annotation.tailrec
 
 class Index private (map: Map[String, Seq[Song]]) {
-  private def sort(songs: Seq[Song]): Seq[Song] = {
-    @tailrec
-    def aux(s1: Song, s2: Song, fs: List[Song => String]): Boolean = {
-      if (fs.isEmpty) return true
-      val comparison = fs.head(s1).compareTo(fs.head(s2))
-      if (comparison != 0) comparison < 0 else aux(s1, s2, fs.tail)
-    }
-    songs.sortWith(aux(_, _, List(_.artist, _.year.toString, _.album, _.track.toString)))
-  }
+  private val sortingPriority = List[Function[Song, String]](_.artist, _.year.toString, _.album, _.track.toString)
+  private def sort(songs: Seq[Song]): Seq[Song] =
+    songs.sortWith((s1, s2) => sortingPriority.map(f => f(s1).compareTo(f(s2))).find(_ != 0).getOrElse(1) > 0)
   def find(s: String) = map.get(s.toLowerCase).getOrElse(Nil)
   def findIntersection(ss: TraversableOnce[String]): Seq[Song] = {
     def findSet(s: String) = find(s).toSet
-    def tail(terms: List[String], result: Set[Song]): Seq[Song] = terms match {
-      case Nil       => result.toSeq
-      case (x :: xs) => tail(xs, result.intersect(findSet(x)))
-    }
     val list = ss.toList
-    sort(tail(list.tail, findSet(list.head)).take(10))
+    val intersection = list.tail.foldLeft(findSet(list.head))((agg, term) => agg.intersect(findSet(term)))
+    sort(intersection.toSeq)
   }
 }
 
 object Index {
-  def apply(map: Map[String, Seq[Song]]): Index = {
-    new Index(map)
-  }
+  def apply(map: Map[String, Seq[Song]]): Index = new Index(map)
 }
