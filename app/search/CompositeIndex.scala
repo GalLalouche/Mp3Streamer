b6ba5932e176f11a86f7fc7261044458b356d102
gal_lalouche <glpkmtg@gmail.com>
2016-02-26 17:46:02 +0200

    I need HLists :(

--- app/search/CompositeIndex.scala
+++ app/search/CompositeIndex.scala
@@ -1,19 +1,20 @@
 package search
 
-import models.Song
-import models.Artist
-import models.Album
+import models.{ Album, Artist, Song }
 
 class CompositeIndex private (indexBuilder: IndexBuilder) {
   private def buildIndexFromCache[T: Jsonable: Indexable](implicit m: Manifest[T]) =
     indexBuilder.buildIndexFor(MetadataCacher.load[T])
-  def find[T](terms: Seq[String], index: Index[T]): Seq[T] =
-    index findIntersection terms take 10
+  private def find(terms: Seq[String]) = new { // currying because Scala isn't functional enough :(
+    def apply[T](index: Index[T]): Seq[T] = index findIntersection terms take 10
+  }
   private val songIndex = buildIndexFromCache[Song]
   private val albumIndex = buildIndexFromCache[Album]
   private val artistIndex = buildIndexFromCache[Artist]
-  def search(terms: Seq[String]) =
-    (find(terms, songIndex), find(terms, albumIndex), find(terms, artistIndex))
+  def search(terms: Seq[String]) = {
+    val finder = find(terms)
+    (finder(songIndex), finder(albumIndex), finder(artistIndex))
+  }
 }
 
 object CompositeIndex {
