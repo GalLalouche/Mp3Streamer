33d42061bcf497245d29dad411853fa00700d3dc
gal_lalouche <glpkmtg@gmail.com>
2016-03-24 22:50:38 +0200

    console socket should no longer fail if no subscribers

--- app/websockets/ConsoleSocket.scala
+++ app/websockets/ConsoleSocket.scala
@@ -5,9 +5,10 @@ import loggers.Logger
 
 /** Sends console messages to the listeners */
 object ConsoleSocket extends WebSocketController with Logger {
-	override def trace(s: String) = out._2.push("[Trace] " + s)
-	override def debug(s: String) = out._2.push("[Debug] " + s)
-	override def info(s: String) = out._2.push("[Info] " + s)
-	override def warn(s: String, e: Exception = null) = out._2.push("[Warn] " + s + " exception: " + e.getMessage)
-	override def error(s: String, e: Exception) = out._2.push("[Error] " + s + " exception: " + e.getMessage)
+  private def pushOpt(s: String) = Option(out).map(_._2).foreach(_ push s)
+  override def trace(s: String) = pushOpt("[Trace] " + s)
+  override def debug(s: String) = pushOpt("[Debug] " + s)
+  override def info(s: String) = pushOpt("[Info] " + s)
+  override def warn(s: String, e: Exception = null) = pushOpt("[Warn] " + s + " exception: " + e.getMessage)
+  override def error(s: String, e: Exception) = pushOpt("[Error] " + s + " exception: " + e.getMessage)
 }
