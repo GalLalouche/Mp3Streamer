f6586d759b99ae7b9556207b391639c8b15b924f
gal_lalouche <glpkmtg@gmail.com>
2016-03-24 23:21:06 +0200

    Cover downloader no longer moves images around
    ugly ass hack

--- app/mains/cover/AsyncFolderImagePanel.scala
+++ app/mains/cover/AsyncFolderImagePanel.scala
@@ -4,7 +4,7 @@ import java.util.concurrent.BlockingQueue
 
 import scala.concurrent.Lock
 import scala.swing.event.MouseClicked
-import scala.swing.{Button, GridPanel, Label}
+import scala.swing.{Button, GridPanel, Label, TextArea}
 
 /** MUTANT TEENAGE NINJA TURTLES :D */
 private class AsyncFolderImagePanel(rows: Int, cols: Int, imageProvider: BlockingQueue[FolderImage])
@@ -12,6 +12,7 @@ private class AsyncFolderImagePanel(rows: Int, cols: Int, imageProvider: Blockin
   private val thread = new DelayedThread("Image placer")
   private val waitForNextClick = new Lock
   waitForNextClick.available = false
+  private var realSize = 0
   def start() {
     def createImagePanel(image: FolderImage) =
       new Label {
@@ -22,20 +23,27 @@ private class AsyncFolderImagePanel(rows: Int, cols: Int, imageProvider: Blockin
         }
       }
     thread.start(() => {
-      if (contents.isEmpty)
-        contents += Button.apply("Fuck it, I'll do it myself!") {
-          AsyncFolderImagePanel.this.publish(OpenBrowser)
-          thread.close()
+        if (contents.isEmpty) {
+          realSize = 0
+          for (i <- 0 until rows * cols)
+          contents += new TextArea("Placeholder for image #" + i)
+          contents += Button.apply("Fuck it, I'll do it myself!") {
+            AsyncFolderImagePanel.this.publish(OpenBrowser)
+            thread.close()
+          }
+          contents += Button.apply("Show me more")(waitForNextClick.release())
         }
-      else if (contents.size < rows * cols)
-        contents += createImagePanel(imageProvider.take())
-      else {
-        // if done, add two buttons
-        contents += Button.apply("Show me more")(waitForNextClick.release())
-        waitForNextClick.acquire()
-        contents.clear()
+      else if (realSize < rows * cols) {
+          contents.update(realSize, createImagePanel(imageProvider.take()))
+          realSize += 1
+        }
+        else {
+          // if done, wait for release
+          waitForNextClick.acquire()
+          contents.clear()
+        }
       }
-    })
+    )
   }
 
   override def close() = {
