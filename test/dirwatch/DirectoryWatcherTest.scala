88608a56503647a982325ee8afa749bb7973675a
gal_lalouche <glpkmtg@gmail.com>
2016-04-01 20:21:45 +0300

    Unignored DirectoryWatcher, and updated tests for observable

--- test/dirwatch/DirectoryWatcherTest.scala
+++ test/dirwatch/DirectoryWatcherTest.scala
@@ -1,82 +1,71 @@
-//package dirwatch
-//
-//import java.io.File
-//import java.util.concurrent.TimeUnit
-//import scala.concurrent.duration.FiniteDuration
-//import org.junit.runner.RunWith
-//import org.specs2.mock.Mockito
-//import org.specs2.runner.JUnitRunner
-//import org.specs2.time.Duration
-//import akka.actor.{ActorDSL, ActorSystem}
-//import akka.testkit.{DefaultTimeout, ImplicitSender, TestProbe}
-//import akka.util.Timeout._
-//import akka.util.Timeout
-//import dirwatch.DirectoryWatcher._
-//import models.TempDirTest
-//import java.nio.file.Paths
-//
-//@RunWith(classOf[JUnitRunner])
-//class DirectoryWatcherTest extends TempDirTest with Mockito {
-//	// FFS
-//	implicit def durationToFinishDuration(d: Duration) = FiniteDuration(d.inMillis, TimeUnit.MILLISECONDS)
-//	implicit val x = ActorSystem("test")
-//	class WatchedDir extends TempDir {
-//		val folderAdded = mock[() => Unit]
-//		val probe = new TestProbe(x) with DefaultTimeout {
-//			val x = 10 seconds
-//			override val timeout: Timeout = x.inMilliseconds
-//		}
-//		val ref = ActorDSL.actor(new DirectoryWatcher(probe.ref, tempDir))
-//		probe expectMsg Started
-//	}
-//
-//	"DirectoryWatcher" >> {
-//		"handle new creations" >> {
-//			"new file created" >> new WatchedDir {
-//				tempDir.addFile("foo.bar")
-//				probe expectMsg OtherChange
-//			}
-//			"new folder created" >> new WatchedDir {
-//				tempDir.addSubDir("foo")
-//				probe expectMsg DirectoryCreated(tempDir / "foo" /)
-//			}
-//			"new folder created in recursive" >> new WatchedDir {
-//				val foo = tempDir.addSubDir("foo")
-//				probe expectMsg DirectoryCreated(tempDir / "foo" /)
-//				foo.addSubDir("bar")
-//				probe expectMsg DirectoryCreated(tempDir / "foo" / "bar" /)
-//			}
-//		}
-//		"handle deletes" >> {
-//			"file deleted" >> new WatchedDir {
-//				val file = tempDir.addFile("foo.bar")
-//				file.delete
-//				probe expectMsg (OtherChange)
-//			}
-//			"directory deleted" >> new WatchedDir {
-//				val d = tempDir.addSubDir("foo")
-//				probe expectMsg DirectoryCreated(tempDir / "foo" /) // waits for change
-//				d.deleteAll
-//				probe expectMsg DirectoryDeleted(tempDir \ "foo")
-//			}
-//		}
-//		"handle renames" >> {
-//			"directory renamed" >> new WatchedDir {
-//				val d = tempDir.addSubDir("foo")
-//				probe expectMsg DirectoryCreated(tempDir / "foo" /) // waits for change
-//				d.dir.renameTo(new File(d.parent, "bar"))
-//				probe expectMsg DirectoryDeleted(tempDir \ "foo")
-//				probe expectMsg DirectoryCreated(tempDir / "bar" /)
-//			}
-//		}
-//	}
-//	"Path" >> {
-//		"override hashcode and equals" >> new TempDir {
-//			val p1 = Paths.get(tempDir.path)
-//			val p2 = Paths.get(tempDir.path)
-//			p1 should not be p2
-//			p1.hashCode === p2.hashCode
-//			p1 === p2
-//		}
-//	}
-//}
+package dirwatch
+
+import java.io.File
+import java.util.concurrent.{LinkedBlockingQueue, TimeUnit}
+
+import akka.actor.ActorSystem
+import common.rich.path.TempDirectory
+import dirwatch.DirectoryWatcher._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FreeSpec, Matchers, OneInstancePerTest}
+import org.specs2.time.Duration
+import rx.lang.scala.Subscriber
+
+import scala.concurrent.duration.FiniteDuration
+
+class DirectoryWatcherTest extends FreeSpec with MockitoSugar with OneInstancePerTest with Matchers {
+  // FFS
+  implicit def durationToFinishDuration(d: Duration) = FiniteDuration(d.inMillis, TimeUnit.MILLISECONDS)
+  implicit val x = ActorSystem("test")
+  val tempDir = TempDirectory()
+  val q = new LinkedBlockingQueue[DirectoryEvent]()
+  def expectMessage(msg: DirectoryEvent) {
+    q.poll(1, TimeUnit.SECONDS) match {
+      case null => fail(s"Expected $msg, but did not receive")
+      case e: DirectoryEvent if msg == e => ()
+      case _ => expectMessage(msg)
+    }
+  }
+  def acceptMessage(msg: DirectoryEvent) = q offer msg
+  DirectoryWatcher(Seq(tempDir)).apply(Subscriber(acceptMessage))
+  "DirectoryWatcher" - {
+    "handle new creations" - {
+      "new file created" in {
+        tempDir.addFile("foo.bar")
+        expectMessage(OtherChange)
+      }
+      "new folder created" in {
+        tempDir.addSubDir("foo")
+        expectMessage(DirectoryCreated(tempDir / "foo" /))
+      }
+      "new folder created in recursive" in {
+        val foo = tempDir.addSubDir("foo")
+        expectMessage(DirectoryCreated(tempDir / "foo" /))
+        foo.addSubDir("bar")
+        expectMessage(DirectoryCreated(tempDir / "foo" / "bar" /))
+      }
+    }
+    "handle deletes" - {
+      "file deleted" in {
+        val file = tempDir.addFile("foo.bar")
+        file.delete
+        expectMessage(OtherChange)
+      }
+      "directory deleted" in {
+        val d = tempDir.addSubDir("foo")
+        expectMessage(DirectoryCreated(tempDir / "foo" /)) // waits for change
+        d.deleteAll
+        expectMessage(DirectoryDeleted(tempDir \ "foo"))
+      }
+    }
+    "handle renames" - {
+      "directory renamed" in {
+        val d = tempDir.addSubDir("foo")
+        expectMessage(DirectoryCreated(tempDir / "foo" /)) // waits for change
+        d.dir.renameTo(new File(d.parent, "bar"))
+        expectMessage(DirectoryDeleted(tempDir \ "foo"))
+        expectMessage(DirectoryCreated(tempDir / "bar" /))
+      }
+    }
+  }
+}
