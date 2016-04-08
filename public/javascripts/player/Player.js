b39a50cb6b2dddcd796f92e6d601ad7575eadb47
gal_lalouche <glpkmtg@gmail.com>
2016-04-08 17:25:19 +0300

    Defined a new player and playlist interface

---
+++ public/javascripts/player/Player.js
@@ -0,0 +1,52 @@
+
+class Player {
+  load(song) {
+    throw new Error("Abstract")
+  }
+  playCurrentSong() {
+    throw new Error("Abstract")
+  }
+  stop() {
+    throw new Error("Abstract")
+  }
+  pause() {
+    throw new Error("Abstract")
+  }
+  // one of PLAYING, PAUSED, STOPPED
+  isPaused() {
+    throw new Error("Abstract")
+  }
+  restart() {
+    this.stop();
+    this.playCurrentSong();
+  }
+  togglePause() {
+    if (this.isPaused())
+      this.playCurrentSong()
+    else
+      this.pause()
+  }
+}
+class Playlist {
+  add(song, playNow) {
+    throw new Error("Abstract")
+  }
+  next() {
+    throw new Error("Abstract")
+  }
+  prev() {
+    throw new Error("Abstract")
+  }
+  currentIndex() {
+    throw new Error("Abstract")
+  }
+  currentPlayingSong() {
+    return this.songs()[this.currentIndex()]
+  }
+  // returns an array of songs
+  songs() {
+    throw new Error("Abstract")
+  }
+}
+gplayer = new Player()
+gplaylist = new Playlist()
