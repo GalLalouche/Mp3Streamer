9843829bccd6a47935544d69953e075f30c1723c
gal_lalouche <glpkmtg@gmail.com>
2016-04-08 17:25:53 +0300

    shortcuts now go throught the interface

--- public/javascripts/shortcuts.js
+++ public/javascripts/shortcuts.js
@@ -1,47 +1,33 @@
-$(function() {
-  const click = what => $(".jp-" + what).click()
+$(function () {
   const isFromInput = tag => tag == 'input' || tag == 'textarea'
-  const isPaused = () => $('#jquery_jplayer_1').data().jPlayer.status.paused
-  function togglePlay() { isPaused() ? click("play") : click("pause") }
 
-  $(document).keyup(function(e) {
-    const jPlayer = $('#jquery_jplayer_1').data().jPlayer;
-    if (isFromInput(e.target.tagName.toLowerCase()))
+  $(document).keyup(function (e) {
+    if (isFromInput(e.target.tagName.toLowerCase())) // ignore text input into boxes
       return
     const letter = String.fromCharCode(e.which);
     switch (letter) {
-    case 'Z':
-      if (playlist.current > 0) // if not first song, go to previous song; otherwise restart song
-        click("previous");
-      else {
-        click("stop");
-        click("play");
-      }
-      break;
-    case 'X': // restart song if playing, otherwise unpause
-      if (isPaused()) {
-        click("play");
-      } else {
-        click("stop");
-        click("play");
-      }
-      break;
-    case 32: // Space key
-    case 'K': // fucking youtube :\
-    case 'C':
-      togglePlay()
-      break;
-    case 'V':
-      click("stop");
-      break;
-    case 'B':
-      click("next");
-      break;
+      case 'Z':
+        gplayer.stop();
+        if (gplaylist.currentIndex() > 0) // if not first song, go to previous song; otherwise restart song
+          gplaylist.prev()
+        gplayer.load(gplaylist.currentPlayingSong())
+        gplayer.playCurrentSong()
+        break;
+      case 32: // Space key
+      case 'K': // fucking youtube :\
+      case 'C':
+        gplayer.togglePause()
+        break;
+      case 'V':
+        gplayer.stop()
+        break;
+      case 'B':
+        gplaylist.next()
+        break;
     }
   });
-
-  // technically not a shortcut, but oh well
-  $(document).on("click", ".poster", function(e) {
-    togglePlay();
+  // pauses on poster click
+  $(document).on("click", ".poster", function (e) {
+    gplayer.togglePause()
   });
 });
