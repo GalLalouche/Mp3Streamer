a96cbe7782489be40431bfe8b74eb6b27b1729a6
Gal Lalouche <lalouche@google.com>
2016-07-21 15:51:08 +0300

    RichOptionFuture and tests for RichFuture

--- app/common/RichFuture.scala
+++ app/common/RichFuture.scala
@@ -16,9 +16,13 @@ object RichFuture {
     })
     def get: T = Await.result($, Duration.Inf)
     // like recover, but doesn't care about the failure
-    def orElse(t: => T): Future[T] = $.recover {case e => t}
+    def orElse(t: => T): Future[T] = $.recover { case e => t }
     // implicits suck with overloads it seems
-    def orElseTry(t: => Future[T]): Future[T] = $.recoverWith {case e => t}
+    def orElseTry(t: => Future[T]): Future[T] = $.recoverWith { case e => t }
     def onEnd[S](f: => Future[S]): Future[S] = $.flatMap(e => f)
+  }
+  implicit class richOptionFuture[T]($: Future[Option[T]])(implicit ec: ExecutionContext) {
+    def ifNone(t: => T): Future[T] = $.map(_ getOrElse t)
+    def ifNoneTry(t: => Future[T]): Future[T] = $.flatMap(_ map Future.successful getOrElse t)
   }
 }
