4308838d14a35b51b41c83748726659214c71f0b
Gal Lalouche <lalouche@google.com>
2016-07-21 18:40:21 +0300

    added NewLocalStorage and tests

---
+++ app/common/storage/NewLocalStorage.scala
@@ -0,0 +1,18 @@
+package common.storage
+
+import scala.concurrent.{ExecutionContext, Future}
+
+abstract class NewLocalStorage[Key, Value](implicit ec: ExecutionContext) {
+  /** If an existing value exists, override it. */
+  protected def internalForceStore(k: Key, v: Value): Future[Unit]
+  /** Returns the previous value associated with the key, or None. */
+  def forceStore(k: Key, v: Value): Future[Option[Value]] =
+    load(k).flatMap(existing => internalForceStore(k, v).map(e => existing))
+  /** Does not override. Returns true if successful, false otherwise. */
+  def store(k: Key, v: Value): Future[Boolean] =
+    load(k)
+      .map(_.isDefined)
+      .flatMap(if (_) Future successful false else internalForceStore(k, v).map(e => true))
+  /** Returns the value associated with the key, if one exists, or None. */
+  def load(k: Key): Future[Option[Value]]
+}
