428208081d550cc3b9c00b82666dae33ca4b4989
Gal PC <glpkmtg@gmail.com>
2016-07-22 12:41:19 +0300

    refactoring and fixing NewAlbumsRetriever

--- app/backend/mb/ArtistReconStorage.scala
+++ app/backend/mb/ArtistReconStorage.scala
@@ -10,7 +10,7 @@ import scala.concurrent.Future
 private object ArtistReconStorage extends ReconStorage[Artist] {
   private class Artists(tag: Tag) extends Table[(String, Option[String], Boolean)](tag, "ARTISTS") {
     def name = column[String]("NAME", O.PrimaryKey)
-    def musicBrainzId = column[Option[String]]("MUSIC_BRAINZ_String")
+    def musicBrainzId = column[Option[String]]("MUSIC_BRAINZ_ID")
     def isIgnored = column[Boolean]("IS_IGNORED", O.Default(false))
     def * = (name, musicBrainzId, isIgnored)
   }
--- app/common/storage/OnlineRetrieverCacher.scala
+++ app/common/storage/OnlineRetrieverCacher.scala
@@ -1,18 +1,21 @@
 package common.storage
 
-import scala.concurrent.{ExecutionContext, Future}
 import common.RichFuture._
 
+import scala.concurrent.{ExecutionContext, Future}
+
 /**
-  * Tries to first retrieve information from a local repository.
-  * If it fails, it will try to use an online API, and save the result persistently.
-  */
-class OnlineRetrieverCacher[Key, Value](localStorage: LocalStorage[Key, Value], onlineRetriever: Key => Future[Value])
-                                       (implicit ec: ExecutionContext) extends (Key => Future[Value]) {
+ * Tries to first retrieve information from a local repository.
+ * If it fails, it will try to use an online API, and save the result persistently.
+ */
+class OnlineRetrieverCacher[Key, Value](
+    localStorage: LocalStorage[Key, Value],
+    onlineRetriever: Retriever[Key, Value])
+    (implicit ec: ExecutionContext) extends Retriever[Key, Value] {
   override def apply(k: Key): Future[Value] = localStorage.load(k)
-    .ifNoneTry {
-      val f = onlineRetriever(k)
-      f.foreach(localStorage.store(k, _))
-      f
-    }
+      .ifNoneTry(onlineRetriever(k).flatMap(v =>
+        localStorage.store(k, v)
+            .filter(identity)
+            .map(e => v))
+      )
 }
