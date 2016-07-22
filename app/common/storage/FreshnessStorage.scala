f521cbc31eae3c13b31564671fcbd7f49927af17
Gal PC <glpkmtg@gmail.com>
2016-07-22 15:48:09 +0300

    added FreshnessStorage

--- app/common/storage/FreshnessStorage.scala
+++ app/common/storage/FreshnessStorage.scala
@@ -1,13 +1,20 @@
-//package common.storage
-//
-//import org.joda.time.DateTime
-//
-//import scala.concurrent.{ExecutionContext, Future}
-//
-//class FreshnessStorage[Key, Value](storage: LocalStorage[Key, (Value, DateTime)])
-//                                  (implicit ec: ExecutionContext) extends LocalStorage[Key, Value] {
-//  def freshness(k: Key): Future[Option[(Value, DateTime)]] = storage load k
-//  override def store(k: Key, v: Value): Future[Option[Value]] = ???
-////    storage.store(k, v -> DateTime.now()).map(_.map(_._1))
-//  override def load(k: Key): Future[Option[Value]] = storage.load(k).map(_.map(_._1))
-//}
+package common.storage
+
+import common.rich.RichT._
+import org.joda.time.DateTime
+
+import scala.concurrent.{ExecutionContext, Future}
+
+
+class FreshnessStorage[Key, Value](storage: LocalStorage[Key, (Value, DateTime)])
+    (implicit ec: ExecutionContext) extends LocalStorage[Key, Value] {
+  private def now(v: Value): (Value, DateTime) = v -> DateTime.now
+  private def toValue(v: Future[Option[(Value, DateTime)]]) = v.map(_.map(_._1))
+  def freshness(k: Key): Future[Option[(Value, DateTime)]] = storage load k
+  override def store(k: Key, v: Value) =
+    storage.store(k, now(v))
+  override def load(k: Key): Future[Option[Value]] =
+    storage.load(k) |> toValue
+  override def forceStore(k: Key, v: Value): Future[Option[Value]] =
+    storage.forceStore(k, now(v)) |> toValue
+}
--- app/common/storage/RefreshableStorage.scala
+++ app/common/storage/RefreshableStorage.scala
@@ -1,19 +1,21 @@
-//package common.storage
-//
-//import common.rich.RichT._
-//import org.joda.time.{DateTime, Duration}
-//
-//import scala.concurrent.{ExecutionContext, Future}
-//
-//class RefreshableStorage[Key, Value](freshnessStorage: FreshnessStorage[Key, Value],
-//                                     onlineRetriever: Retriever[Key, Value],
-//                                     maxAge: Duration)
-//                                    (implicit ec: ExecutionContext) extends (Key => Future[Value]) {
-//  private def age(dt: DateTime): Duration = Duration.millis(DateTime.now().getMillis - dt.getMillis)
-//  private def refresh(k: Key): Future[Value] =
-//    onlineRetriever(k).flatMap(v => freshnessStorage.store(k, v).map(Unit => v))
-//  override def apply(k: Key): Future[Value] =
-//    freshnessStorage.freshness(k)
-//      .map(_.map(_._2.mapTo(age).isShorterThan(maxAge)).getOrElse(false))
-//      .flatMap(b => if (b) freshnessStorage.load(k).map(_.get) else refresh(k))
-//}
+package common.storage
+
+import common.rich.RichT._
+import org.joda.time.{DateTime, Duration}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class RefreshableStorage[Key, Value](freshnessStorage: FreshnessStorage[Key, Value],
+                                     onlineRetriever: Retriever[Key, Value],
+                                     maxAge: Duration)
+                                     (implicit ec: ExecutionContext) extends Retriever[Key, Value] {
+  private def age(dt: DateTime): Duration = Duration.millis(DateTime.now().getMillis - dt.getMillis)
+  private def needsRefresh(k: Key): Future[Boolean] =
+    freshnessStorage.freshness(k)
+        .map(_.map(_._2.mapTo(age).isLongerThan(maxAge)).getOrElse(true))
+  
+  private def refresh(k: Key): Future[Value] =
+    onlineRetriever(k).flatMap(v => freshnessStorage.store(k, v).map(Unit => v))
+  override def apply(k: Key): Future[Value] =
+    needsRefresh(k).flatMap(b => if (b) refresh(k) else freshnessStorage.load(k).map(_.get))
+}
