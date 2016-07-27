b5ee2f9d8fa5732533b6744d7d0bdc44261704e6
Gal Lalouche <lalouche@google.com>
2016-07-27 18:03:19 +0300

    added test to MbHtmlLinkExtractor

--- app/backend/mb/MbHtmlLinkExtractor.scala
+++ app/backend/mb/MbHtmlLinkExtractor.scala
@@ -12,21 +12,20 @@ import scala.concurrent.{ExecutionContext, Future}
 
 private sealed class MbHtmlLinkExtractor[T <: Reconcilable](metadataType: String)(implicit ec: ExecutionContext)
     extends ExternalLinkProvider[T] {
-  private def getHtml(artistId: String): Future[Document] =
+  private[mb] def getHtml(artistId: String): Future[Document] =
     DocumentDownloader(Url(s"https://musicbrainz.org/$metadataType/$artistId"))
-
   private def extractLink(e: Element): ExternalLink[T] = {
-    val url: Url = Url(e.child(0).attr("href"))
+    val url: Url = Url(e.child(0).attr("href").mapIf(_.startsWith("//")).to("https:" + _))
     val sourceName = e.className
-      .takeWhile(_ != '-')
-      .mapIf(_ == "no").to(x => e.child(0).text())
+        .takeWhile(_ != '-')
+        .mapIf(_ == "no").to(x => e.child(0).text())
     ExternalLink(url, Host(sourceName, url.host))
   }
   def extractLinks(d: Document): Seq[ExternalLink[T]] =
     d.select(".external_links")
-      .select("li")
-      .dropRight(1)
-      .map(extractLink)
+        .select("li")
+        .filterNot(_.className() == "all-relationships")
+        .map(extractLink)
 
   override def apply(id: ReconID): Future[Traversable[ExternalLink[T]]] =
     getHtml(id.id).map(extractLinks)
