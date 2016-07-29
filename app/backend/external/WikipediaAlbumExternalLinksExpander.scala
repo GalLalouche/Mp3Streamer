d1b9d0fc45cdccdfd00dfb19247b773b3b74b615
Gal PC <glpkmtg@gmail.com>
2016-07-29 22:42:33 +0300

    fixed a bug where wikipedia failed if there was no link to extract

--- app/backend/external/WikipediaAlbumExternalLinksExpander.scala
+++ app/backend/external/WikipediaAlbumExternalLinksExpander.scala
@@ -5,22 +5,19 @@ import java.util.regex.Pattern
 import backend.Url
 import backend.recon.Album
 import common.rich.RichT._
-import common.rich.collections.RichTraversableOnce._
 import org.jsoup.nodes.Document
 
 import scala.collection.JavaConversions._
 
 object WikipediaAlbumExternalLinksExpander extends ExternalLinkExpander[Album](Host.Wikipedia) {
   val re = Pattern compile "http://www.allmusic.com/album/[a-zA-Z\\-0-9]+"
-  private def extractAllMusicLink(d: Document): ExternalLink[Album] =
+  private def extractAllMusicLink(d: Document): Option[ExternalLink[Album]] =
     d.select("a")
         .map(_.attr("href"))
-        .filter(re.matcher(_).matches)
-        .single
-        .mapTo(Url)
-        .mapTo(url => ExternalLink(url, Host("allmusic", url.host)))
-
-  override def apply(d: Document): Links[Album] = {
-    List(extractAllMusicLink(d))
-  }
+        .find(re.matcher(_).matches)
+        .map(_
+            .mapTo(Url)
+            .mapTo(url => ExternalLink(url, Host("allmusic", url.host))))
+  
+  override def apply(d: Document): Links[Album] = extractAllMusicLink(d)
 }
--- test/src/backend/external/WikipediaAlbumExternalLinksExpanderTest.scala
+++ test/src/backend/external/WikipediaAlbumExternalLinksExpanderTest.scala
@@ -7,10 +7,16 @@ import org.jsoup.Jsoup
 import org.scalatest.FreeSpec
 
 class WikipediaAlbumExternalLinksExpanderTest extends FreeSpec with AuxSpecs {
-  "foobar" in {
-    val d = Jsoup.parse(getResourceFile("example.html").readAll)
+  def getDocument(s: String) = Jsoup.parse(getResourceFile(s).readAll)
+  "extract allmusic link" in {
     val allMusicLink =
-      WikipediaAlbumExternalLinksExpander(d).filter(_.host.name == "allmusic").map(_.link.address).single
+      WikipediaAlbumExternalLinksExpander(getDocument("allmusic_link.html"))
+          .filter(_.host.name == "allmusic")
+          .map(_.link.address)
+          .single
     allMusicLink shouldReturn "http://www.allmusic.com/album/born-in-the-usa-mw0000191830"
+  }
+  "succeed even if there is no link" in {
+    WikipediaAlbumExternalLinksExpander(getDocument("no_link.html")) shouldReturn Nil
   }
 }
