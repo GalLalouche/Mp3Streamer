f037f4314df46d95c6bac4478d987bcd39c15762
Gal Lalouche <lalouche@google.com>
2016-08-05 02:12:06 +0300

    added app bak client conf lib Mp3Streamer.iml project public README resources target test X in the hosts of new links

--- app/backend/external/ExternalPipe.scala
+++ app/backend/external/ExternalPipe.scala
@@ -4,12 +4,18 @@ import backend.recon.{ReconID, Reconcilable}
 import backend.storage.Retriever
 
 import scala.concurrent.{ExecutionContext, Future}
+import common.rich.collections.RichSet._
 
 class ExternalPipe[T <: Reconcilable](reconciler: Retriever[T, ReconID],
                                       provider: Retriever[ReconID, Links[T]],
                                       expander: Retriever[Links[T], Links[T]])
                                      (implicit ec: ExecutionContext) extends Retriever[T, Links[T]] {
-  private def expand(links: Links[T]) = expander(links).map(_.toSet ++ links.toSet)
+  private def markDiff(el: ExternalLink[T]): ExternalLink[T] =
+    el.copy(host = el.host.copy(name = el.host.name + "*"))
+  private def expand(existing: Links[T]): Future[Links[T]] = expander(existing).map { newLinks =>
+    val existingSet: Set[ExternalLink[T]] = existing.toSet
+    (newLinks.toSet \ existingSet map markDiff) ++ existingSet
+  }
   override def apply(t: T): Future[Links[T]] =
     reconciler(t)
         .flatMap(provider)
---
+++ test/src/backend/external/ExternalPipeTest.scala
@@ -0,0 +1,25 @@
+package src.backend.external
+
+import backend.TestConfiguration._
+import backend.Url
+import backend.external.{ExternalLink, ExternalPipe, Host}
+import backend.recon.{Album, ReconID}
+import common.AuxSpecs
+import org.scalatest.FreeSpec
+
+import scala.concurrent.Future
+import common.RichFuture._
+
+class ExternalPipeTest extends FreeSpec with AuxSpecs {
+  val existingLink: ExternalLink[Album] = ExternalLink(Url("existing"), Host("host", Url("hosturl")))
+  val newLink: ExternalLink[Album] = ExternalLink(Url("new"), Host("newhost", Url("newhosturl")))
+  val expectedNewLink: ExternalLink[Album] = ExternalLink(Url("new"), Host("newhost*", Url("newhosturl")))
+  "should add * to new links" in {
+    val $ = new ExternalPipe[Album](x => Future successful ReconID("foobar"), x => Future successful List(existingLink), x => Future successful List(newLink))
+    $(null).get shouldReturn Set(existingLink, expectedNewLink)
+  }
+  "should not add * to existing links" in {
+    val $ = new ExternalPipe[Album](x => Future successful ReconID("foobar"), x => Future successful List(existingLink), x => Future successful List(newLink, existingLink))
+    $(null).get shouldReturn Set(existingLink, expectedNewLink)
+  }
+}
