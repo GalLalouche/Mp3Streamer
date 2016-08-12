c706e1530504af5ea739a2a2962d501c50a698dd
Gal PC <glpkmtg@gmail.com>
2016-08-12 21:34:35 +0300

    no more varness in cover :D

--- app/mains/cover/AsyncFolderImagePanel.scala
+++ app/mains/cover/AsyncFolderImagePanel.scala
@@ -1,61 +1,41 @@
 package mains.cover
 
-import java.util.concurrent.Semaphore
-
 import scala.concurrent.ExecutionContext
 import scala.swing.event.MouseClicked
 import scala.swing.{Button, GridPanel, Label, TextArea}
 
-/** MUTANT TEENAGE NINJA TURTLES :D */
-private class AsyncFolderImagePanel(rows: Int, cols: Int, imagesSupplier: ImagesSupplier)(implicit ec: ExecutionContext)
-    extends GridPanel(rows0 = rows, cols0 = cols) with AutoCloseable {
-  val openBrowserButton: Button = Button.apply("Fuck it, I'll do it myself!") {
-    AsyncFolderImagePanel.this.publish(OpenBrowser)
-    this.close()
-  }
-  private val thread = new DelayedThread("Image placer")
-  private val waitForNextClick = new Semaphore(0)
-  private var realSize = 0
-  def start() {
-    def createImagePanel(image: FolderImage) =
-      new Label {
-        icon = image.imageIcon
-        listenTo(mouse.clicks)
-        reactions += {
-          case e: MouseClicked => AsyncFolderImagePanel.this.publish(Selected(image))
-        }
-      }
-    thread.start(
-      () => {
-        if (contents.isEmpty) {
-          realSize = 0
-          (0 until rows * cols).foreach(i => contents += new TextArea("Placeholder for image #" + i))
-          contents += openBrowserButton
-          contents += Button.apply("Show me more")(waitForNextClick.release())
-          Thread sleep 100
-        }
-        else if (realSize < rows * cols) {
-          val currentIndex = realSize
-          imagesSupplier.next().map(createImagePanel).foreach{e =>
-            contents.synchronized {
-              contents.update(currentIndex, e)
-            }
-          }
-          realSize += 1
-        }
-        else {
-          // if done, wait for release
-          waitForNextClick.acquire()
-          contents.clear()
-        }
-      }
-    )
+/** Eventually publishes an ImageChoice event. */
+private class AsyncFolderImagePanel(rows: Int, cols: Int, imagesSupplier: ImagesSupplier)
+                                   (implicit ec: ExecutionContext) extends GridPanel(rows0 = rows, cols0 = cols) {
+  private def createImagePanel(image: FolderImage) = new Label {
+    icon = image.imageIcon
+    listenTo(mouse.clicks)
+    reactions += {
+      case e: MouseClicked => AsyncFolderImagePanel.this.publish(Selected(image))
+    }
   }
 
-  override def close() = {
-    thread.close()
+  // TODO consider creating a new panel instead
+  def refresh() {
     contents.clear()
-    super.repaint()
-    visible = false
+    // Prepopulate the grid to avoid images moving around.
+    (0 until rows * cols).foreach(i => contents += new TextArea("Placeholder for image #" + i))
+    contents += Button.apply("Fuck it, I'll do it myself!") {
+      AsyncFolderImagePanel.this.publish(OpenBrowser)
+    }
+    contents += Button.apply("Show me more...") {
+      refresh()
+    }
+    0 until (rows * cols) foreach { currentIndex =>
+      imagesSupplier.next().map(createImagePanel).foreach { e =>
+        contents.synchronized {
+          contents.update(currentIndex, e)
+          // forces a redrawing of the panel
+          visible = false
+          Thread sleep 10
+          visible = true
+        }
+      }
+    }
   }
 }
--- app/mains/cover/ImageSelectionPanel.scala
+++ app/mains/cover/ImageSelectionPanel.scala
@@ -10,28 +10,24 @@ import scala.swing.event.{ComponentAdded, WindowClosing}
 
 /** Displays several images to the user, and returns the selected image */
 private class ImageSelectionPanel private(imagesSupplier: ImagesSupplier)(implicit ec: ExecutionContext) {
-  private val rows = 2
-  private val cols = 3
   def choose(): Future[ImageChoice] = {
     val promise = Promise[ImageChoice]
     val frame = new Frame {
-      reactions += {case e: WindowClosing => promise success Cancelled}
+      reactions += { case e: WindowClosing => promise success Cancelled }
     }
-    val panel = new AsyncFolderImagePanel(cols = cols, rows = rows, imagesSupplier = imagesSupplier)
-    panel.reactions += {
-      case e: ComponentAdded => frame.pack()
-      case e: ImageChoice =>
-        promise success e
-        panel.close()
+    val panel = new AsyncFolderImagePanel(cols = 3, rows = 2, imagesSupplier = imagesSupplier) {
+      reactions += {
+        case e: ComponentAdded =>
+          frame.pack()
+          frame.repaint()
+        case e: ImageChoice => promise success e
+      }
     }
     frame.contents = panel
-    panel.start()
+    panel.refresh()
     frame.open()
     val $ = promise.future
-    $.onComplete { e =>
-      panel.close()
-      frame.dispose()
-    }
+    $ onComplete { e => frame.dispose() }
     $
   }
 }
@@ -42,18 +38,17 @@ private object ImageSelectionPanel {
 
   def main(args: Array[String]): Unit = {
     import common.RichFuture._
-    implicit val ec = new ExecutionContext {
-      override def reportFailure(cause: Throwable): Unit = ???
-      override def execute(runnable: Runnable): Unit = runnable.run()
-    }
-    apply(new ImagesSupplier {
+
+    import scala.concurrent.ExecutionContext.Implicits.global
+    val x = apply(new ImagesSupplier {
       override def next(): Future[FolderImage] = {
-        val p = new FolderImage(
-        new IOFile(new File("""D:\Incoming\Bittorrent\Completed\Music\Bob Dylan\1 - Studio Albums\1963 - The Freewheelin Bob Dylan\folder.jpg""")))
-        Future successful p
+        Future apply {
+          new FolderImage(
+            new IOFile(
+              new File("""D:\Incoming\Bittorrent\Completed\Music\Bob Dylan\1 - Studio Albums\1963 - The Freewheelin Bob Dylan\folder.jpg""")))
+        }
       }
     }).get
-    println("Done")
-    readLine()
+    println(x)
   }
 }
