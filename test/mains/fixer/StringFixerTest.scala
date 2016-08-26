a1ccb661bfa5b5f50ee05bf0d29eb11a47f2ea77
Gal PC <glpkmtg@gmail.com>
2016-08-26 10:49:25 +0300

    StringFixer bug fix + tests (finally)

---
+++ test/mains/fixer/StringFixerTest.scala
@@ -0,0 +1,26 @@
+package mains.fixer
+
+import common.AuxSpecs
+import org.scalatest.FreeSpec
+
+class StringFixerTest extends FreeSpec with AuxSpecs {
+  private val $ = StringFixer
+  def verifyFix(original: String, fix: String) {
+    s"$original should equal $fix" in {
+      $(original) shouldReturn fix
+    }
+  }
+
+  "one word" - {
+    verifyFix("1St", "1st")
+    verifyFix("2Nd", "2nd")
+    verifyFix("3Rd", "3rd")
+    verifyFix("4th", "4th")
+  }
+  "sentence" - {
+    verifyFix("I Am A Rock", "I am a Rock")
+  }
+  "ordered" in {
+    $.lowerCaseWords.toList.sorted shouldReturn $.lowerCaseWords.toList
+  }
+}
