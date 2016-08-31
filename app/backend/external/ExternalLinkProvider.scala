9a114dd2b282e23718e99cd1c5e5d265d15a9b71
Gal Lalouche <lalouche@google.com>
2016-08-31 13:52:56 +0300

    Refactored ExternalLinkProvider to be a type

--- app/backend/external/ExternalLinkProvider.scala
+++
@@ -1,7 +0,0 @@
-package backend.external
-
-import backend.recon.{ReconID, Reconcilable}
-import backend.Retriever
-
-trait ExternalLinkProvider[T <: Reconcilable] extends Retriever[ReconID, Links[T]]
-
--- app/backend/external/package.scala
+++ app/backend/external/package.scala
@@ -1,10 +1,11 @@
 package backend
 
 import backend.external.extensions.ExtendedLink
-import backend.recon.Reconcilable
+import backend.recon.{ReconID, Reconcilable}
 
 package object external{
   type Links[R <: Reconcilable] = Traversable[ExternalLink[R]]
   type ExtendedLinks[R <: Reconcilable] = Traversable[ExtendedLink[R]]
   type HostMap[T] = Map[Host, T]
+  type ExternalLinkProvider[R <: Reconcilable] = Retriever[ReconID, Links[R]]
 }
