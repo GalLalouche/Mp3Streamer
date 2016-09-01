8d64ce7a355e55883a023ca82e5b136367d0df72
Gal Lalouche <lalouche@google.com>
2016-09-01 10:08:22 +0300

    Additional Reconiclers is only called if needed

--- app/backend/external/ExternalPipe.scala
+++ app/backend/external/ExternalPipe.scala
@@ -10,38 +10,53 @@ import common.rich.collections.RichTraversableOnce._
 
 import scala.concurrent.{ExecutionContext, Future}
 
-private[external] class ExternalPipe[R <: Reconcilable](reconciler: Retriever[R, ReconID],
-                                      provider: Retriever[ReconID, Links[R]],
-                                      expander: Traversable[ExternalLinkExpander[R]],
-                                      additionalReconciler: Traversable[Reconciler[R]])
-                                     (implicit ec: ExecutionContext) extends Retriever[R, Links[R]] {
+/**
+ * Encompasses all online steps for fetching the external link for a given entity.
+ * @param reconciler Matches the entity to an API's ID
+ * @param linksRetriever Fetches the external links attached to the entity's ID
+ * @param expanders Attempts to expand the links found by scraping the initial set
+ * @param additionalReconcilers Any additional, standalone reconciliations that can be performed
+ */
+//TODO recursively deep?
+private class ExternalPipe[R <: Reconcilable](reconciler: Retriever[R, ReconID],
+                                              linksRetriever: Retriever[ReconID, Links[R]],
+                                              expanders: Traversable[ExternalLinkExpander[R]],
+                                              additionalReconcilers: Traversable[Reconciler[R]])
+                                             (implicit ec: ExecutionContext) extends Retriever[R, Links[R]] {
   private def markDiff(el: ExternalLink[R]): ExternalLink[R] =
     el.copy(host = el.host.copy(name = el.host.name + "*"))
-  private def getDiff(existingLinks: Links[R], newLinks: Links[R]): Links[R] = {
+  private def filterLinksWithNewHosts(existingLinks: Links[R], newLinks: Links[R]): Links[R] = {
     val map = existingLinks.map(_.host).toSet
     newLinks.filterNot(map contains _.host)
   }
 
-  private def filterExpanders(existingHosts: Set[Host]) = { // remove expanders that can only returns existing hosts
+  // remove expanders that can only returns existing hosts
+  private def filterExpanders(existingHosts: Set[Host]) = {
     def compose(es: Traversable[ExternalLinkExpander[R]])(links: Links[R]): Future[Links[R]] = {
       val map = es.mapBy(_.sourceHost)
       Future sequence links.flatMap(e => map.get(e.host).map(_ (e))) map (_.flatten)
     }
-    expander.filterNot(e => existingHosts >= e.potentialHostsExtracted.toSet) |> compose
+    expanders.filterNot(_.potentialHostsExtracted.toSet <= existingHosts) |> compose
   }
-  private def filterReconcilers(existingHosts: Set[Host]) = // removes reconcilers for existing hosts
-    additionalReconciler.filterNot(_.host |> existingHosts)
+
+  private def applyFilteredReconcilers(r: R, existingHosts: Set[Host]) = {
+    def filterReconcilers(existingHosts: Set[Host]) = // removes reconcilers for existing hosts
+      additionalReconcilers.filterNot(_.host |> existingHosts)
+    Future.sequence(filterReconcilers(existingHosts).map(_ (r))).map(_.flatten)
+  }
+
   private def expand(r: R, existing: Links[R]): Future[Links[R]] = {
-    val existingHosts = existing.map(_.host).toSet
+    def extractHosts(ls: Links[R]) = ls.map(_.host).toSet
+    val existingHosts = existing |> extractHosts
     for (newLinks <- filterExpanders(existingHosts)(existing);
-         additionalLinks <- Future sequence filterReconcilers(existingHosts).map(_ (r)) map (_.flatten)) yield {
+         additionalLinks <- applyFilteredReconcilers(r, existingHosts ++ extractHosts(newLinks))) yield {
       val existingSet = existing.toSet
       val newSet = newLinks.++(additionalLinks).toSet
-      existingSet ++ (getDiff(existingSet, newSet) map markDiff)
+      existingSet ++ (filterLinksWithNewHosts(existingSet, newSet) map markDiff)
     }
   }
   override def apply(r: R): Future[Links[R]] =
     reconciler(r)
-        .flatMap(provider)
+        .flatMap(linksRetriever)
         .flatMap(expand(r, _))
 }
--- test/scala/backend/external/ExternalPipeTest.scala
+++ test/scala/backend/external/ExternalPipeTest.scala
@@ -20,43 +20,52 @@ class ExternalPipeTest extends FreeSpec with AuxSpecs {
   private val reconciledLink: ExternalLink[Album] = ExternalLink(Url("new2"), Host("newhost2", Url("newhosturl2")))
   private val expectedNewLinks: Links[Album] = List(ExternalLink(Url("new"), Host("newhost*", Url("newhosturl"))),
     ExternalLink(Url("new2"), Host("newhost2*", Url("newhosturl2"))))
-  val constReconciler = new Reconciler[Album](reconciledLink.host) {
-    override def apply(a: Album) = Future successful Some(reconciledLink)
-  }
   def constExpander(links: ExternalLink[Album]*) = new ExternalLinkExpander[Album] {
     override def sourceHost: Host = existingHost
     override def potentialHostsExtracted: Traversable[Host] = links.map(_.host)
     override def apply(v1: ExternalLink[Album]): Future[Links[Album]] = Future successful links
   }
   val newLinkExpander = constExpander(expandedLink)
+  val newLinkReconciler = new Reconciler[Album](reconciledLink.host) {
+    override def apply(a: Album) = Future successful Some(reconciledLink)
+  }
   "should add * to new links" in {
     val $ = new ExternalPipe[Album](x => Future successful ReconID("foobar"),
       x => Future successful List(existingLink),
       List(newLinkExpander),
-      List(constReconciler))
+      List(newLinkReconciler))
     $(null).get shouldReturn (Set(existingLink) ++ expectedNewLinks)
   }
-  "should not invoke on existing hosts" in {
+  "lazy" - {
     val failed = Future failed new AssertionError("Shouldn't have been invoked")
-    val failedReconciler = new Reconciler[Album](existingHost) {
+    def failedReconciler(host: Host) = new Reconciler[Album](host) {
       override def apply(a: Album) = failed
     }
-    val failedExpander = new ExternalLinkExpander[Album] {
-      override val sourceHost: Host = existingHost
-      override val potentialHostsExtracted: Traversable[Host] = List(existingHost)
-      override def apply(v1: ExternalLink[Album]): Future[Links[Album]] = failed
+    "should not invoke on existing hosts" in {
+      val failedExpander = new ExternalLinkExpander[Album] {
+        override val sourceHost: Host = existingHost
+        override val potentialHostsExtracted: Traversable[Host] = List(existingHost)
+        override def apply(v1: ExternalLink[Album]): Future[Links[Album]] = failed
+      }
+      val $ = new ExternalPipe[Album](x => Future successful ReconID("foobar"),
+        x => Future successful List(existingLink),
+        List(failedExpander, newLinkExpander),
+        List(failedReconciler(existingHost), newLinkReconciler))
+      $(null).get shouldReturn Set(existingLink) ++ expectedNewLinks
     }
-    val $ = new ExternalPipe[Album](x => Future successful ReconID("foobar"),
-      x => Future successful List(existingLink),
-      List(failedExpander, newLinkExpander),
-      List(failedReconciler, constReconciler))
-    $(null).get shouldReturn Set(existingLink) ++ expectedNewLinks
+    "Should not invoke additional reconcilers if expanders already returned the host" in {
+      val $ = new ExternalPipe[Album](x => Future successful ReconID("foobar"),
+        x => Future successful List(existingLink),
+        List(newLinkExpander),
+        List(failedReconciler(expandedLink.host)))
+      $(null).get shouldReturn Set(existingLink) ++ expectedNewLinks.take(1)
+    }
   }
   "should ignored new, extra links" in {
     val $ = new ExternalPipe[Album](x => Future successful ReconID("foobar"),
       x => Future successful List(existingLink),
       List(constExpander(expandedLink, rehashedLinks)),
-      List(constReconciler))
+      List(newLinkReconciler))
     $(null).get shouldReturn Set(existingLink) ++ expectedNewLinks
 
   }
@@ -64,7 +73,7 @@ class ExternalPipeTest extends FreeSpec with AuxSpecs {
     val $ = new ExternalPipe[Album](x => Future successful ReconID("foobar"),
       x => Future successful List(existingLink, existingLink.copy(link = Url("existing2"))),
       List(newLinkExpander),
-      List(constReconciler))
+      List(newLinkReconciler))
     $(null).get should have size 4
   }
 }
