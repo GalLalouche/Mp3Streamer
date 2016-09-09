9015ff757185d3aebe09968d1167a6c01ebb6757
Gal Lalouche <lalouche@google.com>
2016-09-09 21:41:31 +0300

    added main for updating SongGroup

--- app/songs/SongGroups.scala
+++ app/songs/SongGroups.scala
@@ -1,5 +1,6 @@
 package songs
 
+import backend.configs.StandaloneConfig
 import common.io.{DirectoryRef, FileRef}
 import common.rich.RichT._
 import models.Song
@@ -16,10 +17,24 @@ object SongGroups {
     Future(root addFile "song_groups.json")
   private def writeToJsonFile(s: String)(implicit root: DirectoryRef, ec: ExecutionContext): Future[Unit] =
     getJsonFile.map(_ write s)
-  def save(groups: Traversable[SongGroup])(implicit root: DirectoryRef, ec: ExecutionContext): Future[Unit] =
-    groups
-        .map(_.songs.map(Jsonable.SongJsonifier.jsonify).toSeq |> JsArray)
-        .map(_.toString).mkString("\n") |> writeToJsonFile
-  def load(implicit root: DirectoryRef, ec: ExecutionContext): Future[Traversable[SongGroup]] = getJsonFile.map(
-    _.lines.map(Json.parse).map(_.as[JsArray].value.map(_.as[JsObject] |> Jsonable.SongJsonifier.parse)).map(_.toSet |> SongGroup))
+  def save(groups: Traversable[SongGroup])(implicit root: DirectoryRef, ec: ExecutionContext): Future[Unit] = groups
+      .map(_.songs.map(Jsonable.SongJsonifier.jsonify).toSeq |> JsArray)
+      .map(_.toString)
+      .mkString("\n") |> writeToJsonFile
+  def load(implicit root: DirectoryRef, ec: ExecutionContext): Future[Traversable[SongGroup]] = getJsonFile.map(_.lines
+      .map(Json.parse)
+      .map(_.as[JsArray].value.map(_.as[JsObject] |> Jsonable.SongJsonifier.parse))
+      .map(_.toSet |> SongGroup))
+
+  // Appends new groups and saves them
+  def main(args: Array[String]): Unit = {
+    import common.rich.RichFuture._
+    implicit val c = StandaloneConfig
+    import c._
+    def append(g: SongGroup): Future[Unit] =
+      load.map(g :: _.toList).flatMap(save)
+    val group: SongGroup = ???
+    append(group).get
+    println("Done")
+  }
 }
