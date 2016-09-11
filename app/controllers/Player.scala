103a74ca5eeef8033a16b3981bbb25b30514acd8
gal_lalouche <glpkmtg@gmail.com>
2016-09-11 16:59:43 +0300

    Fixed a bug in RichOption.either.

--- app/controllers/Player.scala
+++ app/controllers/Player.scala
@@ -8,13 +8,13 @@ import common.rich.RichT._
 import common.rich.path.Directory
 import common.rich.path.RichFile._
 import decoders.DbPowerampCodec
+import common.rich.primitives.RichEither._
+import common.rich.primitives.RichOption._
 import models._
 import play.api.libs.json.{JsArray, JsObject, JsString, JsValue}
 import play.api.mvc._
 import search.Jsonable._
 import songs.{SongGroup, SongGroups, SongSelector}
-import common.rich.primitives.RichOption._
-import common.rich.primitives.RichEither._
 
 /** Handles fetch requests of JSON information, and listens to directory changes. */
 object Player extends Controller with Debug {
@@ -37,14 +37,14 @@ object Player extends Controller with Debug {
   private def toJson(ss: SongGroup): JsArray = ss.songs map toJson mapTo JsArray
   private def toJson(e: Either[Song, SongGroup]): JsValue = e.resolve(toJson, toJson)
 
-  private def group(s: Song): Either[Song, SongGroup] = songGroups get s mapTo (_ either s) swap
+  private def group(s: Song): Either[Song, SongGroup] = songGroups get s either s
 
   def randomSong = Action {
     Ok(songSelector.randomSong |> group |> toJson)
   }
 
   def album(path: String) = Action {
-    Ok(Directory(URLDecoder.decode(path, "UTF-8")) |> Album.apply |> (_.songs.map(toJson)) |> JsArray.apply)
+    Ok(Utils.parseFile(path) |> Directory.apply |> Album.apply |> (_.songs.map(toJson)) |> JsArray.apply)
   }
 
   def song(path: String) = Action {
