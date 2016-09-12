d8380d522080cd6b0aa25413d90349f27f43076a
Gal PC <glpkmtg@gmail.com>
2016-09-12 12:47:52 +0300

    Fixing up XMLs and REGs

--- app/backend/configs/NonPersistentConfig.scala
+++ app/backend/configs/NonPersistentConfig.scala
@@ -6,7 +6,7 @@ import slick.driver.{H2Driver, JdbcProfile}
 
 trait NonPersistentConfig extends Configuration {
   override lazy implicit val driver: JdbcProfile = H2Driver
-  override implicit val db: driver.backend.DatabaseDef =
+  override implicit lazy val db: driver.backend.DatabaseDef =
     driver.api.Database.forURL("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", driver = "org.H2.JDBC")
   override implicit lazy val rootDirectory: DirectoryRef = new MemoryRoot
 }
