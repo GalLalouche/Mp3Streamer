b0ba1881c423a4941b519cd32435795afc42882e
gal_lalouche <glpkmtg@gmail.com>
2016-09-17 19:46:54 +0300

    Lyrics baseline. Maybe.

--- public/javascripts/lyrics.js
+++ public/javascripts/lyrics.js
@@ -1,37 +1,44 @@
 $(function () {
-  const lyrics = $('#lyrics');
+  const lyrics = $('#lyrics')
 
   function showLyrics(content) {
-    lyrics.html(content);
+    autoScroll = true
+    lyrics.html(content)
+    if (previousContent === lyrics.html()) // Ensure the same HTML formatting is used for comparison
+      return // HTML wasn't changed, so don't reset the baselines
+    previousContent = lyrics.html() // Ensure the same HTML formatting is used for comparison
+    scrollBaseline = 0
+    timeBaseline = 0
   }
 
   Lyrics.show = function (song) {
-    showLyrics("Loading lyrics...");
-    $.get("lyrics/" + song.file, l => showLyrics(l));
+    autoScroll = true
+    lyrics.html("Fetching lyrics...")
+    $.get("lyrics/" + song.file, function(l) {
+      showLyrics(l)
+      scrollLyrics()
+    })
   }
 
-  var interval = null;
-  var autoScroll = false;
+  var previousContent = ""
+  var autoScroll = false
+  var scrollBaseline = 0
+  var timeBaseline = 0
   function scrollLyrics() {
-    clearInterval(interval); // just in case
-    interval = setInterval(function scrollLyrics() {
-      const heightBuffer = lyrics.height() / 2.0; // don't start scrolling right at the beginning of the song
-      const timePercentage = gplayer.currentPlayingRelative() / 100;
-      autoScroll = true;
-      lyrics.scrollTop((lyrics.prop('scrollHeight') * timePercentage) - heightBuffer);
-    }, 1000)
+    // Don't start scrolling right at the beginning of the song if there is no baseline set
+    const heightBaseline =  scrollBaseline || (lyrics.height() / -1.75)
+    const timePercentage = (gplayer.currentPlayingRelative() - timeBaseline) / 100.0
+    autoScroll = true
+    lyrics.scrollTop(lyrics.prop('scrollHeight') * timePercentage + heightBaseline)
   }
-  scrollLyrics();
-  // when the *user* scrolls manually, stop auto scrolling for n seconds
-  var timeout = null;
-  lyrics.scroll(function resetScrollingInterval() {
-    if (autoScroll) { // don't trigger on auto scrolls :| bah this is ugly. NO - THIS. IS. JAVASCRIPT!
-      autoScroll = false;
-      return;
+  setInterval(scrollLyrics, 100)
+
+  lyrics.scroll(function() { // When the user scrolls manually, reset the baselines
+    if (!autoScroll) {
+      scrollBaseline = lyrics.scrollTop()
+      timeBaseline = scrollBaseline && gplayer.currentPlayingRelative()
     }
-    clearInterval(interval); // stop auto scrolling
-    clearTimeout(timeout); // kill any previous request to start auto scrolling
-    timeout = setTimeout(scrollLyrics, 5000); // start auto scrolling after 10 seconds
-  });
-});
-Lyrics = {};
+    autoScroll = false
+  })
+})
+Lyrics = {}
