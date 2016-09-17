38be96e97db87f9c559993d552ea9be6e9dded36
gal_lalouche <glpkmtg@gmail.com>
2016-09-17 16:03:59 +0300

    Refactoring

--- public/javascripts/jplayerdata.js
+++ public/javascripts/jplayerdata.js
@@ -1,54 +1,55 @@
 $(function () {
   const randomSongUrl = "data/randomSong";
-  // get a random song to start off with
-  $.get(randomSongUrl, function (data) {
-    // setup playlist
-    playlist = new jPlayerPlaylist({
-      jPlayer: "#jquery_jplayer_1",
-      cssSelectorAncestor: "#jp_container_1"
-    }, [data], {
-      swfPath: "../js",
-      supplied: "webmv, ogv, m4a, oga, mp3, flac"
-    });
-    $("#jp_poster_0").addClass("poster");
-    $("#jquery_jplayer_1").addClass("poster");
-    if (mute) // mute for debugging
-      $(".jp-mute").click()
-    else // or full volume for regular
-      $(".jp-volume-max").click();
-    $(".jp-shuffle").click(); // default to shuffle
-    // save a reference to the old next method before changing it
-    playlist.oldNext = playlist.next;
-    // same as the old, but loads a new random song if in shuffle mode and at the end
-    playlist.next = function () {
-      if (shouldLoadNextSongFromRandom())
-        loadNextRandom(true); 
-      else 
-        playlist.oldNext();
-    };
-    $(playlist.cssSelector.jPlayer).data("jPlayer").onPlay = function () {
-      const currentlyPlayingSong = playlist.currentPlayingSong();
-      const songInfo = `${currentlyPlayingSong.artistName} - ${currentlyPlayingSong.title}`;
-      $(".jp-currently-playing").html(songInfo);
-      document.title = songInfo;
-      $('#favicon').remove();
-      $('head').append(`<link href="${$("img.poster")[0].src}" id="favicon" rel="shortcut icon">`);
-      Lyrics.show(currentlyPlayingSong)
-      External.show(currentlyPlayingSong)
-      Volume.setPeak(currentlyPlayingSong)
-    };
+  // Init playlist
+  playlist = new jPlayerPlaylist({
+    jPlayer: "#jquery_jplayer_1",
+    cssSelectorAncestor: "#jp_container_1"
+  }, [], {
+    swfPath: "../js",
+    supplied: "webmv, ogv, m4a, oga, mp3, flac"
   });
+  // Modify next to fetch a random song if in shuffle mode and at the last song
+  playlist.oldNext = playlist.next;
+  const shouldLoadNextSongFromRandom = () => playlist.shuffled && playlist.isLastSongPlaying()
+  playlist.next = function () {
+    if (shouldLoadNextSongFromRandom())
+      loadNextRandom(true);
+    else
+      playlist.oldNext();
+  };
+  // On play event hook
+  // TODO don't call if the same song?
+  $(playlist.cssSelector.jPlayer).data("jPlayer").onPlay = function () {
+    const currentlyPlayingSong = playlist.currentPlayingSong();
+    const songInfo = `${currentlyPlayingSong.artistName} - ${currentlyPlayingSong.title}`;
+    $(".jp-currently-playing").html(songInfo);
+    document.title = songInfo;
+    $('#favicon').remove();
+    $('head').append(`<link href="${$("img.poster")[0].src}" id="favicon" rel="shortcut icon">`);
+    Lyrics.show(currentlyPlayingSong)
+    External.show(currentlyPlayingSong)
+    Volume.setPeak(currentlyPlayingSong)
+  };
+  // Poster classes
+  $("#jp_poster_0").addClass("poster");
+  $("#jquery_jplayer_1").addClass("poster");
+  // Mute for debugging if requested
+  if (mute)
+    $(".jp-mute").click()
+  else
+    $(".jp-volume-max").click();
+  $(".jp-shuffle").click(); // default to shuffle
   function loadNextRandom(playNow) {
     $.get(randomSongUrl, function (data) {
       playlist.add(data, playNow);
     });
-  };
-  function shouldLoadNextSongFromRandom() {
-    return playlist.shuffled && playlist.isLastSongPlaying()
   }
+
+  loadNextRandom(true)
+  // Fetches new songs before current song ends
   setInterval(function () {
-    const jPlayer = $(playlist.cssSelector.jPlayer).data("jPlayer");
-    var isSongNearlyFinished = jPlayer.htmlElement.media.duration - jPlayer.htmlElement.media.currentTime < WAIT_DELAY;
+    const media = $(playlist.cssSelector.jPlayer).data("jPlayer").htmlElement.media;
+    const isSongNearlyFinished = media.duration - media.currentTime < WAIT_DELAY;
     if (shouldLoadNextSongFromRandom() && isSongNearlyFinished)
       loadNextRandom(false);
   }, (WAIT_DELAY - 5) * 1000);
