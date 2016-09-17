878b4c5a9fe37bd8456ebd70e34f16fee1f88158
gal_lalouche <glpkmtg@gmail.com>
2016-09-17 12:30:50 +0300

    load playlist

--- public/javascripts/playlist.js
+++ public/javascripts/playlist.js
@@ -2,4 +2,7 @@ $(function () {
   $("#update_playlist").click(function() {
     $.post("playlist", JSON.stringify(gplaylist.songs().slice(gplaylist.currentIndex()).map(x => x.file)))
   })
+  $("#load_playlist").click(function() {
+    $.get("playlist", x => x.forEach(e => gplaylist.add(e, false)))
+  })
 })
