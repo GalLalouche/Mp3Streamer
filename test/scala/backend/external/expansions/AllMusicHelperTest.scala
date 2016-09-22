081103c73836ec79a023b6222a17d2c9e9a93662
Gal Lalouche <lalouche@google.com>
2016-09-22 11:48:07 +0300

    Moved canonization tests to helper

--- test/scala/backend/external/expansions/AllMusicHelperTest.scala
+++ test/scala/backend/external/expansions/AllMusicHelperTest.scala
@@ -25,4 +25,32 @@ class AllMusicHelperTest extends FreeSpec with AuxSpecs {
       }
     }
   }
+  "canonize" - {
+    "mw link" in {
+      $.canonize(ExternalLink(Url("http://www.allmusic.com/album/born-in-the-usa-mw0000191830"), Host.Wikipedia))
+          .get.link.address shouldReturn "http://www.allmusic.com/album/born-in-the-usa-mw0000191830"
+    }
+    "rlink" - {
+      def withRedirection(source: String, destination: String) = {
+        def redirect(source: String, destination: String)(http: HttpURLConnection) = new FakeHttpURLConnection(http) {
+          assert(http.getURL.toString.equals(source) && !http.getInstanceFollowRedirects)
+          override def getResponseCode: Int = HttpURLConnection.HTTP_MOVED_PERM
+          override def getHeaderField(s: String): String =
+            if (s == "location") destination else throw new AssertionError()
+        }
+        implicit val config = this.config.copy(_httpTransformer = redirect(source, destination))
+        new AllMusicHelper
+      }
+      "regular" in {
+        withRedirection("http://www.allmusic.com/album/r827504", "http://www.allmusic.com/album/home-mw0000533017")
+            .canonize(ExternalLink(Url("http://www.allmusic.com/album/r827504"), Host.Wikipedia))
+            .get.link.address shouldReturn "http://www.allmusic.com/album/home-mw0000533017"
+      }
+      "without www" in {
+        withRedirection("http://www.allmusic.com/album/ghost-r2202519", "http://www.allmusic.com/album/ghost-mw0002150605")
+            .canonize(ExternalLink(Url("http://www.allmusic.com/album/ghost-r2202519"), Host.Wikipedia))
+            .get.link.address shouldReturn "http://www.allmusic.com/album/ghost-mw0002150605"
+      }
+    }
+  }
 }
--- test/scala/backend/external/expansions/WikipediaAlbumExternalLinksExpanderTest.scala
+++ test/scala/backend/external/expansions/WikipediaAlbumExternalLinksExpanderTest.scala
@@ -7,12 +7,12 @@ import backend.configs.TestConfiguration
 import backend.external.{ExternalLink, FakeHttpURLConnection, Host}
 import common.AuxSpecs
 import common.rich.RichFuture._
+import common.rich.RichT._
 import common.rich.collections.RichTraversableOnce._
 import common.rich.path.RichFile._
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.scalatest.FreeSpec
-import common.rich.RichT._
 
 class WikipediaAlbumExternalLinksExpanderTest extends FreeSpec with AuxSpecs {
   private implicit val config = TestConfiguration().copy(_documentDownloader = u => getDocument(u.address))
@@ -30,35 +30,7 @@ class WikipediaAlbumExternalLinksExpanderTest extends FreeSpec with AuxSpecs {
   "extract allmusic link" in {
     getAllMusicLinkAddress("allmusic_link.html") shouldReturn "http://www.allmusic.com/album/born-in-the-usa-mw0000191830"
   }
-  "canonize allmusic links" - {
-    "mw link" in {
-      $(ExternalLink(Url("allmusic_link.html"), Host.Wikipedia))
-          .get.single.link.address shouldReturn "http://www.allmusic.com/album/born-in-the-usa-mw0000191830"
-    }
-    "rlink" - {
-      def withRedirection(source: String, destination: String) = {
-        def redirect(source: String, destination: String)(http: HttpURLConnection) = new FakeHttpURLConnection(http) {
-          assert(http.getURL.toString.equals(source) && !http.getInstanceFollowRedirects)
-          override def getResponseCode: Int = HttpURLConnection.HTTP_MOVED_PERM
-          override def getHeaderField(s: String): String =
-            if (s == "location") destination else throw new AssertionError()
-        }
-        implicit val config = this.config.copy(_httpTransformer = redirect(source, destination))
-        new WikipediaAlbumExternalLinksExpander()
-      }
-      "regular" in {
-        withRedirection("http://www.allmusic.com/album/r827504", "http://www.allmusic.com/album/home-mw0000533017")
-            .apply(ExternalLink(Url("allmusic_rlink.html"), Host.Wikipedia))
-            .get.single.link.address shouldReturn "http://www.allmusic.com/album/home-mw0000533017"
-      }
-      "without www" in {
-        withRedirection("http://www.allmusic.com/album/ghost-r2202519", "http://www.allmusic.com/album/ghost-mw0002150605")
-            .apply(ExternalLink(Url("allmusic_rlink2.html"), Host.Wikipedia))
-            .get.single.link.address shouldReturn "http://www.allmusic.com/album/ghost-mw0002150605"
-      }
-    }
-  }
-  "Return nothing if response code isn't 301 or 200" in {
+  "Return nothing on error" in {
     implicit val config = this.config.copy(_httpTransformer = new FakeHttpURLConnection(_) {
       override def getResponseCode: Int = HttpURLConnection.HTTP_INTERNAL_ERROR
       override def getHeaderField(s: String): String = throw new AssertionError() // makes sure it isn't called
