9aa0e67c92fd40966183a7a8c5d94e50ac790004
Gal Lalouche <lalouche@google.com>
2016-09-23 19:23:47 +0300

    added clone to routes

--- app/controllers/Playlist.scala
+++ app/controllers/Playlist.scala
@@ -1,12 +1,16 @@
 package controllers
 
 import common.io.JsonableSaver
+import common.RichJson._
 import common.rich.RichT._
 import play.api.libs.json.{JsArray, Json}
 import play.api.mvc.{Action, AnyContent, Controller}
 import playlist.PlaylistQueue
 import playlist.PlaylistQueue._
 import models.Song
+import playlist.PlaylistClone
+import playlist.PlaylistClone.PlaylistCloneJsonable
+import scala.concurrent.duration.DurationInt
 
 object Playlist extends Controller {
   private implicit val c = PlayConfig
@@ -14,14 +18,25 @@ object Playlist extends Controller {
   private val saver = new JsonableSaver
   private def getStringFromBody(a: AnyContent): String = a.asFormUrlEncoded.get.keys.head
   private def arrayOfPathsToSong(a: JsArray): Seq[Song] = a.value.map(_.as[String]).map(Utils.parseSong)
-  def get = Action {
-    Ok(saver.loadObject.songs map Player.toJson mapTo JsArray.apply)
+  def getQueue = Action {
+    Ok(saver.loadObject[PlaylistQueue].songs map Player.toJson mapTo JsArray.apply)
   }
-  def set = Action { request =>
+  def setQueue = Action { request =>
     val playlist = request.body
         .mapTo(_ |> getStringFromBody |> Json.parse)
         .mapTo(_.as[JsArray] |> arrayOfPathsToSong |> PlaylistQueue.apply)
     saver save playlist
     Created.withHeaders("Location" -> "playlist")
+  }
+  def getClone = Action {
+    Ok(saver.loadObject[PlaylistClone] |> PlaylistCloneJsonable.jsonify)
+  }
+  def setClone = Action { request =>
+    val json = request.body |> getStringFromBody |> Json.parse
+    val songs = json array "songs" mapTo arrayOfPathsToSong
+    val duration: Int = json / "duration"
+    val index: Int = json / "index"
+    saver save PlaylistClone(songs, index, duration.seconds)
+    Created.withHeaders("Location" -> ???)
   }
 }
