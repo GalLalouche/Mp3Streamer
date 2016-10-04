519adf523274af3193969d5d357b6e226a7e5cbe
gal_lalouche <glpkmtg@gmail.com>
2016-10-04 16:59:00 +0300

    Added Jazz folder

--- app/models/RealLocations.scala
+++ app/models/RealLocations.scala
@@ -6,7 +6,7 @@ import common.rich.path.Directory
 /** Can be extended to override its values in tests */
 class RealLocations extends MusicFinder {
 	override val dir = IODirectory("d:/media/music")
-	override val subDirs = List("Rock", "New Age", "Classical", "Metal")
+	override val subDirs = List("Rock", "New Age", "Classical", "Metal", "Jazz")
 	override val extensions = List("mp3", "flac")
 	override def genreDirs: Seq[IODirectory] = super.genreDirs.asInstanceOf[Seq[IODirectory]]
 	def getSongFilePathsInDir(d: Directory): Seq[String] = super.getSongFilePathsInDir(new IODirectory(d))
