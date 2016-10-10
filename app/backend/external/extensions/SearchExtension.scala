cbf5a966dd8fec32972cbe32fba90cdcf055b3ae
Gal Lalouche <lalouche@google.com>
2016-10-10 12:02:26 +0300

    Added tests to SearchExtension
    Also, remove URL encoding (that would be handled by the browser anyway).

--- app/backend/external/extensions/SearchExtension.scala
+++ app/backend/external/extensions/SearchExtension.scala
@@ -1,7 +1,5 @@
 package backend.external.extensions
 
-import java.net.URLEncoder
-
 import backend.Url
 import backend.external.{ExtendedLinks, Host}
 import backend.recon.Reconcilable
@@ -11,7 +9,7 @@ import common.rich.collections.RichSet._
 object SearchExtension {
   def apply[R <: Reconcilable](h: Host, r: R): ExtendedLink[R] =
     ExtendedLink(Url("javascript:void(0)"), h.copy(name = h.name + "?"), List(LinkExtension("Google",
-      s"http://www.google.co.il/search?q=${URLEncoder.encode(s"${r.normalize} ${h.name}", "UTF-8")}" |> Url)))
+      s"http://www.google.com/search?q=${r.normalize} ${h.name}" |> Url)))
 
   def extendMissing[R <: Reconcilable](allHosts: TraversableOnce[Host], r: R, links: ExtendedLinks[R]): ExtendedLinks[R] =
     links ++ (allHosts.toSet \ links.map(_.host.canonize).toSet map (SearchExtension(_, r)))
