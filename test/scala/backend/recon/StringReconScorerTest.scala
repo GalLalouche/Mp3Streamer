2430815c174b38f5fb50a4c9200b4d5c67cbb969
Gal Lalouche <lalouche@google.com>
2016-10-20 13:45:43 +0300

    Removed "and" from recon scoring
    Avoid the amguity of & vs. "and". Also some refactoring.

--- test/scala/backend/recon/StringReconScorerTest.scala
+++ test/scala/backend/recon/StringReconScorerTest.scala
@@ -4,8 +4,20 @@ import common.AuxSpecs
 import org.scalatest.FreeSpec
 
 class StringReconScorerTest extends FreeSpec with AuxSpecs {
-  val $ = StringReconScorer
-  "ascii" in {
-    $("Verisäkeet", "Verisakeet") should be > 0.9
+  private val $ = StringReconScorer
+  def verifyHighReconScore(str1: String, str2: String): Unit = {
+    s"$str1 and $str2 should have a high recon score" in {
+      $(str1,str2) should be > 0.9
+    }
+  }
+  "ascii" - {
+    verifyHighReconScore("Verisäkeet", "Verisakeet")
+  }
+  "ignores non-letters or digits" - {
+    verifyHighReconScore("f,o.o-b=a&r", "foo bar")
+    verifyHighReconScore("1984", "1+9_8*4")
+  }
+  "ignores and" - {
+    verifyHighReconScore("Songs of Moors & Misty fields", "Songs of moors and misty fields")
   }
 }
