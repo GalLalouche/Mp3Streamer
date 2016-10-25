4708f8b2ec7212816b348b9617573f6f1aec0f72
Gal Lalouche <lalouche@google.com>
2016-10-25 15:59:52 +0300

    some small StringFixer fixes

--- test/scala/mains/fixer/StringFixerTest.scala
+++ test/scala/mains/fixer/StringFixerTest.scala
@@ -5,28 +5,48 @@ import org.scalatest.FreeSpec
 
 class StringFixerTest extends FreeSpec with AuxSpecs {
   private val $ = StringFixer
+
   private def verifyFix(original: String, fix: String) {
-    s"$original should equal $fix" in {
-      $(original) shouldReturn fix
-    }
+    $(original) shouldReturn fix
   }
 
-  "one word" - {
-    verifyFix("1St", "1st")
-    verifyFix("2Nd", "2nd")
-    verifyFix("3Rd", "3rd")
+  "Ordered numerals" in {
+    verifyFix("1st", "1st")
+    verifyFix("2nd", "2nd")
+    verifyFix("3rd", "3rd")
     verifyFix("4th", "4th")
+    verifyFix("27th", "27th")
+    verifyFix("101st", "101st")
   }
-  "capitalize" - {
-    List("foobar", "FOOBAR", "Foobar", "FoOBaR", "fOObAr").foreach(verifyFix(_, "Foobar"))
+  "I and a" in {
+    verifyFix("a foo", "A Foo")
+    verifyFix("foo a", "Foo a")
+    verifyFix("i foo", "I Foo")
+    verifyFix("foo i", "Foo I")
   }
-  "sentence" - {
-    verifyFix("I Am A Rock", "I am a Rock")
+  "Mixed caps" in {
+    List("FOO", "FoO", "fOo").foreach(e => verifyFix(e, e))
   }
-  "ordered" in {
+  "Capitalize" - {
+    List("foobar", "Foobar").foreach(verifyFix(_, "Foobar"))
+  }
+  "Sentence" in {
+    verifyFix("i am a rock", "I am a Rock")
+    verifyFix("a rock am i", "A Rock am I")
+  }
+  "Is sorted" in {
     $.lowerCaseWords.sorted shouldReturn $.lowerCaseWords
   }
-  "asciify" - {
+  "With dots" in {
+    List("F.F.S.", "f.f.s.").foreach(verifyFix(_, "F.F.S."))
+  }
+  "Roman numerals" in {
+    verifyFix("mmxvi", "MMXVI")
+  }
+  "Asciify" in {
     verifyFix("Köyliönjärven jäällä", "Koylionjarven Jaalla")
+  }
+  "with delimiters" - {
+    verifyFix("foo/bar", "Foo/Bar")
   }
 }
