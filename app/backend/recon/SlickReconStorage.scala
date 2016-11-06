a45a6e06955bb7f7b4f66bb3990ef5b0ebd2616b
Gal Lalouche <lalouche@google.com>
2016-11-06 13:40:46 +0200

    AlbumReconStorage can delete all albums by an artist

--- app/backend/recon/SlickReconStorage.scala
+++ app/backend/recon/SlickReconStorage.scala
@@ -13,7 +13,7 @@ abstract class SlickReconStorage[K <: Reconcilable](implicit c: Configuration,
     with ToFunctorOps with FutureInstances {
   import c.driver.api._
 
-  private class Rows(tag: Tag) extends Table[(String, Option[String], Boolean)](tag, m.runtimeClass.getSimpleName.replaceAll("\\$", "") + "S") {
+  private class Rows(tag: Tag) extends Table[(String, Option[String], Boolean)](tag, _tableName = m.runtimeClass.getSimpleName.replaceAll("\\$", "") + "S") {
     def name = column[String]("KEY", O.PrimaryKey)
     def reconId = column[Option[String]]("RECON_ID")
     def isIgnored = column[Boolean]("IS_IGNORED", O.Default(false))
@@ -22,7 +22,7 @@ abstract class SlickReconStorage[K <: Reconcilable](implicit c: Configuration,
   private val rows = TableQuery[Rows]
   private val db = c.db
   def store(k: K, id: Option[String]): Future[Boolean] =
-    store(k, id.map(ReconID.apply) -> (false == id.isDefined))
+    store(k, id.map(ReconID.apply) -> id.isEmpty)
   /** If an existing value exists, override it. */
   override protected def internalForceStore(k: K, value: (Option[ReconID], Boolean)): Future[Unit] =
   db.run(rows.insertOrUpdate(k.normalize, value._1.map(_.id), value._2)) >| Unit
@@ -39,4 +39,36 @@ abstract class SlickReconStorage[K <: Reconcilable](implicit c: Configuration,
 }
 
 class ArtistReconStorage(implicit c: Configuration) extends SlickReconStorage[Artist]
-class AlbumReconStorage(implicit c: Configuration) extends SlickReconStorage[Album]
+class AlbumReconStorage(implicit c: Configuration) extends ReconStorage[Album] {
+  import c.driver.api._
+  private class Rows(tag: Tag) extends Table[(String, String, Option[String], Boolean)](tag, "ALBUMS") {
+    def album = column[String]("ALBUM", O.PrimaryKey)
+    def artist = column[String]("ARTIST")
+    def reconId = column[Option[String]]("RECON_ID")
+    def isIgnored = column[Boolean]("IS_IGNORED", O.Default(false))
+    def artistIndex = index("ARTIST_INDEX", artist)
+    def * = (album, artist, reconId, isIgnored)
+  }
+  private val rows = TableQuery[Rows]
+  private val db = c.db
+  override protected def internalForceStore(a: Album, value: (Option[ReconID], Boolean)): Future[Unit] =
+    db.run(rows.insertOrUpdate(a.normalize, a.artist.normalize, value._1.map(_.id), value._2)) >| Unit
+  override def load(a: Album): Future[Option[(Option[ReconID], Boolean)]] =
+    db.run(rows
+        .filter(_.album === a.normalize)
+        .map(e => e.isIgnored -> e.reconId)
+        .result
+        .map(_.headOption.map(_.swap.mapTo(e => e._1.map(ReconID) -> e._2))))
+  override def internalDelete(a: Album): Future[Unit] =
+    db.run(rows.filter(_.album === a.normalize).delete).>|(Unit)
+  override def utils = SlickStorageUtils(c)(rows)
+  /** Delete all recons for albums for that artist */
+  def deleteAllRecons(a: Artist): Future[Traversable[(String, Option[ReconID], Boolean)]] = {
+    val artistRows = rows.filter(_.artist === a.normalize)
+    for (existingRows <- db.run(artistRows
+        .map(e => (e.album, e.reconId, e.isIgnored))
+        .result
+        .map(_.map(_.mapTo(e => (e._1, e._2.map(ReconID), e._3)))));
+         _ <- db.run(artistRows.delete)) yield existingRows
+  }
+}
