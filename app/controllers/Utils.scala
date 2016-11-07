dc6b0ee871876e3b3743c9ee2c9f78ebb1717d65
gal_lalouche <glpkmtg@gmail.com>
2016-11-07 20:06:12 +0200

    "file" attribute in song JSON is URL encoded
    Since apparently jquery doesn't do it automatically,
    and songs with "&" in their paths get mangled

--- app/controllers/Utils.scala
+++ app/controllers/Utils.scala
@@ -10,6 +10,7 @@ import models.{Poster, Song}
 import play.api.libs.json.{JsObject, JsString}
 import play.api.mvc.AnyContent
 import search.ModelsJsonable.SongJsonifier
+import common.RichJson._
 
 import scala.concurrent.ExecutionContext
 
@@ -19,9 +20,13 @@ private object Utils {
     override implicit val logger: Logger =
       new CompositeLogger(new ConsoleLogger with FilteringLogger, new DirectoryLogger(rootDirectory addSubDir "log"))
   }
-  def toJson(s: Song): JsObject = SongJsonifier.jsonify(s) +
-      ("poster" -> JsString("/posters/" + Poster.getCoverArt(s).path)) +
-      (s.file.extension -> JsString("/stream/download/" + URLEncoder.encode(s.file.path, "UTF-8")))
+  def toJson(s: Song): JsObject = {
+    // TODO lenses?
+    val $ = SongJsonifier.jsonify(s)
+    $ + ("file" -> URLEncoder.encode($.str("file"), "UTF-8")) +
+        ("poster" -> JsString("/posters/" + Poster.getCoverArt(s).path)) +
+        (s.file.extension -> JsString("/stream/download/" + URLEncoder.encode(s.file.path, "UTF-8")))
+  }
   def parseSong(path: String): Song = Song(parseFile(path))
   def parseFile(path: String): File = new File(URLDecoder.decode(path, "UTF-8"))
 
