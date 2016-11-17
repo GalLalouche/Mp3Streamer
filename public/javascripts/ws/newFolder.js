c1987f19799648b35f63c1856cd205ed517221e8
gal_lalouche <glpkmtg@gmail.com>
2016-11-17 20:16:41 +0200

    Removed newFolder socket

--- public/javascripts/ws/newFolder.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	openConnection("newFolder", function(msg) {
-		// in case pop blocker is disabled, fuck that and just add the folder
-		if (!window.confirm || window.confirm("Found new folder(" + msg.data + "), would you like to add it to the playlist?"))
-			$.get("/data/albums/" + msg.data, function(data) {
-				playlist.add(data, false);
-			});
-	});
-});
