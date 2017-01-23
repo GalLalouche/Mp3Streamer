fcfe2348419d91e709495584c462900548e9cda1
Gal Lalouche <lalouche@google.com>
2017-01-23 10:24:45 +0200

    Ignoring and removing artists and albums ServerSide.

--- app/controllers/Albums.scala
+++ app/controllers/Albums.scala
@@ -7,19 +7,45 @@ import mains.albums.NewAlbums
 import play.api.libs.json._
 import play.api.mvc._
 
+import scala.concurrent.Future
+import common.RichJson._
+
+import scalaz.std.FutureInstances
+import scalaz.syntax.ToFunctorOps
+
 /** Handles fetch requests of JSON information, and listens to directory changes. */
-object Albums extends Controller with Debug {
+object Albums extends Controller with Debug
+    with FutureInstances with ToFunctorOps {
   import Utils.config
   private val newAlbums = new NewAlbums()
+
   private def toJson(a: Album): JsObject =
     Json.obj("title" -> a.title, "year" -> a.year)
   private def toJson(a: Artist, albums: Seq[Album]): JsObject =
     Json.obj("artistName" -> a.name, "albums" -> JsArray(albums map toJson))
   private def toJson(m: Map[Artist, Seq[Album]]): JsArray =
     m.map(e => toJson(e._1, e._2)).toSeq |> JsArray.apply
+
   def albums = Action.async {
     newAlbums.load.map(toJson).map(Ok(_))
   }
+
+  private def updateNewAlbums[A](extractor: Request[AnyContent] => A,
+                                 action: A => Future[Unit]) = Action.async { request =>
+    action(extractor(request)).>|(NoContent)
+  }
+
+  private def extractArtist(request: Request[AnyContent]): Artist =
+    request.body |> Utils.getStringFromBody |> Artist
+
+  def removeArtist = updateNewAlbums(extractArtist, newAlbums.removeArtist)
+  def ignoreArtist = updateNewAlbums(extractArtist, newAlbums.ignoreArtist)
+  private def extractAlbum(request: Request[AnyContent]): Album = {
+    val json = request.body |> Utils.getStringFromBody |> Json.parse
+    Album(json str "title", json int "year", Artist(json str "artistName"))
+  }
+  def removeAlbum = updateNewAlbums(extractAlbum, newAlbums.removeAlbum)
+  def ignoreAlbum = updateNewAlbums(extractAlbum, newAlbums.ignoreAlbum)
 
   def index = Action {
     Ok(views.html.new_albums())
--- app/mains/albums/NewAlbums.scala
+++ app/mains/albums/NewAlbums.scala
@@ -7,6 +7,7 @@ import common.Jsonable
 import common.RichJson._
 import common.io.JsonableSaver
 import common.rich.RichFuture._
+import common.ds.RichMap._
 import common.rich.RichT._
 import mains.fixer.StringFixer
 import models.RealLocations
@@ -40,13 +41,44 @@ object NewAlbums {
 }
 class NewAlbums(implicit c: Configuration)
     extends ToBindOps with FutureInstances {
-  import NewAlbums._
   import c._
+  import NewAlbums._
+  private val logger = c.logger
+
+  private val artistReconStorage = new ArtistReconStorage()
+  private val albumReconStorage = new AlbumReconStorage()
+
   private val jsonableSaver = new JsonableSaver() {
     override protected def jsonFileName[T: Manifest] = {
-      s"New${implicitly[Manifest[T]].runtimeClass.getSimpleName.replaceAll("\\$", "") }s.json"
+      s"New${implicitly[Manifest[T]].runtimeClass.getSimpleName.replaceAll("\\$", "")}s.json"
     }
   }
+
+  private def save(m: Map[Artist, Seq[Album]]): Unit = {
+    jsonableSaver save m.flatMap(_._2)
+  }
+
+  private def ignore[R <: Reconcilable](r: R, reconStorage: ReconStorage[R]): Future[Unit] = {
+    logger.debug(s"Ignoring $r")
+    reconStorage.load(r).map { existing =>
+      assert(existing.isDefined)
+      val existingData = existing.get
+      assert(existingData._1.isDefined)
+      reconStorage.store(r, existingData._1 -> true)
+    }
+  }
+
+  def removeArtist(a: Artist): Future[Unit] = {
+    logger.debug(s"Removing $a")
+    load.map(_ - a).map(save)
+  }
+  def ignoreArtist(a: Artist): Future[Unit] = ignore(a, artistReconStorage) >> removeArtist(a)
+  def removeAlbum(a: Album): Future[Unit] = {
+    logger.debug(s"Removing $a")
+    load.map(_.modified(a.artist, _.filterNot(_ == a))).map(save)
+  }
+  def ignoreAlbum(a: Album): Future[Unit] = ignore(a, albumReconStorage) >> removeAlbum(a)
+
   private val retriever = new NewAlbumsRetriever(
     new ReconcilerCacher(new ArtistReconStorage(), new MbArtistReconciler()), new RealLocations {
       override val subDirs: List[String] = List("Rock", "Metal")
@@ -54,12 +86,13 @@ class NewAlbums(implicit c: Configuration)
   def fetchAndSave: Future[Traversable[Album]] = {
     val $ = retriever.findNewAlbums |> toFuture
     // TODO add consume method to RichFuture
-    $.map(e => {jsonableSaver save e; e })
+    $.map(e => {
+      jsonableSaver save e
+      e
+    })
   }
-  def load: Future[Map[Artist, Seq[Album]]] = Future(jsonableSaver.loadArray).map { as =>
-    as
-        .map(e => e.copy(artist = Artist(StringFixer(e.artist.name))))
-        .groupBy(_.artist)
-        .mapValues(_.sortBy(_.year))
-  }
+  def load: Future[Map[Artist, Seq[Album]]] = Future(jsonableSaver.loadArray)
+      .map(_.map(e => e.copy(artist = Artist(StringFixer(e.artist.name))))
+          .groupBy(_.artist)
+          .mapValues(_.sortBy(_.year)))
 }
