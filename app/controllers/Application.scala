c70ecf618691dd21b5aaba95706bf7e1b43888ca
Gal PC <glpkmtg@gmail.com>
2017-03-09 11:59:20 +0200

    Revert "Remove old controllers"
    This reverts commit 6bbf7e60cc24388fb3bb5d9c76257e20a1110ce1.
    
    Stop deleting Application you twat!

---
+++ app/controllers/Application.scala
@@ -0,0 +1,29 @@
+package controllers
+
+import java.io.File
+
+import play.api.mvc._
+
+object Application extends Controller {
+  def index(mute: Boolean) = Action {
+    Ok(views.html.main(mute))
+  }
+  def song = Action {
+    val file = new File("C:/dev/web/play-2.1.2/Mp3Streamer/public/resources/songs/13 Wonderwall.mp3")
+    Status(200).sendFile(file).withHeaders(("Content-length", file.length.toString), ("Accept-Ranges", "bytes"),
+      ("X-Pad", "avoid browser bug"), ("Content-Transfer-Encoding", "binary"), ("Cache-Control", "no-cache"),
+      ("Content-Disposition", "attachment; filename=" + file.getName.replaceAll(",", "%2c")),
+      ("Content-Range", "byte %d/%d".format(file.length, file.length)), ("Content", "audio/mp3"))
+  }
+
+  def sjs() = Action {
+    val f: File = new File("C:/dev/web/Mp3Streamer/client/player.html")
+    Ok(scala.io.Source.fromFile(f.getCanonicalPath).mkString).as("text/html")
+  }
+
+  def sjsClient(path: String) = Action {
+    val f: File = new File("C:/dev/web/Mp3Streamer/client/" + path)
+    Ok(scala.io.Source.fromFile(f.getCanonicalPath).mkString).as("text/script")
+  }
+
+}
