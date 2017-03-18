ee468699bfeb1ce618726eaadeb980e3dd8b55f3
gal_lalouche <glpkmtg@gmail.com>
2017-03-18 16:08:32 +0200

    Manual image search uses new square ratio feature

--- app/mains/cover/DownloadCover.scala
+++ app/mains/cover/DownloadCover.scala
@@ -1,5 +1,6 @@
 package mains.cover
 
+import java.net.URLEncoder
 import java.nio.file.Files
 
 import backend.configs.StandaloneConfig
@@ -28,25 +29,30 @@ object DownloadCover {
    * @param albumDir Should contain the songs. The songs' metadata will be used to search for the correct picture.
    * @return A future command to move the downloaded file to the directory, and delete all temporary files
    */
-  def apply(albumDir: Directory): Future[Directory => Unit] = getSearchUrlFor(albumDir) |> { searchUrl =>
-    downloader.download(searchUrl, "UTF-8")
-      .map(new String(_, "UTF-8"))
-      .map(extractImageURLs)
-      .flatMap(selectImage)
-      .map {
-        case Selected(img) => fileMover(img)
-        case OpenBrowser =>
-          Process( """C:\Users\Gal\AppData\Local\Google\Chrome\Application\chrome.exe " """" + searchUrl + "\"").!!
-          throw new RuntimeException("User opened browser")
-        case Cancelled => throw new RuntimeException("User opted out")
-      }
+  def apply(albumDir: Directory): Future[Directory => Unit] = new SearchUrlProvider(albumDir) |> {urlProvider =>
+    downloader.download(urlProvider.automaticSearchUrl, "UTF-8")
+        .map(new String(_, "UTF-8"))
+        .map(extractImageURLs)
+        .flatMap(selectImage)
+        .map {
+          case Selected(img) => fileMover(img)
+          case OpenBrowser =>
+            // String interpolation is acting funky for some reason (will fail at runtime)
+            Process("""C:\Users\Gal\AppData\Local\Google\Chrome\Application\chrome.exe "%s"""".format(urlProvider.manualSearchUrl)).!!
+            throw new RuntimeException("User opened browser")
+          case Cancelled => throw new RuntimeException("User opted out")
+        }
   }
 
-  // constructs the search URL using the songs' metadata
-  private def getSearchUrlFor(albumDir: Directory) = {
-    val query = Song(albumDir.files.filter(e => e.extension == "mp3" || e.extension == "flac").head)
-      .mapTo(song => s"${song.artistName} ${song.albumName}")
-    s"https://www.google.com/search?tbs=isz%3Aex%2Ciszw%3A500%2Ciszh%3A500&tbm=isch&q=$query".replaceAll(" ", "%20")
+  private class SearchUrlProvider(albumDir: Directory) {
+    private val album = Song(albumDir.files.filter(e => e.extension == "mp3" || e.extension == "flac").head).album
+    private val query = URLEncoder.encode(s"${album.artistName} ${album.title}", "UTF-8")
+    // tbm=isch => image search
+    private val prefix = s"https://www.google.com/search?tbm=isch&q=$query"
+    // tbs=isz%3Aex%2Ciszw%3A500%2Ciszh%3A500 => required image size is 500x500
+    val automaticSearchUrl: String = prefix + "&tbs=isz%3Aex%2Ciszw%3A500%2Ciszh%3A500"
+    // tbs=iar:s => relax the automatic requirement, and search for sqaure images
+    val manualSearchUrl: String = prefix + "&tbs=iar:s"
   }
 
   private def fileMover(f: FolderImage)(outputDirectory: Directory) {
@@ -60,9 +66,9 @@ object DownloadCover {
 
   private def extractImageURLs(html: String): Seq[String] =
     """"ou":"[^"]+"""".r
-      .findAllIn(html) // assumes format "ou":"<url>". fucking closure :\
-      .map(_.dropWhile(_ != ':').drop(2).dropRight(1))
-      .toVector
+        .findAllIn(html) // assumes format "ou":"<url>". fucking closure :\
+        .map(_.dropWhile(_ != ':').drop(2).dropRight(1))
+        .toVector
 
   private def selectImage(imageURLs: Seq[String]): Future[ImageChoice] =
     ImageSelectionPanel(ImagesSupplier.withCache(imageURLs.iterator,
@@ -73,7 +79,7 @@ object DownloadCover {
     val path = if (args.nonEmpty)
       args(0)
     else
-      return
+      """D:\Media\Music\Rock\Progressive Rock\Mostly Autumn\2012 The Ghost Moon Orchestra"""
     val folder = Directory(path)
     println("Downloading cover image for " + path)
     Await.result(apply(folder), Duration.Inf)(folder)
