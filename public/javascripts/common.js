35d535693f6dc2d233a261dbcba4583f6f7b5879
gal_lalouche <glpkmtg@gmail.com>
2017-05-17 20:12:00 +0300

    Even more permissive url parser

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -99,9 +99,7 @@ function isValidUrl(urlString) {
   const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
       '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+\\:]*)*'+ // port and path
-      '(\\?[\\;&a-z\\d%_.~+=-]*)?'+ // query string
-      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+      '(\\:\\d+)?.*') // port and path
   return pattern.test(urlString)
 }
 
