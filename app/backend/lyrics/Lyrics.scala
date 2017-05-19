10510d20521fcdc87112102997872cd915eded98
gal_lalouche <glpkmtg@gmail.com>
2017-05-19 13:16:17 +0300

    Simplification of the lyrics sealed trait

--- app/backend/lyrics/Lyrics.scala
+++ app/backend/lyrics/Lyrics.scala
@@ -1,13 +1,13 @@
 package backend.lyrics
 
-sealed abstract class Lyrics {
-  val html: String
-  val source: String
+sealed trait Lyrics {
+  def html: String
+  def source: String
 }
 case class HtmlLyrics(source: String, html: String) extends Lyrics {
-  override def toString: String = "(From " + source + ")\n" + html
+  override def toString: String = s"(From $source)\n$html"
 }
 case class Instrumental(source: String) extends Lyrics {
   override val html = "<img src='assets/images/TrebleClef.png' width='30' height='68' /><b>Instrumental</b>"
-  override def toString: String = "(From " + source + ")\n" + "Instrumental"
+  override def toString: String = s"(From $source) Instrumental"
 }
