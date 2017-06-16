0f554b72c7e344821d89c206f57fac2832175097
gal_lalouche <glpkmtg@gmail.com>
2017-06-16 14:20:48 +0300

    Removed "/" methods from PathRef

--- app/common/io/FileRef.scala
+++ app/common/io/FileRef.scala
@@ -6,21 +6,17 @@ import common.rich.RichT._
 /** Either a file or a dir */
 abstract class PathRef {
   def path: String
-  def /(name: String): PathRef
-  def /(): DirectoryRef
   def name: String
   override def toString: String = s"${this.simpleName}: $name"
 }
 
 /** a place holder for a directory */
 private final class TempRef(dir: DirectoryRef) extends PathRef {
-  override def /(name: String): DirectoryRef =
   throw new UnsupportedOperationException(s"This instance is only a placeholder for dir <$dir>")
   override def path: String =
     throw new UnsupportedOperationException(s"This instance is only a placeholder for dir <$dir>")
   override def name: String =
     throw new UnsupportedOperationException(s"This instance is only a placeholder for dir <$dir>")
-  override def /(): DirectoryRef = dir
 }
 
 /** must exist */
@@ -35,12 +31,10 @@ trait FileRef extends PathRef {
     if (content.isEmpty) Nil
     else content split "\n"
   }
-  final def extension = {
+  final def extension: String = {
     val i = name.lastIndexOf('.')
     if (i == -1) "" else name.substring(i + 1).toLowerCase
   }
-  def /(name: String) = throw new UnsupportedOperationException(s"file <$path> is not a directory")
-  def /() = throw new UnsupportedOperationException(s"file <$path> is not a directory")
 
   def lastModified: LocalDateTime
 }
@@ -50,13 +44,11 @@ trait DirectoryRef extends PathRef {
   def getFile(name: String): Option[FileRef]
   def addSubDir(name: String): DirectoryRef
   def getDir(name: String): Option[DirectoryRef]
-  def /(name: String): PathRef = getFile(name).orElse(getDir(name).map(new TempRef(_))).get
   /** Files and dirs */
   def paths: Seq[PathRef]
   def dirs: Seq[DirectoryRef] = paths collect { case e: DirectoryRef => e }
   def files: Seq[FileRef] = paths collect { case e: FileRef => e }
   def deepDirs: Seq[DirectoryRef] = dirs ++ (dirs flatMap (_.deepDirs))
   def deepFiles: Seq[FileRef] = files ++ (dirs flatMap (_.deepFiles))
-  override def /(): DirectoryRef = this
   def lastModified: LocalDateTime
 }
