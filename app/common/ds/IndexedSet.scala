c846164174403f98069d41538ae7389a3c53bf26
gal_lalouche <glpkmtg@gmail.com>
2017-06-23 16:25:25 +0300

    Refactored IndexedSet to a trait
    Its implementation is now a proper parameterized class, instead of having a
    type parameter.

--- app/common/ds/IndexedSet.scala
+++ app/common/ds/IndexedSet.scala
@@ -2,29 +2,25 @@ package common.ds
 
 import scalaz.Semigroup
 
+/** Sums values whose key function is equal. */
+trait IndexedSet[T] extends Traversable[T] {
+  def +(v: T): IndexedSet[T]
+  def ++(vs: TraversableOnce[T]): IndexedSet[T]
+  override def foreach[U](f: T => U)
+}
 
-abstract class IndexedSet[Value : Semigroup] extends Traversable[Value] {
-  type Key
-  protected val index: Value => Key
-  protected val map: Map[Key, Value] // can't be a constructor object since it depends on the type
-  private def withMap(map: Map[Key, Value]): IndexedSet[Value] = IndexedSet.withMap(index, map)
+private class IndexedSetImpl[Value: Semigroup, Key](map: Map[Key, Value], index: Value => Key) extends IndexedSet[Value] {
   def +(v: Value): IndexedSet[Value] = {
     val key = index(v)
     val newV = map.get(key).map(implicitly[Semigroup[Value]].append(_, v)).getOrElse(v)
-    require(key == index(newV), s"Inconsistent keys for $v ($key) and new $newV (${index(newV) })")
-    withMap(map + ((key, newV)))
+    assert(key == index(newV), s"Inconsistent keys for $v ($key) and new $newV (${index(newV)})")
+    new IndexedSetImpl(map.updated(key, newV), index)
   }
-  def ++(vs: TraversableOnce[Value]): IndexedSet[Value] = vs.foldLeft(this)(_ + _)
-  override def foreach[U](f: Value => U) { map.values foreach f }
+  def ++(vs: TraversableOnce[Value]): IndexedSet[Value] = vs.foldLeft(this: IndexedSet[Value])(_ + _)
+  override def foreach[U](f: Value => U) {map.values foreach f}
 }
 
 object IndexedSet {
-  private def withMap[K, V : Semigroup](_index: V => K, _map: Map[K, V]): IndexedSet[V] =
-    new IndexedSet[V] {
-      override type Key = K
-      override protected val index = _index
-      override protected val map = _map
-    }
-  def apply[K, V : Semigroup](index: V => K): IndexedSet[V] = withMap(index, Map())
+  def apply[K, V: Semigroup](index: V => K): IndexedSet[V] = new IndexedSetImpl(Map(), index)
 }
 
