62be802da2c49f6979ed07b44747a40f5e584bc1
gal_lalouche <glpkmtg@gmail.com>
2017-06-23 19:02:03 +0300

    IndexedSet uses RichMapSemi

--- app/common/ds/IndexedSet.scala
+++ app/common/ds/IndexedSet.scala
@@ -6,15 +6,13 @@ import RichMap._
 /** Sums values whose key function is equal. */
 trait IndexedSet[T] extends Traversable[T] {
   def +(v: T): IndexedSet[T]
-  def ++(vs: TraversableOnce[T]): IndexedSet[T]
-  override def foreach[U](f: T => U)
+  def ++(vs: TraversableOnce[T]): IndexedSet[T] = vs.foldLeft(this)(_ + _)
+  override def foreach[U](f: T => U): Unit
 }
 
 private class IndexedSetImpl[Value: Semigroup, Key](map: Map[Key, Value], index: Value => Key) extends IndexedSet[Value] {
-  def +(v: Value): IndexedSet[Value] =
-    new IndexedSetImpl(map.updateWith(index(v), v, implicitly[Semigroup[Value]].append(_, _)), index)
-  def ++(vs: TraversableOnce[Value]): IndexedSet[Value] = vs.foldLeft(this: IndexedSet[Value])(_ + _)
-  override def foreach[U](f: Value => U) {map.values foreach f}
+  def +(v: Value): IndexedSet[Value] = new IndexedSetImpl(map.updateAppend(index(v), v), index)
+  override def foreach[U](f: Value => U) = map.values foreach f
 }
 
 object IndexedSet {
