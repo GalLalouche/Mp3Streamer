8618b24560b964772f8ae406459288188145ccc9
gal_lalouche <glpkmtg@gmail.com>
2017-06-24 13:19:40 +0300

    WeightedIndexBuilder uses RichMap

--- app/search/WeightedIndexBuilder.scala
+++ app/search/WeightedIndexBuilder.scala
@@ -1,31 +1,32 @@
 package search
 
-import common.ds.Collectable._
-import common.ds.RichMap._
 import common.ds.Trie
 import common.rich.RichT._
+import common.rich.collections.RichTraversableOnce._
 
 import scalaz.Semigroup
+import scalaz.std.SetInstances
 import scalaz.syntax.ToSemigroupOps
 
 /** to allow artist's name to be factored in the song search */
 private object WeightedIndexBuilder
-    extends ToSemigroupOps {
+    extends ToSemigroupOps with SetInstances {
   import WeightedIndexable.ops._
-  // TODO replace with some kind of map builder from RichTraversableOnce
-  def buildIndexFor[T: WeightedIndexable : Indexable](ts: TraversableOnce[T]): Index[T] = ts
-      ./:(Map[String, Set[(T, Double)]]()) {(map, indexable) =>
-        indexable.terms
-            .map(e => e.copy(_1 = e._1.toLowerCase)) // TODO lenses
-            ./:(map)((map, weightedTerm) => map.append(weightedTerm._1, indexable -> weightedTerm._2))
-      }.map(e => e._1 -> e._2.toSeq.sortBy(-_._2)) |> Trie.fromSeqMap |> (WeightedIndex(_))
-
+  // TODO lenses
+  private def toLowerCase[A, B](e: (A, (String, B))): (A, (String, B)) = e._1 -> (e._2._1.toLowerCase -> e._2._2)
   implicit object DoubleSemi extends Semigroup[Double] {
     override def append(f1: Double, f2: => Double): Double = f1 + f2
   }
 
-  private case class WeightedIndex[T: WeightedIndexable : Indexable](trie: Trie[(T, Double)]) extends Index[T] {
+  def buildIndexFor[T: WeightedIndexable : Indexable](ts: TraversableOnce[T]): Index[T] = ts
+      .flatMap(e => e.terms.map(e -> _))
+      .map(toLowerCase)
+      .aggregateMap(_._2._1, e => Set(e._1 -> e._2._2))
+      .mapValues(_.toVector.sortBy(_._2))
+      .mapTo(Trie.fromSeqMap)
+      .mapTo(new WeightedIndex(_))
 
+  private class WeightedIndex[T: WeightedIndexable : Indexable](trie: Trie[(T, Double)]) extends Index[T] {
     private def mergeIntersectingKeys[K, V: Semigroup](m1: Map[K, V], m2: Map[K, V]): Map[K, V] =
       m1.filterKeys(m2.contains).map(e => (e._1, e._2 |+| m2(e._1)))
     override def findIntersection(ss: Traversable[String]): Seq[T] = {
