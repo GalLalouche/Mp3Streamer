8d392337dcfa3d83a87cbab333aed0416f512870
Gal Lalouche <lalouche@google.com>
2017-06-25 17:07:39 +0300

    Fixed bad casting of as[JsObject]
    This apperently returns a null for mocks o_O.

--- app/common/Jsonable.scala
+++ app/common/Jsonable.scala
@@ -7,7 +7,7 @@ trait Jsonable[T] {
   def jsonify(t: T): JsObject
   def jsonify(ts: Seq[T]): JsArray = JsArray(ts map jsonify)
   def parse(json: JsObject): T
-  def parse(json: JsArray): Seq[T] = json.value map (_.as[JsObject]) map parse
+  def parse(json: JsArray): Seq[T] = json.value map (_.asInstanceOf[JsObject]) map parse
 }
 
 // For consistency with simulacrum
