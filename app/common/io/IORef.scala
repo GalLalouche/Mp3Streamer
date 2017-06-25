f642e72507b1cff16ed3771a25eba471309a2d2d
Gal Lalouche <lalouche@google.com>
2017-06-25 10:58:27 +0300

    IOFile and IODirectory lazily check for existence
    This is needed in order to be able to create IORefs without IO, as one creates
    a File instance.

--- app/common/io/IORef.scala
+++ app/common/io/IORef.scala
@@ -22,15 +22,16 @@ trait IOSystem extends RefSystem {
   override type D = IODirectory
 }
 
-abstract class IOPath(rp: RichPath) extends PathRef {
+abstract class IOPath(f: File) extends PathRef {
+  private lazy val rp = RichPath.richPath(f)
   override type S = IOSystem
-  override def path = rp.path
-  override def name = rp.name
+  override def path = f.getAbsolutePath
+  override def name = f.getName
   override def parent = IODirectory(rp.parent)
 }
 
 /** For production; actual files on the disk */
-class IOFile(val file: File) extends IOPath(RichPath richPath file) with FileRef {
+class IOFile(val file: File) extends IOPath(file) with FileRef {
   private lazy val rich = RichFile(file)
   override def bytes: Array[Byte] = rich.bytes
   override def write(bs: Array[Byte]) = {
@@ -45,22 +46,19 @@ class IOFile(val file: File) extends IOPath(RichPath richPath file) with FileRef
     rich appendLine line
     this
   }
-  override def path: String = rich.path
   override def readAll: String = rich.readAll
-  override def name: String = rich.name
   override def lastModified: LocalDateTime = file |> FileUtils.lastModified
 }
 
-class IODirectory(val dir: Directory) extends IOPath(dir) with DirectoryRef {
-  def this(path: String) = this(Directory(path))
+class IODirectory(file: File) extends IOPath(file) with DirectoryRef {
+  lazy val dir: Directory = Directory(file)
+  def this(path: String) = this(new File(path))
   override def addFile(name: String) = new IOFile(dir addFile name)
   private def optionalFile(name: String) = Some(new File(dir.dir, name)).filter(_.exists)
   override def getDir(name: String) =
     optionalFile(name).filter(_.isDirectory).map(e => new IODirectory(new Directory(e)))
   override def addSubDir(name: String) = new IODirectory(dir addSubDir name)
   override def getFile(name: String) = optionalFile(name).map(new IOFile(_))
-  override def path = dir.path
-  override def name = dir.name
   override def dirs = dir.dirs.map(new IODirectory(_))
   override def files = dir.files.map(new IOFile(_))
   override def lastModified: LocalDateTime = dir.dir |> FileUtils.lastModified
