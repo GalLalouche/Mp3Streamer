5653771945043450e1453ffaba7ef3d31799dcba
Gal Lalouche <lalouche@google.com>
2017-06-26 11:58:49 +0300

    Album.songs requires an implicit MusicFinder

--- app/models/Album.scala
+++ app/models/Album.scala
@@ -2,16 +2,17 @@ package models
 
 import java.io.File
 
+import common.io.IODirectory
 import common.rich.path.Directory
 import common.rich.path.RichFile._
 
 case class Album(dir: File, title: String, artistName: String, year: Int) {
-  lazy val songs = Directory(dir).files.filter(x => List("mp3", "flac").contains(x.extension)).map(Song(_)).sortBy(_.track)
+  def songs(implicit mf: MusicFinder): Seq[Song] = mf.getSongsInDir(new IODirectory(dir))
 }
 
 object Album {
-  def apply(dir: Directory) = new Album(dir, 
-      dir.name.replaceAll("\\d+\\w? ?", ""), 
-      dir.parent.name, 
+  def apply(dir: Directory) = new Album(dir,
+      dir.name.replaceAll("\\d+\\w? ?", ""),
+      dir.parent.name,
       dir.files.iterator.filter(x => List("mp3", "flac").contains(x.extension)).map(Song(_).year).next)
 }
