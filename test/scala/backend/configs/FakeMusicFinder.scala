9dbe1f2ab8f76d440a9653a4bff2d20c744847a6
Gal Lalouche <lalouche@google.com>
2017-06-28 11:42:54 +0300

    MusicFinderTest no longer uses IO

--- test/scala/backend/configs/FakeMusicFinder.scala
+++ test/scala/backend/configs/FakeMusicFinder.scala
@@ -8,12 +8,12 @@ import scala.collection.mutable
 class FakeMusicFinder(val dir: MemoryDir) extends MusicFinder {
   override type S = MemorySystem
   override val extensions = Set("mp3")
-  protected override val subDirNames: List[String] = List("music")
-  private val musicDir = dir addSubDir subDirNames.head
+  protected override def subDirNames: List[String] = List("music")
+  private val dirToAddSongsTo = dir addSubDir subDirNames.head
   private val pathToSongs = mutable.HashMap[String, Song]()
   /** Adds a song under root / songs / $artist_name / $album_time / $file_name. */
   def addSong(s: Song): MemoryDir = {
-    val $ = musicDir addSubDir s.artistName addSubDir s.albumName
+    val $ = dirToAddSongsTo addSubDir s.artistName addSubDir s.albumName
     val file = $ addFile s.file.name
     pathToSongs += file.path -> s
     $
--- test/scala/models/MusicFinderTest.scala
+++ test/scala/models/MusicFinderTest.scala
@@ -1,48 +1,44 @@
 package models
 
-import common.io.IODirectory
-import common.rich.path.TempDirectory
-import org.scalatest.{FreeSpec, Matchers, OneInstancePerTest}
+import backend.configs.FakeMusicFinder
+import common.AuxSpecs
+import common.io.MemoryRoot
+import common.rich.collections.RichTraversableOnce._
+import org.scalatest.{FreeSpec, OneInstancePerTest}
 
-class MusicFinderTest extends FreeSpec with OneInstancePerTest with Matchers {
-  // TODO make work using memory dirs
-  private val (tempDir, mf) = {
-    val tempDir = TempDirectory()
-    val dirs = List("a", "b", "c")
-    dirs foreach tempDir.addSubDir
-    val mf = new IOMusicFinder {
-      override val dir = IODirectory(tempDir)
-      protected override val subDirNames = dirs
-      override val extensions = Set("mp3", "flac")
-    }
-    tempDir -> mf
+class MusicFinderTest extends FreeSpec with OneInstancePerTest with AuxSpecs {
+  private val root = new MemoryRoot
+  private val mf = new FakeMusicFinder(root) {
+    protected override def subDirNames = List("a", "b", "c")
+    subDirNames.foreach(root.addSubDir)
+    override val extensions = Set("mp3", "flac")
   }
 
   "MusicFinder" - {
     "find nothing" - {
       def verifyIsEmpty() = mf.getSongFiles shouldBe 'empty
       "when subdirs are empty" in {
-        tempDir.addSubDir("a").addSubDir("b").addSubDir("c")
-        tempDir.addSubDir("b").addSubDir("b").addSubDir("c")
-        tempDir.addSubDir("c").addSubDir("b").addSubDir("c")
+        root.addSubDir("a").addSubDir("b").addSubDir("c")
+        root.addSubDir("b").addSubDir("b").addSubDir("c")
+        root.addSubDir("c").addSubDir("b").addSubDir("c")
         verifyIsEmpty()
       }
       "when file is in root" in {
-        tempDir.addFile("foo.mp3")
+        root.addFile("foo.mp3")
         verifyIsEmpty()
       }
       "when file is in unlisted dir" in {
-        tempDir.addSubDir("d").addFile("foo.mp3")
+        root.addSubDir("d").addFile("foo.mp3")
         verifyIsEmpty()
       }
       "when file has wrong extension" in {
-        tempDir.addSubDir("b").addFile("foo.mp2")
+        root.addSubDir("b").addFile("foo.mp2")
         verifyIsEmpty()
       }
     }
     "Find song" in {
-      tempDir.addSubDir("a").addSubDir("b").addFile("foo.mp3")
-      mf.getSongFiles.map(_.file) should contain((tempDir / "a" / "b" / "foo.mp3").p)
+      val f = root.addSubDir("a").addSubDir("b").addFile("foo.mp3")
+      mf.getSongFiles.single shouldReturn f
     }
   }
 }
