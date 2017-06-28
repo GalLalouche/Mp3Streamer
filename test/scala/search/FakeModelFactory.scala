465a953e7ffbbf611d65d194217723c9e504459c
Gal Lalouche <lalouche@google.com>
2017-06-28 10:32:49 +0300

    No more IO in FakeModelFactory

--- test/scala/search/FakeModelFactory.scala
+++ test/scala/search/FakeModelFactory.scala
@@ -1,16 +1,16 @@
 package search
 
-import java.io.File
 import java.util.UUID
 
-import common.io.{IODirectory, MemoryRoot}
+import common.io.MemoryRoot
 import models._
 
 class FakeModelFactory {
   private val root = new MemoryRoot
-  def album(dir: File = new File("./foobar"), title: String = "album", artistName: String = "artist",
-            year: Int = 2000) =
-    new Album(new IODirectory(dir.getAbsoluteFile), title = title, artistName = artistName, year = year)
+  def album(dirName: String = UUID.randomUUID().toString, title: String = "album",
+            artistName: String = "artist", year: Int = 2000) =
+    new Album(root addSubDir dirName, title = title, artistName = artistName, year = year)
+  // Returns a Song instead of MemorySong for Jsonable typeclassing, since Jsonable is invariant
   def song(filePath: String = s"${UUID.randomUUID()}.mp3", title: String = "title", artistName: String = "artist",
            albumName: String = "album", track: Int = 1, year: Int = 2000, bitRate: String = "320",
            duration: Int = 3600, size: Long = 1000, album: Album = null, discNumber: Option[String] = None,
