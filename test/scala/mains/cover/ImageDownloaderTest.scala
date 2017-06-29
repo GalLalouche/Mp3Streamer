d0b80c81517ce12eec3a50248d7f352f7820261d
Gal Lalouche <lalouche@google.com>
2017-06-29 14:11:00 +0300

    Removed Impatient class

--- test/scala/mains/cover/ImageDownloaderTest.scala
+++ test/scala/mains/cover/ImageDownloaderTest.scala
@@ -4,18 +4,14 @@ import java.nio.charset.MalformedInputException
 
 import backend.configs.TestConfiguration
 import common.AuxSpecs
-import common.concurrency.Impatient
 import common.io.MemoryRoot
 import common.rich.RichFuture._
 import org.mockito.Matchers
 import org.mockito.Mockito.when
-import org.mockito.invocation.InvocationOnMock
-import org.mockito.stubbing.Answer
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.{FreeSpec, OneInstancePerTest, ShouldMatchers}
 
 import scala.concurrent.Future
-import scala.concurrent.duration._
 
 class ImageDownloaderTest extends FreeSpec with ShouldMatchers with MockitoSugar with OneInstancePerTest with AuxSpecs {
   private val tempDir = new MemoryRoot
@@ -43,23 +39,9 @@ class ImageDownloaderTest extends FreeSpec with ShouldMatchers with MockitoSugar
     }
     "Return none if it doesn't succeed" in {
       when(downloader.download(Matchers.anyString(), Matchers.anyString()))
-        .thenReturn(Future.failed(new MalformedInputException(0)))
+          .thenReturn(Future.failed(new MalformedInputException(0)))
       val $ = new ImageDownloader(tempDir, downloader)
       $("url").value.get shouldBe 'failure
-    }
-    "Return none after timeout" in {
-      when(downloader.download(Matchers.anyString(), Matchers.anyString())).thenAnswer(
-        new Answer[Array[Byte]] {
-          override def answer(invocation: InvocationOnMock): Array[Byte] = {
-            while (true) {}
-            throw new AssertionError("Wat")
-          }
-        }
-      )
-      new Impatient[Int](15 millis).apply {
-        Thread sleep 50
-        1
-      } shouldBe 'empty
     }
   }
 }
