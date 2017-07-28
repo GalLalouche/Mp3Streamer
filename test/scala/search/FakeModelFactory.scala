6523a2d7cf6cc38a27fbf3b2c004c34dbfedc3ff
gal_lalouche <glpkmtg@gmail.com>
2017-07-28 15:38:20 +0300

    Move ArbSong to FakeModelFactory

--- test/scala/search/FakeModelFactory.scala
+++ test/scala/search/FakeModelFactory.scala
@@ -4,6 +4,8 @@ import java.util.UUID
 
 import common.io.MemoryRoot
 import models._
+import org.scalacheck.{Arbitrary, Gen}
+import Arbitrary.arbitrary
 
 class FakeModelFactory {
   private val root = new MemoryRoot
@@ -17,4 +19,18 @@ class FakeModelFactory {
     MemorySong(root.addFile(filePath), title, artistName, albumName, track, year, bitRate, duration, size, discNumber,
       trackGain)
   def artist(name: String = "artist", albums: Seq[Album] = Seq()): Artist = Artist(name, albums.toSet)
+  implicit val arbSong: Arbitrary[MemorySong] = Arbitrary(for {
+    title <- arbitrary[String]
+    artistName <- arbitrary[String]
+    albumName <- arbitrary[String]
+    track <- arbitrary[Int].map(_ % 100)
+    year <- arbitrary[Int].map(_ % 3000)
+    bitRate <- arbitrary[Int].map(_ % 10000).map(_ / 32.0).map(_.toString)
+    duration <- arbitrary[Int].map(_ % 1000)
+    size <- arbitrary[Int]
+    discNumber <- arbitrary[Option[String]]
+    trackGain <- arbitrary[Option[Int]].map(_.map(_ % 10000).map(_ / 32.0))
+  } yield song(
+    title = title, artistName = artistName, albumName = albumName, track = track, year = year,
+    bitRate = bitRate, duration = duration, size = size, discNumber = discNumber, trackGain = trackGain))
 }
