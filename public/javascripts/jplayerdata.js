3e6feee495af8095082b412e8f738eaa7112f609
gal_lalouche <glpkmtg@gmail.com>
2017-08-04 16:17:23 +0300

    Slightly more space for songs

--- public/javascripts/jplayerdata.js
+++ public/javascripts/jplayerdata.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
   const randomSongUrl = "data/randomSong";
   // Init playlist
   playlist = new jPlayerPlaylist({
@@ -11,7 +11,7 @@ $(function () {
   // Modify next to fetch a random song if in shuffle mode and at the last song
   playlist.oldNext = playlist.next;
   const shouldLoadNextSongFromRandom = () => playlist.isLastSongPlaying()
-  playlist.next = function () {
+  playlist.next = function() {
     if (shouldLoadNextSongFromRandom())
       loadNextRandom(true);
     else
@@ -19,7 +19,7 @@ $(function () {
   };
   // On play event hook
   // TODO don't call if the same song?
-  $(playlist.cssSelector.jPlayer).data("jPlayer").onPlay = function () {
+  $(playlist.cssSelector.jPlayer).data("jPlayer").onPlay = function() {
     const currentlyPlayingSong = playlist.currentPlayingSong();
     const songInfo = `${currentlyPlayingSong.artistName} - ${currentlyPlayingSong.title}`;
     $(".jp-currently-playing").html(songInfo);
@@ -31,25 +31,30 @@ $(function () {
     Volume.setPeak(currentlyPlayingSong)
   };
   // Poster classes
-  $("#jp_poster_0").addClass("poster");
+  const poster = $("#jp_poster_0")
+  poster.addClass("poster");
   $("#jquery_jplayer_1").addClass("poster");
   // Mute for debugging if requested
   if (mute)
     $(".jp-mute").click()
   else
     $(".jp-volume-max").click();
+
   function loadNextRandom(playNow) {
-    $.get(randomSongUrl, function (data) {
+    $.get(randomSongUrl, function(data) {
       playlist.add(data, playNow);
     });
   }
 
   loadNextRandom(true)
-  // Fetches new songs before current song ends
-  setInterval(function () {
+  // Fetches new songs before current song ends.
+  setInterval(function() {
     const media = $(playlist.cssSelector.jPlayer).data("jPlayer").htmlElement.media;
     const isSongNearlyFinished = media.duration - media.currentTime < WAIT_DELAY;
     if (shouldLoadNextSongFromRandom() && isSongNearlyFinished)
       loadNextRandom(false);
   }, (WAIT_DELAY - 5) * 1000);
+
+  // Technically CSS, but can't be done with pure css it seems.
+  poster.css("margin-left", (($(".jp-video").width() - poster.width()) / 2) + "px")
 });
