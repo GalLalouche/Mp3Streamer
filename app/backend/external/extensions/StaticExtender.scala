86a3dfa5f0962b10a22dbc5769f7c059bcab3211
gal_lalouche <glpkmtg@gmail.com>
2017-08-05 14:17:42 +0300

    Better error in static extender

--- app/backend/external/extensions/StaticExtender.scala
+++ app/backend/external/extensions/StaticExtender.scala
@@ -1,5 +1,6 @@
 package backend.external.extensions
 
+import backend.configs.StandaloneConfig
 import backend.external.{MarkedLink, MarkedLinks}
 import backend.recon.Reconcilable
 import common.rich.RichT._
@@ -7,6 +8,11 @@ import common.rich.RichT._
 /** Extenders whose result is not dependent on other links. */
 private trait StaticExtender[R <: Reconcilable] extends LinkExtender[R] {
   protected def apply(t: R, e: MarkedLink[R]): Seq[LinkExtension[R]]
-  override def apply(t: R, e: MarkedLinks[R]): Seq[LinkExtension[R]] =
-    e.flatMap(_.opt.filter(_.host == this.host)).ensuring(_.size <= 1).flatMap(apply(t, _)).toList
+  override def apply(t: R, e: MarkedLinks[R]): Seq[LinkExtension[R]] = {
+    val linksForHost = e.flatMap(_.opt.filter(_.host == this.host))
+    if (linksForHost.size > 1)
+    // TODO pass config?
+      StandaloneConfig.logger.warn(s"Expected a single host for <$t>, but found: <$linksForHost>.")
+    linksForHost.take(1).flatMap(apply(t, _)).toList
+  }
 }
