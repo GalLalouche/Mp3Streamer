5f1e75b127c031ad7b35e037c1e1dd912bbdad96
gal_lalouche <glpkmtg@gmail.com>
2017-09-01 13:36:20 +0300

    Fixed CueTools clean up and added printlns

--- app/mains/splitter/CueTools.scala
+++ app/mains/splitter/CueTools.scala
@@ -1,22 +1,22 @@
 package mains.splitter
 
 import java.io.File
-import java.nio.file.Files
 
-import common.rich.path.Directory
 import common.rich.path.RichFile._
+import common.rich.path.{Directory, RichFileUtils}
 
 import scala.sys.process.Process
 
 private object CueTools {
   private val exe = """C:\Program Files (x86)\CUETools_2.1.5\CUETools.exe"""
   private def clean(dir: Directory) {
-    dir.files
-        .filter(_.extension == "flac")
-        .foreach(f => Files.move(f.toPath, new File(dir.getParentFile, f.name).toPath)) // move files up
-    dir.deleteAll
+    println("Moving flac files to parent dir")
+    dir.files.filter(_.extension == "flac").foreach(f => RichFileUtils.move(f, dir.parent))
+    println("Deleting convert dir")
+    dir.deleteAll()
   }
   def split(cueFile: File) {
+    println("Invoking CUETools")
     Process(exe, Seq("/convert", s""""${cueFile.getAbsolutePath}"""")).!!
     clean(Directory(cueFile.getParent) / "convert" /)
   }
