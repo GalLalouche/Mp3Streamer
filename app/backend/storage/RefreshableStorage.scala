652493e39c6a9a8f873eb61348244e0d1bf7a415
gal_lalouche <glpkmtg@gmail.com>
2018-01-16 11:11:15 +0200

    use handleButKeepOriginal (removed TODO)

--- app/backend/storage/RefreshableStorage.scala
+++ app/backend/storage/RefreshableStorage.scala
@@ -21,21 +21,15 @@ class RefreshableStorage[Key, Value](
   def needsRefresh(k: Key): Future[Boolean] =
     freshnessStorage.freshness(k)
         .map(_.forall(_.exists(_.mapTo(age) > maxAge)))
+
   private def refresh(k: Key): Future[Value] =
     for (v <- onlineRetriever(k);
          _ <- freshnessStorage.forceStore(k, v))
       yield v
-
   override def apply(k: Key): Future[Value] =
     needsRefresh(k).flatMap(isOld => {
-      lazy val oldData = freshnessStorage load k
-      if (isOld) {
-        val r = refresh(k)
-        // TODO orElseTry that keeps original failure if the next try fails
-        r.orElseTry(oldData ifNoneTry r)
-      }
-      else
-        oldData.map(_.get)
+      lazy val oldData = freshnessStorage load k map (_.get)
+      if (isOld) refresh(k) handleButKeepOriginal oldData.const else oldData
     })
 
   def withAge(k: Key): Future[(Value, Option[LocalDateTime])] =
