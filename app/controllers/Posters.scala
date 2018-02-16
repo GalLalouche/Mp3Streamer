933c2a0ece47b1cfe561504b451d8fa3f93cf589
gal_lalouche <glpkmtg@gmail.com>
2018-02-16 18:03:43 +0200

    Remove Posters.display since it was pointless

--- app/controllers/Posters.scala
+++ app/controllers/Posters.scala
@@ -2,12 +2,9 @@ package controllers
 
 import play.api.mvc.Action
 
+// TODO Move to its own package?
 object Posters extends LegacyController {
 	def image(path: String) = Action {
 		Ok sendFile ControllerUtils.parseFile(path)
-	}
-
-	def displayImage(path: String) = Action {
-		Ok(views.html.poster("/posters/" + path))
 	}
 }
