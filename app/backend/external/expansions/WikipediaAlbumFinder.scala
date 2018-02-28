d97017053e0173119b8490b6db8a09ae9d778b62
Gal Lalouche <lalouche@google.com>
2018-02-28 13:45:27 +0200

    WikipediaSameHostExander tries multiple links

--- app/backend/external/expansions/WikipediaAlbumFinder.scala
+++ app/backend/external/expansions/WikipediaAlbumFinder.scala
@@ -5,29 +5,31 @@ import backend.external.Host
 import backend.recon.{Album, StringReconScorer}
 import common.io.InternetTalker
 import common.rich.RichT._
-import common.rich.func.ToTraverseMonadPlusOps
+import common.rich.func.{MoreSeqInstances, MoreTraverseInstances, ToTraverseMonadPlusOps}
 import common.rich.primitives.RichBoolean._
 import common.rich.primitives.RichString._
 import org.jsoup.nodes.Document
-import scalaz.std.{FutureInstances, OptionInstances}
+import scalaz.std.FutureInstances
 
 import scala.collection.JavaConverters._
 import scala.concurrent.Future
 
 private class WikipediaAlbumFinder(implicit it: InternetTalker) extends SameHostExpander(Host.Wikipedia)
-    with ToTraverseMonadPlusOps with FutureInstances with OptionInstances {
+    with ToTraverseMonadPlusOps with FutureInstances with MoreSeqInstances with MoreTraverseInstances {
   override protected def findAlbum(d: Document, a: Album): Future[Option[Url]] = {
     def score(linkName: String): Double = StringReconScorer(a.title, linkName)
-    d.select("a").asScala
-        .find(e => score(e.text) > 0.95)
+    d.select("a").asScala.toSeq
+        .filter(e => score(e.text) > 0.95)
         .map(_.attr("href"))
         .filter(_.nonEmpty)
         .filterNot(_ contains "redlink=1")
         .map("https://en.wikipedia.org" + _ |> Url)
         .filterTraverse(isNotRedirected)
+        .map(_.headOption)
   }
 
   private def isNotRedirected(link: Url): Future[Boolean] = {
+    // TODO Should check if the redirection points to the original url.
     val title = link.address dropAfterLast '/'
     val urlWithoutRedirection = s"https://en.wikipedia.org/w/index.php?title=$title&redirect=no"
     it.downloadDocument(Url(urlWithoutRedirection))
--- test/backend/external/expansions/WikipediaAlbumFinderTest.scala
+++ test/backend/external/expansions/WikipediaAlbumFinderTest.scala
@@ -9,9 +9,11 @@ class WikipediaAlbumFinderTest extends SameHostExpanderSpec {
     new WikipediaAlbumFinder
 
   private val DiscographyExtractLink = "https://en.wikipedia.org/w/index.php?title=Lady_in_Gold_(album)&redirect=no"
+  private val Redirected = "wiki_redirected.html"
+  private val NotRedirected = "wikipedia-discography.html"
   "extract the album when possible" in {
     findAlbum("wikipedia-discography.html", Album("Lady in Gold", 2016, Artist("Blues Pills")),
-      DiscographyExtractLink -> "wikipedia-discography.html")
+      DiscographyExtractLink -> NotRedirected)
         .get.link shouldReturn Url("https://en.wikipedia.org/wiki/Lady_in_Gold_(album)")
   }
   "Return nothing on red link" in {
@@ -19,6 +21,13 @@ class WikipediaAlbumFinderTest extends SameHostExpanderSpec {
   }
   "Return nothing on redirected link" in {
     findAlbum("wikipedia-discography.html", Album("Lady in Gold", 2016, Artist("Blues Pills")),
-      DiscographyExtractLink -> "wiki_redirected.html") shouldReturn None
+      DiscographyExtractLink -> Redirected) shouldReturn None
+  }
+  "Tries all links until one of them works" in {
+    // The below HTML includes two links, the first one is redirected, and the second is disambiguated.
+    findAlbum("two_wiki_links.html", Album("Preachers of the night", 2013, Artist("Powerwolf")),
+      "https://en.wikipedia.org/w/index.php?title=Preachers_of_the_Night_(album)&redirect=no" -> NotRedirected,
+      "https://en.wikipedia.org/w/index.php?title=Preachers_of_the_Night&redirect=no" -> Redirected)
+        .get.link shouldReturn Url("https://en.wikipedia.org/wiki/Preachers_of_the_Night_(album)")
   }
 }
