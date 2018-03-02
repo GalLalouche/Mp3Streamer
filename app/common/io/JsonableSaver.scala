2cf2aebf89588daa5fba421e9754e39c53cf65e9
Gal PC <glpkmtg@gmail.com>
2018-03-02 12:53:30 +0200

    fixup! Fix: classes jsonifying arrays could not be loaded as jsonables

--- app/common/io/JsonableSaver.scala
+++ app/common/io/JsonableSaver.scala
@@ -17,13 +17,11 @@ class JsonableSaver(implicit rootDirectoryProvider: RootDirectoryProvider) exten
   private val workingDir = rootDirectoryProvider.rootDirectory addSubDir "data" addSubDir "json"
   protected def jsonFileName[T: Manifest]: String =
     s"${manifest.runtimeClass.getSimpleName.replaceAll("\\$", "")}s.json"
-  private def save[T: Manifest](js: JsValue) {
-    workingDir addFile jsonFileName write js.toString
-  }
+  private def save[T: Manifest](js: JsValue): Unit = workingDir addFile jsonFileName write js.toString
 
   /**
-   * All files of the same type will be saved in the same file. Last save overwrites
-   * previous save. Saves in the same order that was traversed, so load will return in the same order as well.
+   * All files of the same type will be saved in the same file. Last save overwrites previous save.
+   * Saves in the same order that was traversed, so load will return in the same order as well.
    */
   // TODO replace TraversableOnce with a Non-Empty list?
   def save[T: Jsonable : Manifest](data: TraversableOnce[T]) {
@@ -34,8 +32,9 @@ class JsonableSaver(implicit rootDirectoryProvider: RootDirectoryProvider) exten
 
   /**
    * Similar to save, but doesn't overwrite the data
-   * @param dataAppender A function that takes the old data and appends the new data. Since some constraints can exist
-   *                     on the data save, e.g., uniqueness, a simple concatenation of old and new data isn't enough.
+   * @param dataAppender A function that takes the old data and appends the new data. Since some
+   *     constraints can exist on the data save, e.g., uniqueness, a simple concatenation of old
+   *     and new data isn't enough.
    */
   //TODO this could perhaps be handled by a type class that would know its saving constraints?
   def update[T: Jsonable : Manifest](dataAppender: Seq[T] => TraversableOnce[T]) {
@@ -47,11 +46,10 @@ class JsonableSaver(implicit rootDirectoryProvider: RootDirectoryProvider) exten
   /** Loads the previously saved entries, or returns an empty list. */
   def loadArray[T: Jsonable : Manifest]: Seq[T] = load.mapHeadOrElse(_.parse[Seq[T]], Nil)
   /** Loads the previously saved entry, or throws an exception if no file has been found */
-  def loadObject[T: Jsonable : Manifest]: T = {
-    val js = load getOrThrow new FileNotFoundException(s"Couldn't find file for type <$manifest>")
-    js.parse[T]
-  }
+  def loadObject[T: Jsonable : Manifest]: T =
+    load.getOrThrow(new FileNotFoundException(s"Couldn't find file for type <$manifest>")).parse[T]
 
   // Require T: Jsonable, otherwise T will always be inferred as Nothing
-  def lastUpdateTime[T: Jsonable : Manifest]: Option[LocalDateTime] = workingDir getFile jsonFileName map (_.lastModified)
+  def lastUpdateTime[T: Jsonable : Manifest]: Option[LocalDateTime] =
+    workingDir getFile jsonFileName map (_.lastModified)
 }
