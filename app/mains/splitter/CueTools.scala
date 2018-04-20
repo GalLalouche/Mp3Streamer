732ecdbb9a19ebff0f5fcdf4bc15ebb29748d48c
gal_lalouche <glpkmtg@gmail.com>
2018-04-20 18:17:56 +0300

    Delete HTOA flac file after extracting cue files

--- app/mains/splitter/CueTools.scala
+++ app/mains/splitter/CueTools.scala
@@ -11,6 +11,7 @@ private object CueTools {
   private val exe = """C:\Program Files (x86)\CUETools_2.1.5\CUETools.exe"""
   private def clean(dir: Directory) {
     println("Moving flac files to parent dir")
+    dir.files.find(_.name == "00. (HTOA).flac").foreach(_.delete())
     dir.files.filter(_.extension == "flac").foreach(f => RichFileUtils.move(f, dir.parent))
     println("Deleting convert dir")
     dir.deleteAll()
