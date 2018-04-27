100590efd17aba3d5ca05b073669d678d97e0819
gal_lalouche <glpkmtg@gmail.com>
2018-04-27 14:36:59 +0300

    Disabled back button

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -148,8 +148,19 @@ Array.prototype.custom_last = function() {
   return this[this.length - 1]
 }
 
+// TODO move this stuff to an initialization script
 playlist = {};
 jPlayer = {};
 WAIT_DELAY = 25;
 isMobile = navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/) !== null;
 mute=window.location.href.endsWith("mute")
+// Disables back button
+if (window.history && history.pushState) {
+  addEventListener('load', function() {
+    history.pushState(null, null, null); // creates new history entry with same URL
+    addEventListener('popstate', function() {
+      alert("Back key disabled!");
+      history.pushState(null, null, null);
+    });
+  });
+}
