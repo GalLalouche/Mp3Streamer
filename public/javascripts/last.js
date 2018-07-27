3b2cdc4e5779132cc91fcb0b7ce6e658f09d5253
gal_lalouche <glpkmtg@gmail.com>
2018-07-27 14:48:10 +0300

    Added download button to last album

--- public/javascripts/last.js
+++ public/javascripts/last.js
@@ -2,15 +2,21 @@ $(function() {
   const last_albums = elem("ul").appendTo($("#last_albums")).css("list-style", "none")
   const add = "plus"
   const remove = "times"
+  const download = "download"
 
   function onMessage(msg) {
     const album = JSON.parse(msg.data)
-    const listElement = elem("li", `${icon(add)} ${icon(remove)} ${album.artistName}: ${album.title}`).addClass("last-album")
+    const listElement = elem("li", `${icon(add)} ${icon(download)} ${icon(remove)} ${album.artistName}: ${album.title}`).addClass("last-album")
     listElement.find(".fa-" + add).click(function() {
       listElement.remove()
       $.get("data/albums/" + album.dir, e => gplaylist.add(e, false))
     })
     listElement.find(".fa-" + remove).click(function() {
+      listElement.remove()
+    })
+    listElement.find(".fa-" + download).click(function(e) {
+      e.preventDefault() // Needed for Chrome so it doesn't follow the link.
+      window.location.href = "download/" + album.dir
       listElement.remove()
     })
     last_albums.append(listElement)
