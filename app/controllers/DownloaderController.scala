c57ce217f8039597991854d2fa02ae5f31dfb8aa
gal_lalouche <glpkmtg@gmail.com>
2018-08-03 18:15:35 +0300

    Downloader supports resumes

--- app/controllers/DownloaderController.scala
+++ app/controllers/DownloaderController.scala
@@ -1,15 +1,41 @@
 package controllers
 
+import java.io.FileInputStream
+
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
 import backend.Zipper
 import common.io.{IODirectory, IOFile}
+import common.rich.primitives.RichString._
+import play.api.http.HttpEntity
+import play.api.libs.iteratee.Enumerator
+import play.api.libs.iteratee.streams.IterateeStreams
 import play.api.mvc.Action
 
 object DownloaderController extends LegacyController {
   private val zipper = new Zipper()
-  def download(path: String) = Action.async {
+
+  def download(path: String) = Action.async {request =>
+    def parseRange(s: String): Long = s.dropAfterLast('=').takeWhile(_.isDigit).toLong
+    val bytesToSkip: Long = request.headers get "Range" map parseRange getOrElse 0L
     val file = ControllerUtils.parseFile(path)
     require(file.isDirectory)
     zipper.zip(IODirectory(file.getAbsolutePath))
-        .map(f => Ok.sendFile(f.asInstanceOf[IOFile].file, inline = false))
+        .map(file => {
+          val fis = new FileInputStream(file.asInstanceOf[IOFile].file)
+          fis.skip(bytesToSkip)
+          val status = if (bytesToSkip == 0) Ok else PartialContent
+          val source =
+            Source.fromPublisher(IterateeStreams.enumeratorToPublisher(Enumerator fromStream fis)).map(ByteString.apply)
+          status.sendEntity(HttpEntity.Streamed(source, Some(file.size - bytesToSkip), Some("application/zip"))).withHeaders(
+            "Access-Control-Allow-Headers" -> "range, accept-encoding",
+            "Access-Control-Allow-Origin" -> "*",
+            "Accept-Ranges" -> "bytes",
+            "Connection" -> "keep-alive",
+            //"Content-Length" -> s"${}",
+            "Content-Range" -> s"bytes $bytesToSkip-${file.size}/${file.size}",
+            "Content-Disposition" -> s"attachment; filename=${file.name}",
+          )
+        })
   }
 }
