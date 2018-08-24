25ad7d2fbdf35346cc0175ba5f9cd78805c095fe
Gal PC <glpkmtg@gmail.com>
2018-08-24 21:01:10 +0300

    (Slightly) better logging on failure in CompositeLyricsRetriever

--- app/backend/lyrics/retrievers/CompositeHtmlRetriever.scala
+++ app/backend/lyrics/retrievers/CompositeHtmlRetriever.scala
@@ -1,17 +1,21 @@
 package backend.lyrics.retrievers
 
 import backend.Url
+import backend.logging.LoggerProvider
 import backend.lyrics.Lyrics
 import common.rich.func.ToMoreFoldableOps
 import models.Song
 
 import scala.concurrent.{ExecutionContext, Future}
+
 import scalaz.std.OptionInstances
 
 private[lyrics] class CompositeHtmlRetriever(retrievers: List[HtmlRetriever])
-    (implicit ec: ExecutionContext) extends CompositeLyricsRetriever(retrievers) with HtmlRetriever
+    (implicit ec: ExecutionContext, lp: LoggerProvider)
+    extends CompositeLyricsRetriever(retrievers) with HtmlRetriever
     with ToMoreFoldableOps with OptionInstances {
-  def this(retrievers: HtmlRetriever*)(implicit ec: ExecutionContext) = this(retrievers.toList)
+  def this(retrievers: HtmlRetriever*)(implicit ec: ExecutionContext, lp: LoggerProvider) =
+    this(retrievers.toList)
   override def doesUrlMatchHost(url: Url): Boolean = {
     retrievers.exists(_ doesUrlMatchHost url)
   }
--- app/backend/lyrics/retrievers/CompositeLyricsRetriever.scala
+++ app/backend/lyrics/retrievers/CompositeLyricsRetriever.scala
@@ -1,17 +1,23 @@
 package backend.lyrics.retrievers
 
+import backend.logging.LoggerProvider
 import backend.lyrics.Lyrics
-import common.rich.RichT._
 import models.Song
 
 import scala.concurrent.{ExecutionContext, Future}
+
 import scalaz.std.{FutureInstances, ListInstances}
 import scalaz.syntax.ToMonadErrorOps
 
-private[lyrics] class CompositeLyricsRetriever(retrievers: List[LyricsRetriever])(implicit ec: ExecutionContext)
-    extends LyricsRetriever
-        with FutureInstances with ListInstances with ToMonadErrorOps {
+private[lyrics] class CompositeLyricsRetriever(retrievers: List[LyricsRetriever])
+    (implicit ec: ExecutionContext, loggerProvider: LoggerProvider) extends LyricsRetriever
+    with FutureInstances with ListInstances with ToMonadErrorOps {
+  // TODO better errors when no parser is found
   override def apply(s: Song): Future[Lyrics] =
-    retrievers.foldLeft[Future[Lyrics]](retrievers.head.apply(s))((x, y) => x.handleError(y.apply(s).const))
-  def this(retrievers: LyricsRetriever*)(implicit ec: ExecutionContext) = this(retrievers.toList)
+    retrievers.foldLeft[Future[Lyrics]](retrievers.head.apply(s))((x, y) => x.handleError(e => {
+      loggerProvider.logger.error("Failed to parse lyrics: ", e)
+      y.apply(s)
+    }))
+  def this(retrievers: LyricsRetriever*)(implicit ec: ExecutionContext, lp: LoggerProvider) =
+    this(retrievers.toList)
 }
