8a0808294a756abf28e6c7c65e83240a0cac67f1
Gal PC <glpkmtg@gmail.com>
2018-09-19 21:20:59 +0300

    Client side backup

--- public/javascripts/playlist.js
+++ public/javascripts/playlist.js
@@ -1,4 +1,4 @@
-// code for loading and saving playlists and states
+// Loading and saving playlists, either locally (backup) or from the server.
 $(function() {
   $("#update_playlist").click(function() {
     const playlist = gplaylist.songs().slice(gplaylist.currentIndex()).map(x => x.file);
@@ -7,21 +7,45 @@ $(function() {
   $("#load_playlist").click(function() {
     $.get("playlist/queue", x => x.forEach(e => gplaylist.add(e, false)))
   })
-  $("#update_state").click(function() {
-    const data = {
-      songs: gplaylist.songs().map(x => x.file),
+
+  // TODO fix naming cohesion: index vs. currentIndex
+  function getState() {
+    return {
+      songs: gplaylist.songs(),
       index: gplaylist.currentIndex(),
       duration: gplayer.currentPlayingInSeconds()
     }
-    postJson("playlist/state", data, () => $.toast("State successfully updated"))
+  }
+  $("#update_state").click(function() {
+    const state = getState()
+    state.songs = state.songs.map(x => x.file)
+    postJson("playlist/state", state, () => $.toast("State successfully updated"))
   })
+
+  function setState(state) {
+    gplayer.stop()
+    gplaylist.setPlaylist(state.songs, false)
+    gplaylist.select(state.index || state.currentIndex)
+    gplayer.skip(state.duration)
+    // gplayer.playCurrentSong()
+  }
   $("#load_state").click(function() {
-    $.get("playlist/state", data => {
-      gplayer.stop()
-      gplaylist.setPlaylist(data.songs, false)
-      gplaylist.select(data.currentIndex)
-      gplayer.skip(data.duration)
-      // gplayer.playCurrentSong()
-    })
+    $.get("playlist/state", setState)
   })
+
+  const backupKey = "backup"
+  function saveBackup() {
+    localStorage.setItem(backupKey, JSON.stringify(getState()))
+  }
+  function loadBackup() {
+    return JSON.parse(localStorage.getItem(backupKey))
+  }
+  $("#update_backup").click(function() {
+    saveBackup()
+    $.toast("Backup successfully created")
+  })
+  $("#load_backup").click(() => setState(loadBackup()))
+
+  const ONE_MINUTE = 60 * 1000
+  setInterval(saveBackup, ONE_MINUTE)
 })
