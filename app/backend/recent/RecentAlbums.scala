e1c154618e53af7855b1a472876a9acaa451d5f0
Gal PC <glpkmtg@gmail.com>
2018-09-26 13:57:25 +0300

    Refactor recent albums to use topK

--- app/backend/recent/RecentAlbums.scala
+++ app/backend/recent/RecentAlbums.scala
@@ -1,17 +1,15 @@
 package backend.recent
 
-import java.time.ZoneOffset
-
+import common.rich.func.{MoreSeqInstances, ToMoreFoldableOps}
 import javax.inject.Inject
+import backend.RichTime.OrderingLocalDateTime
 import models.{Album, AlbumFactory, MusicFinder}
 
-private class RecentAlbums @Inject()(mf: MusicFinder, albumFactory: AlbumFactory) {
-  def apply(amount: Int): Seq[Album] =
-    mf.genreDirs
-        .flatMap(_.deepDirs)
-        .filter(mf.getSongFilesInDir(_).nonEmpty)
-        .sortBy(_.lastModified)(Ordering.by(-_.toEpochSecond(ZoneOffset.UTC)))
-        .take(amount)
-        .map(albumFactory.fromDir)
-        .map(Album.songs set Nil) // recent doesn't care about songs
+private class RecentAlbums @Inject()(mf: MusicFinder, albumFactory: AlbumFactory)
+    extends ToMoreFoldableOps with MoreSeqInstances {
+  def apply(amount: Int): Seq[Album] = mf.genreDirs
+      .flatMap(_.deepDirs)
+      .filter(mf.getSongFilesInDir(_).nonEmpty)
+      .topK(amount)(Ordering.by(_.lastModified))
+      .map(albumFactory.fromDir(_).copy(songs = Nil)) // recent doesn't care about songs
 }
