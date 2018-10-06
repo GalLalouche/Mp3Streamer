22f53ea9edd82f935a054c021e091243f886afeb
Gal PC <glpkmtg@gmail.com>
2018-10-06 16:04:47 +0300

    Extracted AlbumsFormatter

--- app/backend/albums/AlbumsController.scala
+++ app/backend/albums/AlbumsController.scala
@@ -1,50 +1,19 @@
 package backend.albums
 
-import backend.Retriever
-import backend.recon._
-import common.RichJson._
-import common.json.ToJsonableOps
-import common.rich.RichT._
+import controllers.FormatterUtils
 import javax.inject.Inject
-import mains.fixer.StringFixer
-import play.api.mvc.{AnyContent, InjectedController, Request}
-
-import scala.concurrent.ExecutionContext
-
-import scalaz.std.FutureInstances
-import scalaz.syntax.ToFunctorOps
+import play.api.mvc.InjectedController
 
 /** A web interface to new albums finder. Displays new albums and can update the current file / ignoring policy. */
-class AlbumsController @Inject()(ec: ExecutionContext, $: NewAlbums) extends InjectedController
-    with FutureInstances with ToFunctorOps with ToJsonableOps {
-  private implicit val iec: ExecutionContext = ec
+class AlbumsController @Inject()($: AlbumsFormatter, formatterUtils: FormatterUtils)
+    extends InjectedController {
+  def albums = formatterUtils.ok($.albums)
 
-  def albums = Action.async {
-    $.loadAlbumsByArtist
-        .map(
-          _.map {
-            case (artist, newAlbums) =>
-              artist.name -> newAlbums.map(NewAlbum.title.modify(_ tryOrKeep StringFixer.apply)).jsonify
-          }.jsonify)
-        .map(Ok(_))
-  }
+  def removeArtist() = formatterUtils.parseText($.removeArtist)
+  def ignoreArtist() = formatterUtils.parseText($.ignoreArtist)
 
-  private def updateNewAlbums[A](
-      extract: Request[AnyContent] => A, act: Retriever[A, Unit]) = Action.async {
-    _ |> extract |> act as NoContent
-  }
-
-  private def extractArtist(request: Request[AnyContent]): Artist =
-    request.body.asText.get |> Artist.apply
-  def removeArtist() = updateNewAlbums(extractArtist, $.removeArtist)
-  def ignoreArtist() = updateNewAlbums(extractArtist, $.ignoreArtist)
-
-  private def extractAlbum(request: Request[AnyContent]): Album = {
-    val json = request.body.asJson.get
-    Album(json str "title", json int "year", Artist(json str "artistName"))
-  }
-  def removeAlbum() = updateNewAlbums(extractAlbum, $.removeAlbum)
-  def ignoreAlbum() = updateNewAlbums(extractAlbum, $.ignoreAlbum)
+  def removeAlbum() = formatterUtils.parseJson($.removeAlbum)
+  def ignoreAlbum() = formatterUtils.parseJson($.ignoreAlbum)
 
   def index = Action {
     Ok(views.html.new_albums())
---
+++ app/backend/albums/AlbumsFormatter.scala
@@ -0,0 +1,35 @@
+package backend.albums
+
+import backend.recon.{Album, Artist}
+import common.RichJson._
+import common.json.ToJsonableOps
+import common.rich.RichT._
+import javax.inject.Inject
+import mains.fixer.StringFixer
+import play.api.libs.json.{JsObject, JsValue}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+import scalaz.std.FutureInstances
+import scalaz.syntax.ToFunctorOps
+
+class AlbumsFormatter @Inject()(ec: ExecutionContext, $: NewAlbums)
+    extends ToFunctorOps with FutureInstances
+        with ToJsonableOps {
+  private implicit val iec: ExecutionContext = ec
+
+  def albums: Future[JsValue] = $.loadAlbumsByArtist
+      .map(
+        _.map {
+          case (artist, newAlbums) =>
+            artist.name -> newAlbums.map(NewAlbum.title.modify(_ tryOrKeep StringFixer.apply)).jsonify
+        }.jsonify)
+
+  def removeArtist(artistName: String): Future[_] = $.removeArtist(Artist(artistName))
+  def ignoreArtist(artistName: String): Future[_] = $.ignoreArtist(Artist(artistName))
+
+  private def extractAlbum(json: JsObject): Album =
+    Album(json str "title", json int "year", Artist(json str "artistName"))
+  def removeAlbum(album: JsObject): Future[_] = $.removeAlbum(extractAlbum(album))
+  def ignoreAlbum(album: JsObject): Future[_] = $.ignoreAlbum(extractAlbum(album))
+}
