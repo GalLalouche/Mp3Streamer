23b20c93b386ca320b7cbd8f9ffc9af811730420
Gal PC <glpkmtg@gmail.com>
2018-10-08 20:49:06 +0300

    Fix a couple of CleanModule bugs
    1. Incorrect direction of class subclassing check
    2. Only create a table if doesn't already exist

--- app/backend/module/CleanModule.scala
+++ app/backend/module/CleanModule.scala
@@ -3,26 +3,32 @@ package backend.module
 import com.google.inject.{Provider, TypeLiteral}
 import com.google.inject.matcher.Matchers
 import com.google.inject.spi.{InjectionListener, TypeEncounter, TypeListener}
+import common.rich.primitives.RichClass._
 import common.ModuleUtils
+import common.rich.func.ToMoreMonadOps
 import common.storage.Storage
 import net.codingwell.scalaguice.ScalaModule
 
 import scala.concurrent.ExecutionContext
 
+import scalaz.std.FutureInstances
+
 /** Also creates all needed tables after creating the storage instances them. */
-object CleanModule extends ScalaModule with ModuleUtils {
+object CleanModule extends ScalaModule with ModuleUtils
+    with ToMoreMonadOps with FutureInstances {
   // TODO extract to module utils
   private def storageListener(ecProvider: Provider[ExecutionContext]): TypeListener =
     new TypeListener with InjectionListener[Storage[_, _]] {
       import common.rich.RichFuture._
 
       override def hear[I](`type`: TypeLiteral[I], encounter: TypeEncounter[I]): Unit = {
-        if (`type`.getRawType.isAssignableFrom(classOf[Storage[_, _]]))
+        if (`type`.getRawType.isAssignableTo(classOf[Storage[_, _]]))
           encounter.register(this.asInstanceOf[InjectionListener[I]])
       }
       override def afterInjection(injectee: Storage[_, _]) = {
         implicit val ec: ExecutionContext = ecProvider.get()
-        injectee.utils.createTable().get
+        // TODO createTableIfItDoesn'tExist?
+        injectee.utils.doesTableExist.ifFalse(injectee.utils.createTable).get
       }
     }
 
