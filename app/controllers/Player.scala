79b6c0a02ac092aa6e33651c6ab18c38ee980c7e
Gal PC <glpkmtg@gmail.com>
2018-10-12 16:32:31 +0300

    Extracted PlayerFormatter

--- app/controllers/Player.scala
+++ app/controllers/Player.scala
@@ -1,82 +1,27 @@
 package controllers
 
 import common.Debug
-import common.io.IODirectory
-import common.json.{JsonWriteable, ToJsonableOps}
-import common.rich.RichT._
-import decoders.Mp3Encoder
+import common.json.ToJsonableOps
+import controllers.PlayerFormatter.ShouldEncodeMp3Reader
 import javax.inject.Inject
-import models._
-import play.api.libs.json.JsValue
 import play.api.mvc._
-import songs.{SongGroup, SongGroups, SongSelectorState}
 
 /** Handles fetch requests of JSON information. */
 class Player @Inject()(
-    albumFactory: AlbumFactory,
-    groups: SongGroups,
-    songSelectorState: SongSelectorState,
-    encoder: Mp3Encoder,
-    urlPathUtils: UrlPathUtils,
-    songJsonifier: ControllerSongJsonifier,
+    $: PlayerFormatter,
+    converter: PlayActionConverter,
 ) extends InjectedController with ToJsonableOps with Debug {
-  import songJsonifier.songJsonable
+  private def run(r: ShouldEncodeMp3Reader): Action[AnyContent] =
+    converter.parse(PlayControllerUtils.shouldEncodeMp3)(r.run)
 
-  private val songGroups: Map[Song, SongGroup] = SongGroups.fromGroups(groups.load)
-
-  private def group(s: Song): Either[Song, SongGroup] = songGroups get s toRight s
-
-  // Doesn't extend JsonWritable to avoid recursive implicit lookup.
-  private trait Encodable[A] {
-    def encode(e: A): Unit
-    def jsonify(e: A): JsValue
-  }
-  private object Encodable {
-    private def jsonableEncodable[A: JsonWriteable](f: A => Unit): Encodable[A] =
-      new Encodable[A] {
-        override def encode(e: A): Unit = f(e)
-        override def jsonify(a: A): JsValue = a.jsonify
-      }
-    implicit val songEncodable: Encodable[Song] = jsonableEncodable(encoder ! _.file)
-    implicit val songsEncodable: Encodable[Seq[Song]] =
-      jsonableEncodable(_ foreach songEncodable.encode)
-    implicit val eitherEncodable: Encodable[Either[Song, SongGroup]] =
-      jsonableEncodable(_.fold(songEncodable.encode, songsEncodable encode _.songs))
-  }
-  private def encodeIfChrome[A](encodable: A)(request: Request[_])(implicit ev: Encodable[A]): Result = {
-    if (PlayControllerUtils.shouldEncodeMp3(request))
-      ev.encode(encodable)
-    Ok(ev.jsonify(encodable))
-  }
-
-  def randomSong = Action {
-    encodeIfChrome(group(songSelectorState.randomSong)) _
-  }
-
+  def randomSong = run($.randomSong)
   // For debugging
-  def randomMp3Song = Action {
-    encodeIfChrome(group(songSelectorState.randomMp3Song)) _
-  }
-  def randomFlacSong = Action {
-    encodeIfChrome(group(songSelectorState.randomFlacSong)) _
-  }
+  def randomMp3Song = run($.randomMp3Song)
+  def randomFlacSong = run($.randomFlacSong)
 
-  private def songsInAlbum(path: String): Seq[Song] =
-    urlPathUtils.parseFile(path) |> IODirectory.apply |> albumFactory.fromDir |> Album.songs.get
-  def album(path: String) = Action {
-    encodeIfChrome(songsInAlbum(path)) _
-  }
-  def discNumber(path: String, requestedDiscNumber: String) = Action {
-    val songsWithDiscNumber =
-      songsInAlbum(path).filter(_.discNumber.exists(requestedDiscNumber ==)).ensuring(_.nonEmpty)
-    encodeIfChrome(songsWithDiscNumber) _
-  }
+  def album(path: String) = run($.album(path))
+  def discNumber(path: String, requestedDiscNumber: String) = run($.discNumber(path, requestedDiscNumber))
 
-  def song(path: String) = Action {
-    encodeIfChrome(group(urlPathUtils parseSong path)) _
-  }
-
-  def nextSong(path: String) = Action {
-    encodeIfChrome(songSelectorState.followingSong(urlPathUtils.parseSong(path)).get) _
-  }
+  def song(path: String) = run($.song(path))
+  def nextSong(path: String) = run($.nextSong(path))
 }
---
+++ app/controllers/PlayerFormatter.scala
@@ -0,0 +1,72 @@
+package controllers
+
+import common.io.IODirectory
+import common.json.{JsonWriteable, ToJsonableOps}
+import common.rich.RichT._
+import controllers.PlayerFormatter.ShouldEncodeMp3Reader
+import decoders.Mp3Encoder
+import javax.inject.Inject
+import models._
+import play.api.libs.json.JsValue
+import songs.{SongGroup, SongGroups, SongSelectorState}
+
+import scala.language.implicitConversions
+
+import scalaz.Reader
+
+private class PlayerFormatter @Inject()(
+    albumFactory: AlbumFactory,
+    groups: SongGroups,
+    songSelectorState: SongSelectorState,
+    encoder: Mp3Encoder,
+    urlPathUtils: UrlPathUtils,
+    songJsonifier: ControllerSongJsonifier,
+) extends ToJsonableOps {
+  import songJsonifier.songJsonable
+
+  private val songGroups: Map[Song, SongGroup] = SongGroups.fromGroups(groups.load)
+  private def group(s: Song): Either[Song, SongGroup] = songGroups get s toRight s
+
+  // Doesn't extend JsonWritable to avoid recursive implicit lookup.
+  private trait Encodable[E] {
+    def encode(e: E): Unit
+    def jsonify(e: E): JsValue
+    def reader(e: E): ShouldEncodeMp3Reader = Reader {b =>
+      if (b)
+        encode(e)
+      jsonify(e)
+    }
+  }
+  private object Encodable {
+    private def jsonableEncodable[A: JsonWriteable](f: A => Unit): Encodable[A] =
+      new Encodable[A] {
+        override def encode(e: A): Unit = f(e)
+        override def jsonify(a: A): JsValue = a.jsonify
+      }
+    implicit val songEncodable: Encodable[Song] = jsonableEncodable(encoder ! _.file)
+    implicit val songsEncodable: Encodable[Seq[Song]] =
+      jsonableEncodable(_ foreach songEncodable.encode)
+    implicit val eitherEncodable: Encodable[Either[Song, SongGroup]] =
+      jsonableEncodable(_.fold(songEncodable.encode, songsEncodable encode _.songs))
+  }
+  private implicit def encodableReader[E: Encodable]($: E): ShouldEncodeMp3Reader =
+    implicitly[Encodable[E]].reader($)
+
+  def randomSong: ShouldEncodeMp3Reader = group(songSelectorState.randomSong)
+  def randomMp3Song: ShouldEncodeMp3Reader = group(songSelectorState.randomMp3Song)
+  def randomFlacSong: ShouldEncodeMp3Reader = group(songSelectorState.randomFlacSong)
+
+  private def songsInAlbum(path: String): Seq[Song] =
+    urlPathUtils.parseFile(path) |> IODirectory.apply |> albumFactory.fromDir |> Album.songs.get
+  def album(path: String): ShouldEncodeMp3Reader = songsInAlbum(path)
+  def discNumber(path: String, requestedDiscNumber: String): ShouldEncodeMp3Reader =
+    songsInAlbum(path).filter(_.discNumber.exists(requestedDiscNumber ==)).ensuring(_.nonEmpty)
+
+  def song(path: String): ShouldEncodeMp3Reader = group(urlPathUtils parseSong path)
+  def nextSong(path: String): ShouldEncodeMp3Reader =
+    songSelectorState.followingSong(urlPathUtils.parseSong(path)).get
+}
+
+object PlayerFormatter {
+  type ShouldEncodeMp3Reader = Reader[Boolean, JsValue]
+}
