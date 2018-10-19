791eb2a3a0ec52ba46de803d779c6af525a5beaa
Gal PC <glpkmtg@gmail.com>
2018-10-19 15:10:10 +0300

    Append year to flac files after extraction

--- app/mains/splitter/FlacSplitter.scala
+++ app/mains/splitter/FlacSplitter.scala
@@ -2,24 +2,47 @@ package mains.splitter
 
 import java.io.File
 
+import common.io.IODirectory
+import common.rich.collections.RichTraversableOnce._
 import common.rich.path.{Directory, RichFileUtils}
 import common.rich.path.RichFile._
 import javax.inject.Inject
+import models.IOMusicFinder
+import org.jaudiotagger.audio.AudioFileIO
+import org.jaudiotagger.tag.FieldKey
 
-//splits cue file and fixes the flac output
-private class FlacSplitter @Inject()(cueSplitter: CueSplitter) {
-  private def clean(output: Directory, destination: Directory): Unit = {
+import scala.collection.JavaConverters._
+
+// Splits cue file and fixes the flac output.
+private class FlacSplitter @Inject()(cueSplitter: CueSplitter, mf: IOMusicFinder) {
+  private def clean(output: Directory, destination: Directory, bigFlac: File): Unit = {
     println("Moving flac files to parent dir")
     output.files.find(_.name == "00. (HTOA).flac").foreach(_.delete())
     output.files.find(_.name == "garbage.cue").foreach(_.delete())
     output.files.filter(_.extension == "flac").foreach(f => RichFileUtils.move(f, destination))
     println("Deleting convert dir")
     output.deleteAll()
+
+    // Append year manually if it doesn't exist, since for some reason the splitters won't
+    lazy val bigFlacAudioFile = AudioFileIO.read(bigFlac)
+
+    bigFlacAudioFile.getTag.getFields(FieldKey.YEAR).asScala.headOption.foreach(year => {
+      mf.getSongFilesInDir(IODirectory(destination)).foreach(f => {
+        val audioFile = AudioFileIO.read(f.file)
+        if (audioFile.getTag.getFields(FieldKey.YEAR).isEmpty) {
+          println("Fixing year on " + f.name)
+          audioFile.getTag.setField(year)
+          audioFile.commit()
+        }
+      })
+    })
   }
 
-  def apply(file: File): Unit = {
-    val dir = cueSplitter(file)
-    clean(dir, file.parent)
+  def apply(cueFile: File): Unit = {
+    val dir = cueFile.parent
+    val bigFlacFile = dir.files.filter(_.extension == "flac").single
+    val output = cueSplitter(cueFile)
+    clean(output, dir, bigFlacFile)
     println("Done")
   }
 }
