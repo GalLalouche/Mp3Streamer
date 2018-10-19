5d32a10424ba266a4b4f4a4539a7feedb57461b1
Gal PC <glpkmtg@gmail.com>
2018-10-19 15:10:00 +0300

    Fixed flac splitting on dirs with "."

--- app/mains/splitter/CueSplitter.scala
+++ app/mains/splitter/CueSplitter.scala
@@ -3,9 +3,10 @@ package mains.splitter
 import java.io.File
 
 import common.rich.path.Directory
+import common.rich.path.RichFile._
 
 private trait CueSplitter {
   def apply(cueFile: File, flacFile: File): Directory
   def apply(cueFile: File): Directory =
-    apply(cueFile, flacFile = new File(cueFile.getAbsolutePath.takeWhile(_ != '.') + ".flac"))
+    apply(cueFile, flacFile = cueFile.parent / (cueFile.nameWithoutExtension + ".flac"))
 }
