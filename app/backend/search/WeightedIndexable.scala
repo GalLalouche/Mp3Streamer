4b46d935dac8b5e038247303c2115f85ac2273c0
Gal PC <glpkmtg@gmmail.com>
2019-02-02 16:51:10 +0200

    Refactor WeightedIndexable

--- app/backend/search/WeightedIndexable.scala
+++ app/backend/search/WeightedIndexable.scala
@@ -1,30 +1,37 @@
 package backend.search
 
+import common.rich.func.ToMoreFoldableOps
 import models._
 import simulacrum.typeclass
 
-@typeclass private trait WeightedIndexable[T] {
-  protected def mainName(t: T): String
-  protected def secondaryName(t: T): String
+import scalaz.std.OptionInstances
+
+@typeclass private trait WeightedIndexable[T]
+    extends ToMoreFoldableOps with OptionInstances {
+  protected def mainTerm(t: T): String
+  protected def secondaryTerms(t: T): Traversable[String]
   private def split(s: String, weight: Double) = s split " " map (_ -> weight)
-  def terms(t: T): Seq[(String, Double)] = split(mainName(t), 1.0) ++ split(secondaryName(t), 0.1)
+  def terms(t: T): Seq[(String, Double)] = split(mainTerm(t), 1.0) ++ secondaryTerms(t).flatMap(split(_, 0.1))
   def sortBy(t: T): Product
 }
 
 private object WeightedIndexable {
+  private def classicalMusicTerms(s: Song): Traversable[String] =
+    s.composer ++ s.orchestra ++ s.conductor ++ s.opus
   implicit object SongIndexer extends WeightedIndexable[Song] {
-    override protected def mainName(t: Song): String = t.title
-    override protected def secondaryName(t: Song): String = t.artistName
+    override protected def mainTerm(t: Song) = t.title
+    override def secondaryTerms(t: Song) =
+      Set(t.artistName) ++ classicalMusicTerms(t)
     override def sortBy(s: Song): Product = (s.artistName, s.title, s.year, s.albumName, s.track)
   }
   implicit object AlbumIndex extends WeightedIndexable[Album] {
-    override protected def mainName(t: Album): String = t.title
-    override protected def secondaryName(t: Album): String = t.artistName
+    override protected def mainTerm(t: Album) = t.title
+    override protected def secondaryTerms(t: Album) = t.songs.flatMap(SongIndexer.secondaryTerms).toSet
     override def sortBy(a: Album): Product = (a.artistName, a.year, a.title)
   }
   implicit object ArtistIndex extends WeightedIndexable[Artist] {
-    override protected def mainName(t: Artist): String = t.name
-    override protected def secondaryName(t: Artist): String = ""
+    override protected def mainTerm(t: Artist) = t.name
+    override protected def secondaryTerms(t: Artist) = Vector.empty
     override def sortBy(a: Artist): Product = Tuple1(a.name)
   }
 }
