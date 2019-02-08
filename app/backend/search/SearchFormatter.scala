949557ea6f223a309a9944c7b912ad1f38c43ee5
Gal PC <glpkmtg@gmmail.com>
2019-02-08 14:02:09 +0200

    Append classical tags to album where uniform

--- app/backend/search/SearchFormatter.scala
+++ app/backend/search/SearchFormatter.scala
@@ -8,12 +8,18 @@ import javax.inject.Inject
 import models.{Album, ModelJsonable}
 import models.ModelJsonable.{ArtistJsonifier, SongJsonifier}
 import play.api.libs.json.{JsObject, Json}
+import common.RichJson._
 
 private class SearchFormatter @Inject()(state: SearchState) extends ToJsonableOps {
   private implicit val albumJsonableWithDiscNumber: OJsonable[Album] =
     JsonableOverrider.oJsonify[Album]((a, original) => {
       original.mapIf(a.songs.forall(_.discNumber.isDefined)) // All songs need to have a disc number (ignores bonus disc only)
           .to(_ + ("discNumbers" -> a.songs.map(_.discNumber.get).distinct.sorted.jsonify))
+          .append("composer" -> a.composer)
+          .append("conductor" -> a.conductor)
+          .append("opus" -> a.opus)
+          .append("orchestra" -> a.orchestra)
+          .append("performanceYear" -> a.performanceYear)
     })(ModelJsonable.AlbumJsonifier)
 
   def search(path: String): JsObject = {
--- app/models/Album.scala
+++ app/models/Album.scala
@@ -4,4 +4,19 @@ import common.io.DirectoryRef
 import monocle.macros.Lenses
 
 @Lenses
-case class Album(dir: DirectoryRef, title: String, artistName: String, year: Int, songs: Seq[Song])
+case class Album(dir: DirectoryRef, title: String, artistName: String, year: Int, songs: Seq[Song]) {
+  /**
+  * Returns the property if all songs share the same property, e.g., all songs in the album have the same
+  * composer. Otherwise, returns None.
+  */
+  def uniformProperty[A](f: Song => Option[A]): Option[A] = {
+    val set = songs.map(f).toSet
+    if (set.size == 1) set.head else None
+  }
+
+  def composer: Option[String] = uniformProperty(_.composer)
+  def conductor: Option[String] = uniformProperty(_.conductor)
+  def opus: Option[String] = uniformProperty(_.opus)
+  def orchestra: Option[String] = uniformProperty(_.orchestra)
+  def performanceYear: Option[Int] = uniformProperty(_.performanceYear)
+}
