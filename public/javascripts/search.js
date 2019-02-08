eeb52bb27dc4e3bd689ac7933bf41d8d1ff531dc
Gal PC <glpkmtg@gmmail.com>
2019-02-08 22:31:23 +0200

    Small refactors of search.js

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,11 +1,12 @@
 $(function() {
+  const PLAY = "play"
+  const ADD = "plus"
+  const ADD_ENTIRE_ALBUM = "plus-square"
+  const ADD_DISC = "plus-circle"
+  const DOWNLOAD_FILE = "download"
+
   const searchBox = $("#searchbox");
   const results = $("#search-results")
-  const play = "play"
-  const add = "plus"
-  const addEntireAlbum = "plus-square"
-  const addDisc = "plus-circle"
-  const downloadFile = "download"
 
   function setResults(jsArray, requestTime) {
     results.show()
@@ -24,11 +25,12 @@ $(function() {
     }
 
     if (parseInt(results.attr("time")) > requestTime)
-      return // a later request has already set the result
+      return // A later request has already set the result.
     results.attr("time", requestTime)
-    function songItem(song) {
+
+    specificResults("song", function(song) {
       function suffix() {
-        if (!song.composer)
+        if (!song.composer) // Assumes all classical pieces have a composer field.
           return `${song.artistName}: ${song.title} (${song.duration.timeFormat()})`
 
         // TODO handle code duplication of all the toStrings for composers
@@ -38,14 +40,14 @@ $(function() {
         const opus = song.opus ? `, ${song.opus}` : ''
         return `${base}, ${pieceTitle}${opus}`
       }
-      return `${icon(add)} ${icon(play)} ${suffix()}`
-    }
-    specificResults("song", songItem)
+      return `${icon(ADD)} ${icon(PLAY)} ${suffix()}`
+    })
     $.each($(".song-result"), function() {
       const song = $(this).data()
       $(this).custom_tooltip(`${song.year}, ${song.albumName}, ${song.track}`)
     })
-    function albumItem(album) {
+
+    specificResults("album", function(album) {
       function albumText() {
         if (!album.composer) // Assumes all classical pieces have a composer field.
           return `${album.artistName}: ${album.year || "NO_YEAR"} ${album.title}`
@@ -55,22 +57,22 @@ $(function() {
         const base = `${album.artistName}: ${pieceTitle}`
         const opus = album.opus ? `, ${album.opus}` : ''
         const other = [album.performanceYear, album.conductor, album.orchestra].filter(x => x)
-        return base + opus + (other ? ` (${other.join(", ")})`: '')
+        return base + opus + (other ? ` (${other.join(", ")})` : '')
       }
-      const item = `${icon(addEntireAlbum)} ${icon(downloadFile)} ` + albumText()
+      const item = `${icon(ADD_ENTIRE_ALBUM)} ${icon(DOWNLOAD_FILE)} ` + albumText()
       if (album.discNumbers) {
-        const discNumberElements = album.discNumbers.map(d => `<span>${icon(addDisc)}${d}</span>`).join(" ")
+        const discNumberElements = album.discNumbers.map(d => `<span>${icon(ADD_DISC)}${d}</span>`).join(" ")
         return item + "<br>&emsp;&emsp;" + discNumberElements
       } else
         return item
-    }
-    specificResults("album", albumItem)
-    specificResults("artist", _ => "")
+    })
+
+    specificResults("artist", () => "")
     $.each($(".artist-result"), function() {
       const li = $(this)
       const artist = li.data()
       const albums = div().appendTo(li)
-      specificResults("album", a => `${icon(add)} ${a.year} ${a.title}`, albums, artist.albums)
+      specificResults("album", a => `${icon(ADD)} ${a.year} ${a.title}`, albums, artist.albums)
       li.accordion({
         collapsible: true,
         active: false,
@@ -82,8 +84,8 @@ $(function() {
   function clearResults() {
     searchBox.val('')
     results.hide()
-    // TODO can we change attr automatically? Do we want to?
-    results.attr("time", updateTimeOfLastInput()) // ensure late results will be ignored
+    // TODO Can we change attr automatically? Do we want to?
+    results.attr("time", updateTimeOfLastInput()) // Ensure late results will be ignored.
   }
 
   function scan() {
@@ -92,7 +94,7 @@ $(function() {
       openConnection("refresh", function(msg, connection) {
         try {
           $.toast("Found new directory: " + JSON.parse(msg.data).currentDir)
-        } catch (e) {
+        } catch (ignored) {
           if (msg.data.includes("Finished")) {
             connection.close()
             search()
@@ -107,16 +109,16 @@ $(function() {
     const isPlay = e.target.classList.contains("fa-play")
     $.get("data/songs/" + song.file, e => gplaylist.add(e, isPlay))
   })
-  results.on("click", `.album-result .fa-${addEntireAlbum}`, function() {
+  results.on("click", `.album-result .fa-${ADD_ENTIRE_ALBUM}`, function() {
     const album = $(this).parent().data()
     $.get("data/albums/" + album.dir, e => gplaylist.add(e, false))
   })
-  results.on("click", `.album-result .fa-${addDisc}`, function() {
+  results.on("click", `.album-result .fa-${ADD_DISC}`, function() {
     const album = $(this).parent().parent().data()
     const discNumber = $(this).parent().text()
     $.get(`data/discs/${discNumber}/${album.dir}`, e => gplaylist.add(e, false))
   })
-  results.on("click", `.album-result .fa-${downloadFile}`, function() {
+  results.on("click", `.album-result .fa-${DOWNLOAD_FILE}`, function() {
     const album = $(this).parent().data()
     $.get("download/" + album.dir)
   })
@@ -146,8 +148,9 @@ $(function() {
     scan()
   }))
 
-  // Blur search box after enough time has passed and it wasn't updated. By blurring the box, keyboard shortcuts are
-  // Re-enabled. This way, after 10 minutes of playing, you can still press 'K' to pause the damn thing.
+  // Blur search box after enough time has passed and it wasn't updated. By blurring the box, keyboard
+  // shortcuts are Re-enabled. This way, after 10 minutes of playing, you can still press 'K' to pause the
+  // damn thing.
   const INPUT_TIMEOUT_IN_MILLIS = 10000
   setInterval(function() {
     if (Date.now() - timeOfLastInput > INPUT_TIMEOUT_IN_MILLIS)
