dfdbeefc5916390934c1bbcacfbf6fdf3d2b1d62
Gal PC <glpkmtg@gmmail.com>
2019-02-09 13:17:20 +0200

    Move unerasedClass to common

--- app/common/ModuleUtils.scala
+++ app/common/ModuleUtils.scala
@@ -1,8 +1,9 @@
 package common
 
-import com.google.inject.{Binder, Provider, TypeLiteral}
 import com.google.inject.assistedinject.FactoryModuleBuilder
 import com.google.inject.spi.{InjectionListener, TypeEncounter, TypeListener}
+import com.google.inject.{Binder, Provider, TypeLiteral}
+import common.rich.primitives.RichClass._
 import net.codingwell.scalaguice.{typeLiteral, InternalModule}
 
 //noinspection UnitMethodIsParameterless
@@ -13,19 +14,15 @@ trait ModuleUtils {self: InternalModule[_ <: Binder] =>
     binder.install(new FactoryModuleBuilder().build(manifest.runtimeClass))
   }
 
-  // TODO move to RichClass or richT or something
-  private def unerasedClass[A: Manifest]: Class[A] = manifest.runtimeClass.asInstanceOf[Class[A]]
   protected def install[Source: Manifest, Target <: Source : Manifest, Factory: Manifest]: Unit = {
     binder.install(new FactoryModuleBuilder()
-        .implement(
-          typeLiteral[Source],
-          typeLiteral[Target])
-        .build(unerasedClass[Factory]))
+        .implement(typeLiteral[Source], typeLiteral[Target])
+        .build(manifest[Factory].unerasedClass))
   }
 
   protected def requireBinding[A: Manifest]: Unit = binder.getProvider(manifest.runtimeClass)
 
-  protected def provider[A: Manifest]: Provider[A] = binder.getProvider(unerasedClass[A])
+  protected def provider[A: Manifest]: Provider[A] = binder.getProvider(manifest.unerasedClass)
 
   def typeListener[A: Manifest](f: A => Any): TypeListener with InjectionListener[A] =
     new TypeListener with InjectionListener[A] {
