ba5c1413113a4071c23f9fafeabbaa510ed5d5a8
Gal PC <glpkmtg@gmmail.com>
2019-02-19 20:25:59 +0200

    Sort discNumbers by track number in search

--- app/backend/search/SearchFormatter.scala
+++ app/backend/search/SearchFormatter.scala
@@ -3,28 +3,38 @@ package backend.search
 import java.net.URLDecoder
 
 import common.json.{JsonableOverrider, OJsonable, ToJsonableOps}
-import common.rich.RichT._
+import common.RichJson._
+import common.rich.func.MoreTraverseInstances
 import javax.inject.Inject
 import models.{Album, ModelJsonable}
 import models.ModelJsonable.{ArtistJsonifier, SongJsonifier}
 import play.api.libs.json.{JsObject, Json}
-import common.RichJson._
+
+import scalaz.std.OptionInstances
+import scalaz.syntax.ToTraverseOps
 
 private class SearchFormatter @Inject()(state: SearchState) extends ToJsonableOps {
   private implicit val albumJsonableWithDiscNumber: OJsonable[Album] =
-    JsonableOverrider.oJsonify[Album]((a, original) => {
-      original.mapIf(a.songs.forall(_.discNumber.isDefined)) // All songs need to have a disc number (ignores bonus disc only)
-          .to(_ + ("discNumbers" -> a.songs.map(_.discNumber.get).distinct.sorted.jsonify))
-          .append("composer" -> a.composer)
-          .append("conductor" -> a.conductor)
-          .append("opus" -> a.opus)
-          .append("orchestra" -> a.orchestra)
-          .append("performanceYear" -> a.performanceYear)
-    })(ModelJsonable.AlbumJsonifier)
+    JsonableOverrider.oJsonify[Album]((a, original) => original
+        .append("discNumbers", SearchFormatter.discNumbers(a))
+        .append("composer" -> a.composer)
+        .append("conductor" -> a.conductor)
+        .append("opus" -> a.opus)
+        .append("orchestra" -> a.orchestra)
+        .append("performanceYear" -> a.performanceYear)
+    )(ModelJsonable.AlbumJsonifier)
 
   def search(path: String): JsObject = {
     val terms = URLDecoder.decode(path, "UTF-8").split(" ").map(_.toLowerCase)
     val (songs, albums, artists) = state search terms
     Json obj("songs" -> songs.jsonify, "albums" -> albums.jsonify, "artists" -> artists.jsonify)
   }
+}
+
+object SearchFormatter extends ToTraverseOps with MoreTraverseInstances with OptionInstances {
+  // All songs need to have a disc number (ignores bonus disc only).
+  private def discNumbers(a: Album): Option[Seq[String]] =
+    a.songs.traverse(s => s.discNumber.map(_ -> s.track))
+        // Sort disc numbers by track order.
+        .map(_.sortBy(_._2).map(_._1).distinct)
 }
