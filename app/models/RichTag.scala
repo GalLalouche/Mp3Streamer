e199255e49a94c4a04cdf8d0e998c1313105d47f
Gal PC <glpkmtg@gmmail.com>
2019-02-22 18:59:14 +0200

    RichTag.setOption

--- app/models/RichTag.scala
+++ app/models/RichTag.scala
@@ -10,6 +10,10 @@ object RichTag {
   private val customPattern = """Description="([^"]+)"; Text="([^"]+)"; """.r
   implicit class richTag(private val $: Tag) extends AnyVal {
     private def filterNonEmpty(s: String): Option[String] = s.trim.opt.filter(_.nonEmpty)
+    def setOption(key: FieldKey, o: Option[Any]): Tag = {
+      o.foreach(f => $.setField(key, f.toString))
+      $
+    }
     def firstNonEmpty(key: String): Option[String] = $.getFirst(key) |> filterNonEmpty
     def firstNonEmpty(key: FieldKey): Option[String] = $.getFirst(key) |> filterNonEmpty
     def customTags: Map[String, String] = $.getFields("TXXX").asScala
