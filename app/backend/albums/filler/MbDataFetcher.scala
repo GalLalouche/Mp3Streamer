af730256c716d49ffd856f364153342620eb6ff7
Gal PC <glpkmtg@gmmail.com>
2020-05-23 18:06:16 +0300

    Remove extra rate limiting in MbDataFetcher
    No longer necessary since JsonDownloader is already rate limited.
    
    GitHub: Fixes #12

--- app/backend/albums/filler/MbDataFetcher.scala
+++ app/backend/albums/filler/MbDataFetcher.scala
@@ -10,20 +10,15 @@ import scala.concurrent.{ExecutionContext, Future}
 import common.concurrency.{DaemonFixedPool, SimpleTypedActor}
 import common.rich.RichT._
 
-// Ensures MusicBrainz aren't flooded since:
-// 1. A limited number of WS can be used at any given time (single threaded actors).
-// 2. A request for a client has a delay.
 @Singleton private class MbDataFetcher @Inject()(
     logger: Logger,
     meta: MbArtistReconciler,
     finisher: AlbumFinisher,
 ) extends SimpleTypedActor[(Artist, ReconID), Seq[NewAlbumRecon]] {
-  private val other: ExecutionContext = DaemonFixedPool(this.simpleName + "-other", 10)
-  private val internet = SimpleTypedActor.async[(Artist, ReconID), Seq[NewAlbumRecon]](
-    this.simpleName + "-internet", {case (artist: Artist, recon: ReconID) =>
-      Thread sleep 2000
-      logger.verbose(s"Performing fetch for artist <$artist>")
-      meta.getAlbumsMetadata(recon).flatMap(finisher(artist, _))(other)
-    })
-  override def !(m: => (Artist, ReconID)): Future[Seq[NewAlbumRecon]] = internet ! m
+  private val ec: ExecutionContext = DaemonFixedPool(this.simpleName, 10)
+  override def !(m: => (Artist, ReconID)): Future[Seq[NewAlbumRecon]] = {
+    val (artist, recon) = m
+    logger.verbose(s"Performing fetch for artist <$artist>")
+    meta.getAlbumsMetadata(recon).flatMap(finisher(artist, _))(ec)
+  }
 }
