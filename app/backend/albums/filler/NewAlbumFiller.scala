3a0a963060269a05963ec96c54c53416731da5bb
Gal PC <glpkmtg@gmmail.com>
2020-05-24 22:25:03 +0300

    Update JSON after fetching new albums
    Whoops, somehow forgot about this

--- app/backend/albums/filler/NewAlbumFiller.scala
+++ app/backend/albums/filler/NewAlbumFiller.scala
@@ -1,27 +1,35 @@
 package backend.albums.filler
 
-import backend.logging.{FilteringLogger, LoggingLevel}
+import backend.logging.{FilteringLogger, Logger, LoggingLevel}
 import javax.inject.Inject
 import net.codingwell.scalaguice.InjectorExtensions._
 
 import scala.concurrent.{ExecutionContext, Future}
 
-import scalaz.syntax.functor.ToFunctorOps
 import scalaz.syntax.traverse.ToTraverseOps
 import common.rich.func.BetterFutureInstances._
 import common.rich.func.MoreTraverseInstances._
+import common.rich.func.ToMoreFunctorOps
 
 import common.concurrency.DaemonFixedPool
+import common.io.JsonableSaver
 import common.rich.RichFuture._
 import common.rich.RichT._
 
 private class NewAlbumFiller @Inject()(
     cache: ExistingAlbums,
     newAlbumFetcher: NewAlbumFetcher,
+    logger: Logger,
+    writer: JsonableSaver,
 ) {
   private implicit val ec: ExecutionContext = DaemonFixedPool(this.simpleName, 10)
-
-  def go(): Future[_] = cache.artists.traverse(newAlbumFetcher.apply).void
+  def go(): Future[_] = cache.artists
+      .traverse(ToMoreFunctorOps.toProduct(newAlbumFetcher.apply))
+      .map {albums =>
+        val newAlbums = albums.flatMap(_._2.map(_.newAlbum))
+        logger.info(s"Saving new album index with <${newAlbums.size}> new albums")
+        writer.save(newAlbums)
+      }
 }
 
 private object NewAlbumFiller {
