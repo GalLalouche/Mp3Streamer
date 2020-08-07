022919da04f84c20fd6686abf338bde939108b2e
Gal PC <glpkmtg@gmmail.com>
2020-08-07 12:19:18 +0300

    Reduce icon size so to fit more genres

--- app/mains/fixer/NewArtistFolderCreator.scala
+++ app/mains/fixer/NewArtistFolderCreator.scala
@@ -7,6 +7,9 @@ import scala.concurrent.{Future, Promise}
 import scala.swing.{BoxPanel, Component, Frame, GridPanel, Label, Orientation}
 import scala.swing.event.WindowClosing
 
+import scalaz.std.option.optionInstance
+import common.rich.func.ToMoreFoldableOps._
+
 import common.rich.path.Directory
 import common.rich.primitives.RichBoolean._
 import common.rich.RichT._
@@ -16,35 +19,35 @@ import common.rich.RichT._
 * view.
 */
 private object NewArtistFolderCreator {
-  private val NumberOfLabelRows = 1
   def apply(artistName: String): Future[Directory] = {
     val promise = Promise[Directory]()
+
     val frame = new Frame {
       reactions += {case _: WindowClosing => promise.failure(new Exception("User closed the window"))}
     }
     def genres(genre: String) = Directory("d:/media/music/" + genre).dirs
+    // TODO add jazz
     val rockDirs = genres("Rock")
     val metalDirs = genres("Metal")
     frame.contents = new GridPanel(
-      rows0 = Math.max(rockDirs.length, metalDirs.length) + NumberOfLabelRows,
+      rows0 = Math.max(rockDirs.length, metalDirs.length),
       cols0 = 2,
     ) {
-      private def genreComponent(d: Directory): Component =
-        if (d == null) new Label("") else new BoxPanel(Orientation.Horizontal) {
+      private def genreComponent(d: Option[Directory]): Component = d.mapHeadOrElse(
+        d => new BoxPanel(Orientation.Horizontal) {
           val folderImage = d.\("folder.jpg").mapIf(_.exists().isFalse).to(d / "folder.png")
-          contents += ImageIO.read(folderImage).toSquareImageIcon(40).toComponent
+          contents += ImageIO.read(folderImage).toSquareImageIcon(side = 35).toComponent
           contents += new Label(d.name)
         }.onMouseClick {() =>
           frame.dispose()
           promise.success(d addSubDir artistName)
-        }
-      contents += new Label("Rock")
-      contents += new Label("Metal")
-      // ensures correct order in the grid
-      for ((d1, d2) <- rockDirs.zipAll(metalDirs, null, null)) {
-        contents += genreComponent(d1)
-        contents += genreComponent(d2)
-      }
+        },
+        new Label(""),
+      )
+      for {
+        (d1, d2) <- rockDirs.zipAll(metalDirs, null, null) // Ensures correct order in the grid.
+        d <- Vector(d1, d2)
+      } contents += genreComponent(Option(d))
     }
     frame.open()
     promise.future
