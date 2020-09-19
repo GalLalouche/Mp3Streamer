26f650fc82ccbedc50508d5271ee75fc48723e11
Gal PC <glpkmtg@gmmail.com>
2020-09-19 15:00:21 +0300

    MoreInvariantFunctorOps

--- app/backend/albums/filler/storage/LastFetchTimeImpl.scala
+++ app/backend/albums/filler/storage/LastFetchTimeImpl.scala
@@ -2,6 +2,7 @@ package backend.albums.filler.storage
 
 import java.time.{Clock, LocalDateTime, ZoneOffset}
 
+import backend.albums.filler.storage.LastFetchTimeImpl.prependUnit
 import backend.module.StandaloneModule
 import backend.recon.Artist
 import backend.storage.{ComposedFreshnessStorage, DatedFreshness, Freshness}
@@ -11,9 +12,10 @@ import net.codingwell.scalaguice.InjectorExtensions.ScalaInjector
 
 import scala.concurrent.ExecutionContext
 
-import scalaz.syntax.invariantFunctor.ToInvariantFunctorOps
 import scalaz.Scalaz.{ToBindOps, ToFunctorOps}
 import common.rich.func.BetterFutureInstances._
+import common.rich.func.ToMoreInvariantFunctorOps._
+import monocle.Iso
 
 import common.rich.RichFuture.richFuture
 import common.storage.Storage
@@ -24,14 +26,11 @@ private class LastFetchTimeImpl @Inject()(
     ec: ExecutionContext,
 ) extends LastFetchTime {
   private implicit val iec: ExecutionContext = ec
-  // TODO Iso => xmap
-  val xmapped: Storage[Artist, (Unit, Freshness)] =
-    storage.asInstanceOf[Storage[Artist, Option[LocalDateTime]]].xmap(
-      o => (Unit, Freshness.iso.reverse(o)), f => Freshness.iso(f._2)
-    )
+  private val xmapped: Storage[Artist, (Unit, Freshness)] =
+    storage.asInstanceOf[Storage[Artist, Option[LocalDateTime]]].xmapmi(Freshness.iso ^<-> prependUnit)
   private val aux = new ComposedFreshnessStorage[Artist, Unit](xmapped, clock)
   override def update(a: Artist) = aux.update(a, ()).run.void
-  override def ignore(a: Artist) = aux.storeWithoutTimestamp(a, ())
+  override def ignore(a: Artist) = aux.delete(a).run >> aux.storeWithoutTimestamp(a, ())
   override def freshness(a: Artist) = aux.freshness(a)
   override def reset(a: Artist) = {
     val time = LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.UTC)
@@ -40,6 +39,8 @@ private class LastFetchTimeImpl @Inject()(
 }
 
 private object LastFetchTimeImpl {
+  private def prependUnit[A] = Iso[A, (Unit, A)](().->)(_._2)
+
   def main(args: Array[String]): Unit = {
     val injector = Guice.createInjector(StandaloneModule, FillerStorageModule)
     implicit val ec: ExecutionContext = injector.instance[ExecutionContext]
