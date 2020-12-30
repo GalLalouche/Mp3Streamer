0ff84bdb03c3c90fe6a92c122c792cbf66dbba0b
Gal (PC) <>
2020-12-30 17:10:40 +0200

    Extract NewAlbumFiller runner to an ignored launcher

--- app/backend/albums/filler/NewAlbumFiller.scala
+++ app/backend/albums/filler/NewAlbumFiller.scala
@@ -1,22 +1,18 @@
 package backend.albums.filler
 
-import common.rich.RichFuture._
 import java.time.{Clock, Duration}
 
 import backend.albums.filler.storage.CachedNewAlbumStorage
-import backend.logging.{FilteringLogger, Logger, LoggingLevel}
+import backend.logging.Logger
 import backend.recon.Artist
 import backend.storage.{AlwaysFresh, DatedFreshness}
 import javax.inject.{Inject, Singleton}
-import net.codingwell.scalaguice.InjectorExtensions.ScalaInjector
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.math.Ordered.orderingToOrdered
 
-import scalaz.syntax.foldable.ToFoldableOps
 import scalaz.Scalaz.ToBindOps
 import common.rich.func.BetterFutureInstances.betterFutureInstances
-import common.rich.func.MoreIterableInstances._
 import common.rich.func.ToMoreFunctorOps.toMoreFunctorOps
 
 import common.concurrency.SimpleTypedActor
@@ -34,7 +30,7 @@ private class NewAlbumFiller @Inject()(
   // TODO code duplication with RefreshableRetriever
   private val finisher = SimpleTypedActor.async[(Seq[NewAlbumRecon], Set[Artist]), Int](
     "CacherFiller finisher", Function.tupled(storage.storeNew))
-  def update(a: Artist): Future[Int] = storage.freshness(a)
+  def update(maxAge: Duration)(a: Artist): Future[Int] = storage.freshness(a)
       .getOrElseF {
         logger.verbose(s"Storing new artist <$a>")
         storage.reset(a)
@@ -46,7 +42,7 @@ private class NewAlbumFiller @Inject()(
         case DatedFreshness(dt) =>
           val age = dt.age(clock)
           val info = s"<$a> because it is <${age.toDays}> days old (from <${dt.toLocalDate}>)"
-          if (age > NewAlbumFiller.MaxAge) {
+          if (age > maxAge) {
             logger.debug(s"Fetching new data for $info")
             storage.unremoveAll(a) >> fetcher(a).flatMap(finisher.!(_, Set(a)))
           } else {
@@ -55,19 +51,4 @@ private class NewAlbumFiller @Inject()(
           }
       }
       .listen(stored => if (stored > 0) logger.debug(s"Stored <$stored> new albums."))
-}
-private object NewAlbumFiller {
-  import common.rich.func.NumericMonoids.SumNumeric
-
-  // TODO extract to a non-git managed config/runner.
-  private val MaxAge = Duration.ofDays(90)
-
-  def main(args: Array[String]): Unit = {
-    val injector = ExistingAlbumsModules.overridingStandalone(ExistingAlbumsModules.default)
-    injector.instance[FilteringLogger].setCurrentLevel(LoggingLevel.Debug)
-    val e = injector.instance[NewAlbumFiller]
-    val artists = injector.instance[ExistingAlbums].artists
-    implicit val ec: ExecutionContext = injector.instance[ExecutionContext]
-    println(s"Added a total of <${artists.foldMapM(e.update).get}> new albums!")
-  }
 }
