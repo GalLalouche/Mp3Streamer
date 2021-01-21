49b6052707d6ee4e82ce66e2ac16b016b49c7750
Gal PC <glpkmtg@gmmail.com>
2021-01-21 19:15:31 +0200

    More options in FuzzyMatch
    GitHub: Fixes #48

--- app/mains/fixer/new_artist/FuzzyMatch.scala
+++ app/mains/fixer/new_artist/FuzzyMatch.scala
@@ -7,9 +7,17 @@ private object FuzzyMatch {
     @tailrec def go(source: List[Char], dest: List[Char]): Boolean = (source, dest) match {
       case (Nil, _) => true
       case (_, Nil) => false
+      case ('\'' :: st, dt) =>
+        val (sFirst, sRem) = st.splitAt(st.indexOf(' '))
+        val (dFirst, dRem) = dt.splitAt(st.indexOf(' '))
+        sFirst == dFirst && go(sRem, dRem)
       case (sh :: st, dh :: dt) => go(if (sh == dh) st else source, dt)
     }
 
+    if (source.isEmpty)
+      return true
+    if (source.head == '^')
+      return apply(source.slice(1, 2), dest.take(1)) && apply(source.drop(2), dest.tail)
     go(source.toList, (if (source.exists(_.isUpper)) dest else dest.toLowerCase).toList)
   }
 }
--- test/mains/fixer/new_artist/FuzzyMatchTest.scala
+++ test/mains/fixer/new_artist/FuzzyMatchTest.scala
@@ -2,42 +2,71 @@ package mains.fixer.new_artist
 
 import org.scalatest.FreeSpec
 
+import common.rich.primitives.RichBoolean.richBoolean
 import common.test.AuxSpecs
 
 class FuzzyMatchTest extends FreeSpec with AuxSpecs {
+  private implicit class MatchedString($: String) {
+    def fuzzyMatches(dest: String) = assert(FuzzyMatch($, dest), "Should have fuzzy matched")
+    def doesNotFuzzyMatch(dest: String) = assert(FuzzyMatch($, dest).isFalse, "Should not have fuzzy matched")
+  }
   "non-empty with empty string is no match" in {
-    FuzzyMatch("foo", "") shouldReturn false
+    "foo" doesNotFuzzyMatch ""
   }
   "empty with non-empty string is match" in {
-    FuzzyMatch("", "foo") shouldReturn true
+    "" fuzzyMatches "foo"
   }
-  "empty with empty string is no match" in {
-    FuzzyMatch("", "") shouldReturn true
+  "empty with empty string is a match" in {
+    "" fuzzyMatches ""
   }
   "exact string is match" in {
-    FuzzyMatch("foo", "foo") shouldReturn true
+    "foo" fuzzyMatches "foo"
   }
   "no fuzz is no match" in {
-    FuzzyMatch("foo", "bar") shouldReturn false
+    "foo" doesNotFuzzyMatch "bar"
   }
   "match on prefix" in {
-    FuzzyMatch("foo", "foobar") shouldReturn true
+    "foo" fuzzyMatches "foobar"
   }
   "match on suffix" in {
-    FuzzyMatch("foo", "barfoo") shouldReturn true
+    "foo" fuzzyMatches "barfoo"
   }
   "match on fuzzy" in {
-    FuzzyMatch("foo", "f1o2o3") shouldReturn true
+    "foo" fuzzyMatches "f1o2o3"
   }
-  "Smart casing" - {
-    "No match due to upper case in source" in {
-      FuzzyMatch("Foo", "f1o2o3") shouldReturn false
+  "Other options" - {
+    "Smart casing" - {
+      "No match due to upper case in source" in {
+        "Foo" doesNotFuzzyMatch "f1o2o3"
+      }
+      "match due to ignoring case in dest when there is no casing in source" in {
+        "foo" fuzzyMatches "F1O2O3"
+      }
+      "Matching with casing" in {
+        "fOo" fuzzyMatches "fo1O2o3"
+      }
     }
-    "match due to ignoring case in dest when there is no casing in source" in {
-      FuzzyMatch("foo", "F1O2O3") shouldReturn true
+    "Starting ^" - {
+      "Will match if the prefixes match" in {
+        "^fbr" fuzzyMatches "foobar"
+      }
+      "Will not match if the prefixes don't match" in {
+        "^oobr" doesNotFuzzyMatch "foobar"
+      }
+      "Is not case sensitive by default" in {
+        "^fbr" fuzzyMatches "FOobaR"
+      }
     }
-    "Matching with casing" in {
-      FuzzyMatch("fOo", "fo1O2o3") shouldReturn true
+    "Literal '" - {
+      "Will match if the whole word matches" in {
+        "'foo bar" fuzzyMatches "foo bzarzazr"
+      }
+      "Will not match if the whole word does not matches" in {
+        "'foo bar" doesNotFuzzyMatch "foobzarzazr"
+      }
+      "Can use ' in the middle of the word" in {
+        "moo 'foo bar" doesNotFuzzyMatch "maasd8ejasdaoasd8asdjofooasdfjasdfbaasdfjr"
+      }
     }
   }
 }
