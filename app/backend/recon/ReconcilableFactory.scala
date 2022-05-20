c4d45042d6ad32b8d86a001d8479b74a35998ae5
Gal PC <glpkmtg@gmmail.com>
2022-05-20 13:06:29 +0300

    Move ReconcilableFactory directory renames to a file, add a few more names

--- app/backend/recon/ReconcilableFactory.scala
+++ app/backend/recon/ReconcilableFactory.scala
@@ -4,24 +4,25 @@ import backend.recon.Reconcilable.SongExtractor
 import javax.inject.Inject
 import models.MusicFinder
 
+import scala.io.Source
 import scala.util.{Failure, Success, Try}
 
 import common.io.DirectoryRef
+import common.json.ToJsonableOps.jsonifyString
 import common.rich.RichT._
 import common.rich.primitives.RichOption.richOption
 
 class ReconcilableFactory @Inject()(val mf: MusicFinder) {
   type S = mf.S
-  private val invalidDirectoryNames: Map[String, String] = Map(
-    // Some artists have invalid directory characters in their name, so their directory won't match
-    // the artist name. As a stupid hack, just aggregate them below.
-    // Surely a coincidence they all start with A o_O
-    "ArchMatheos" -> "Arch / Matheos",
-    "AndersonStolt" -> "Anderson/Stolt",
-    "ACDC" -> "AC/DC",
-  )
-  def dirNameToArtist(dirName: String): Artist =
-    Artist(dirName optionOrKeep invalidDirectoryNames.get)
+  // Some artists have invalid directory characters in their name, so their directory won't match
+  // the artist name. As a stupid hack, just aggregate them below.
+  private val invalidDirectoryNames: Map[String, String] = Source
+      .fromInputStream(getClass.getResourceAsStream("directory_renames.json"), "utf-8")
+      .getLines
+      .map(_.parseJsonable[Seq[String]].toVector.ensuring(_.size == 2))
+      .map {case Vector(dirName, name) => (dirName, name)}
+      .toMap
+  def dirNameToArtist(dirName: String): Artist = Artist(dirName optionOrKeep invalidDirectoryNames.get)
   // This is Try so the error could be reserved.
   def toAlbum(dir: DirectoryRef): Try[Album] =
     if (dir.name.take(4).exists(_.isDigit)) {
