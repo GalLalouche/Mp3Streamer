45a830a75ca6885220295c4184a09881d10765d9
Gal-PC\Gal <gal.lalouche@gmail.com>
2023-08-12 16:05:53 +0300

    ScoreBasedFilteR: Nicer logging, made verbose instead of debug

--- app/songs/selector/ScoreBasedFilter.scala
+++ app/songs/selector/ScoreBasedFilter.scala
@@ -2,12 +2,14 @@ package songs.selector
 
 import backend.logging.Logger
 import backend.scorer.{CachedModelScorer, ScoreBasedProbability}
+import backend.scorer.FullInfoScore.Scored
 import javax.inject.Inject
 import models.Song
 
 import scala.util.Random
 
 import common.Filter
+import common.rich.RichT.richT
 
 private class ScoreBasedFilter @Inject()(
     random: Random,
@@ -17,11 +19,13 @@ private class ScoreBasedFilter @Inject()(
 ) extends Filter[Song] {
   override def passes(song: Song): Boolean = {
     val percentage = scoreBasedProbability(song)
-    val score = cachedModelScorer(song)
-    val shortSongString = s"${song.artistName} - ${song.title} (${score.orDefaultString})"
+    val fullInfoScore = cachedModelScorer.fullInfo(song)
+    val score = fullInfoScore.toModelScore
+    val source = fullInfoScore.safeCast[Scored].map(_.source).getOrElse("N/A")
+    val shortSongString = s"${song.artistName} - ${song.title} (${score.orDefaultString}, $source)"
     val $ = percentage.roll(random)
-    if ($) logger.debug(s"Chose song <$shortSongString> with probability $percentage")
-    else logger.debug(s"Skipped song <$shortSongString> with probability ${percentage.inverse}")
+    if ($) logger.verbose(s"Chose song <$shortSongString> with probability $percentage")
+    else logger.verbose(s"Skipped song <$shortSongString> with probability ${percentage.inverse}")
     $
   }
 }
