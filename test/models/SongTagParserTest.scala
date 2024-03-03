cd75ead74d540498496a7d0d641f4ae22ed23092
Gal (Windows) <>
2024-03-03 15:28:13 +0200

    SongTagParser: Do not throw on multiple years in filename

--- test/models/SongTagParserTest.scala
+++ test/models/SongTagParserTest.scala
@@ -92,10 +92,8 @@ class SongTagParserTest extends FreeSpec with AuxSpecs with DirectorySpecs {
     "multiple same years succeeds" in {
       SongTagParser.extractYearFromName("blah 5678 blah 5678 blah") shouldReturn Some(5678)
     }
-    "multiple years throws" in {
-      an[Exception] shouldBe thrownBy {
-        SongTagParser.extractYearFromName("blah 5678 blah 1234 blah")
-      }
+    "multiple years returns None" in {
+      SongTagParser.extractYearFromName("blah 5678 blah 1234 blah") shouldReturn None
     }
   }
 }
