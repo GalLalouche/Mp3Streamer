b9958587e39c7d5cbd88718b49e54176c3fbdabe
Gal (Windows) <>
2024-09-18 11:38:25 +0300

    Some typescript experiments

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -2,10 +2,8 @@ String.prototype.takeAfterLast = function(subs) {
   return this.substr(this.lastIndexOf(subs) + 1)
 }
 Boolean.prototype.isFalse = function() {
-  return !this.valueOf();
+  return not(this.valueOf())
 }
-
-const not = b => !b
 
 Number.prototype.timeFormat = function() {
   let hours = Math.floor(this / 3600)
@@ -19,50 +17,6 @@ Number.prototype.timeFormat = function() {
 
   const hourPrefix = hours === "00" ? "" : hours + ":"
   return hourPrefix + minutes + ':' + seconds
-}
-// Copied from https://stackoverflow.com/a/30810322/736508
-// Comments removed for brevity.
-function copyTextToClipboard(text) {
-  const textArea = document.createElement("textarea")
-  textArea.style.position = 'fixed'
-  textArea.style.top = '0'
-  textArea.style.left = '0'
-  textArea.style.width = '2em'
-  textArea.style.height = '2em'
-  textArea.style.padding = '0'
-  textArea.style.border = 'none'
-  textArea.style.outline = 'none'
-  textArea.style.boxShadow = 'none'
-  textArea.style.background = 'transparent'
-  textArea.value = text
-
-  document.body.appendChild(textArea)
-
-  textArea.select()
-
-  // TODO this is deprecated, but actually works right now so ¯\_(ツ)_/¯
-  try {
-    document.execCommand('copy')
-  } catch (ignored) {
-    console.log('Oops, unable to copy')
-  }
-
-  document.body.removeChild(textArea)
-}
-
-function isEmptyObject(obj) {
-  for (const prop in obj)
-    if (Object.prototype.hasOwnProperty.call(obj, prop))
-      return false
-  return true
-}
-
-const _URL_PATTERN = new RegExp('^(https?:\\/\\/)?' + // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-    '(\\:\\d+)?.*') // port and path
-function isValidUrl(urlString) {
-  return _URL_PATTERN.test(urlString)
 }
 
 const ENTER_CODE = 13
@@ -106,14 +60,6 @@ Array.prototype.custom_max = function() {
   return max
 }
 
-// Adding this is a prototype method to Object messes things up for some reason :\
-function map_values(o, f) {
-  let result = {}
-  for (const [key, value] of Object.entries(o))
-    result[key] = f(value)
-  return result
-}
-
 function assert(condition, message) {
   if (condition)
     return
@@ -121,6 +67,7 @@ function assert(condition, message) {
     return assert(condition, "Assertion failed")
   throw typeof Error === "undefined" ? message : new Error(message)
 }
+
 class AssertionError extends Error {
   constructor(message) {
     super(message || "AssertionError")
