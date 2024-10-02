22f1faac7b2e4a25670bf23dad63ab271bb916fa
Gal (Windows) <>
2024-10-02 16:34:08 +0300

    Even more common javascript conversions to typescript

--- public/typescripts/common.ts
+++ public/typescripts/common.ts
@@ -79,3 +79,99 @@ function map_values(o: object, f: (arg0: any) => any): object {
 function not(b: boolean): boolean {
     return !b
 }
+
+type ObjectIndex<K extends string | number | symbol, V> = {
+    [P in K]: V
+};
+
+interface Array<T> {
+    custom_last(): T
+    custom_sort_by<S>(f: (a: T) => S): T[]
+    custom_group_by<S extends string | number | symbol>(f: (a: T) => S): ObjectIndex<S, T[]>
+    custom_max(): T | null
+}
+
+Array.prototype.custom_last = () => this [this.length - 1]
+Array.prototype.custom_sort_by = f =>
+    this
+        .map(e => [e, f(e)])
+        .sort((a, b) => {
+            const fa = a[1]
+            const fb = b[1]
+            if (fa === fb)
+                return 0
+            if (fa < fb)
+                return -1
+            else
+                return 1
+        })
+        .map(e => e[0])
+Array.prototype.custom_group_by = function <S extends string | number | symbol, T>(f: (a: T) => S) {
+    let result: ObjectIndex<S, T[]> = {} as unknown as ObjectIndex<S, T[]>
+    this.forEach((e: T) => {
+        const key = f(e)
+        if (result[key] === undefined)
+            result[key] = []
+        result[key].push(e)
+    })
+    return result
+}
+Array.prototype.custom_max = function () {
+    let max = null
+    for (const e of this) {
+        if (max === null) {
+            max = e
+        } else {
+            max = max > e ? max : e
+        }
+    }
+    return max
+}
+
+function assert(condition: boolean, message?: string) {
+    if (condition)
+        return
+    message = message || "Assertion failed"
+    throw typeof Error === "undefined" ? message : new Error(message)
+}
+
+class AssertionError extends Error {
+    constructor(message?: string) {
+        super(message || "AssertionError")
+    }
+}
+
+interface Number {
+    timeFormat(): string
+    withMinDigits(n: number): string
+}
+
+Number.prototype.withMinDigits = function (n: number): string {
+    return this.toLocaleString('en-US', {minimumIntegerDigits: n})
+}
+Number.prototype.timeFormat = function () {
+    const hours = Math.floor(this / 3600)
+    const minutes = Math.floor((this - (hours * 3600)) / 60)
+    const seconds = Math.round(this - (hours * 3600) - (minutes * 60))
+
+    const hourPrefix = hours == 0 ? "" : hours.withMinDigits(2)
+    return hourPrefix + minutes.withMinDigits(2) + ':' + seconds.withMinDigits(2)
+}
+
+interface String {
+    takeAfterLast(substring: string): string
+}
+
+String.prototype.takeAfterLast = function (subs) {
+    return this.substring(this.lastIndexOf(subs) + 1)
+}
+
+interface Boolean {
+    isFalse(): boolean
+}
+
+Boolean.prototype.isFalse = function () {
+    return not(this.valueOf())
+}
+
+const ENTER_CODE: number = 13
