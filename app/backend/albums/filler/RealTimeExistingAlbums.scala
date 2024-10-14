8289fddcf7179bbaf07de157ecbd40f1854591f2
Gal (Windows) <>
2024-10-14 13:53:03 +0300

    Fallback for artists whose folder doesn't match its name

--- app/backend/albums/filler/RealTimeExistingAlbums.scala
+++ app/backend/albums/filler/RealTimeExistingAlbums.scala
@@ -7,6 +7,8 @@ import models.MusicFinder
 import models.TypeAliases.ArtistName
 
 import common.TimedLogger
+import common.io.DirectoryRef
+import common.rich.RichT.richT
 import common.rich.primitives.RichOption.richOption
 
 private class RealTimeExistingAlbums @Inject() (
@@ -20,7 +22,7 @@ private class RealTimeExistingAlbums @Inject() (
       // "Standard" artists have albums prefixed with release year.
       if (dirs.exists(_.name.take(4).forall(_.isDigit)))
         Vector(reconcilableFactory.toArtist(artistDir))
-      else // Non-standard artists, e.g., DT Sides, don't
+      else // Non-standard artists, e.g., DT Sides, don't.
         dirs.map(dir =>
           Artist(
             mf.parseSong(
@@ -35,8 +37,17 @@ private class RealTimeExistingAlbums @Inject() (
   }
 
   override def albums: Artist => Set[Album] = artist =>
-    getAlbums(artist.normalize).getOrThrow(s"Could not find albums for artist $artist")
+    getAlbums(artist.normalize)
+      .orElse(fallback(artist.normalize))
+      .getOrThrow(s"Could not find albums for artist $artist")
 
   private def getAlbums(artistName: ArtistName): Option[Set[Album]] =
     mf.findArtistDir(artistName).map(_.dirs.map(reconcilableFactory.toAlbum(_).get).toSet)
+
+  private def fallback(artistName: ArtistName): Option[Set[Album]] =
+    mf.albumDirs
+      .filter(mf.getSongsInDir(_).head.artistName == artistName)
+      .toSet
+      .map((e: DirectoryRef) => reconcilableFactory.toAlbum(e).get)
+      .optFilter(_.nonEmpty)
 }
