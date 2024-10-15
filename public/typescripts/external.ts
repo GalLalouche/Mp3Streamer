2bd9969194a604b3e3c81b5f6f347312eae39c4b
Gal (Windows) <>
2024-10-15 23:09:43 +0300

    Typescriptify external
    This also required updating the JSON passed in from the server, since
    timestamp can't be parallel all the hosts.

---
+++ public/typescripts/external.ts
@@ -0,0 +1,171 @@
+import {gplaylist, Song} from "./types.js"
+import {match} from 'ts-pattern'
+import {Poster} from "./poster.js"
+
+export class External {
+  private static _helper: Helper | null
+  private static get helper(): Helper {
+    return this._helper || (this._helper = new Helper())
+  }
+
+  static show(song: Song): void {
+    const externalUrl = remotePath + song.file
+    const self = this
+    $.get(externalUrl, this.helper.showLinks(externalUrl))
+      .fail(function () {
+        self.helper.cleanUp()
+        // FIXME A better error message
+        self.helper.externalDivs.append(span("Error occurred while fetching links"))
+      })
+  }
+}
+
+const remotePath = "external/"
+const HEXA = "[a-f0-9]"
+// E.g., d8f63b51-73e0-4f65-8bd3-bcfe6892fb0e
+const RECON_REGEX = new RegExp(`^(.*/)?${HEXA}{8}-(?:${HEXA}{4}-){3}${HEXA}{12}$`)
+
+function href(target: string, name: string): string {
+  return `<a target=_blank href="${target}">${name}</a>`
+}
+
+class Helper {
+  readonly externalDivs = $(".external")
+  readonly externalAlbum = $("#external-album")
+  readonly externalArtist = $("#external-artist")
+  readonly artistReconBox: JQuery<HTMLElement>
+  readonly albumReconBox: JQuery<HTMLElement>
+  currentPosterRgb: RGB | null = null
+  constructor() {
+    this.artistReconBox =
+      $("<input class='external-recon-id' placeholder='Artist ID' type='text'/>")
+        .appendTo(this.externalArtist)
+    this.externalArtist.appendBr()
+    this.albumReconBox =
+      $("<input class='external-recon-id' placeholder='Album ID' type='text'/>")
+        .appendTo(this.externalAlbum)
+    const self = this
+    this.externalAlbum.appendBr()
+    const updateReconButton = button("Update Recon").appendTo(this.externalDivs)
+    button("Refresh").appendTo(this.externalArtist).click(this.refresh("artist"))
+    button("Refresh").appendTo(this.externalAlbum).click(this.refresh("album"))
+    // Update recon on pressing Enter
+    validateBoxAndButton($(".external-recon-id"), updateReconButton, s => RECON_REGEX.test(s), this.updateRecon)
+    this.externalDivs.on("click", ".copy-to-clipboard", function () {
+      copyTextToClipboard($(this).attr("url")!)
+    })
+
+    // TODO this is a hack to also handle all other fieldsets, probably shouldn't be in this file...
+    Poster.rgbListeners.push(rgb => {
+      this.currentPosterRgb = rgb
+      $("#field-set-group fieldset").each(function () {
+        self.setLinkColor($(this))
+      })
+    })
+  }
+  refresh(target: string): () => void {
+    const self = this
+    return function () {
+      const songPath = gplaylist.currentPlayingSong().file
+      // TODO showLinks should only fetch the links for the target.
+      $.get(`${remotePath}refresh/${target}/${songPath}`, self.showLinks(remotePath + songPath))
+    }
+  }
+  cleanUp() {
+    this.externalDivs.children('ul').remove()
+    this.externalDivs.children('span').remove()
+  }
+  setLinkColor(e: JQuery<HTMLElement>): void {
+    if (!this.currentPosterRgb)
+      return
+    const c1 = this.currentPosterRgb.makeLighter(0.5).toString()
+    const c2 = this.currentPosterRgb.toString()
+    e.css("background-image", `linear-gradient(to top left, ${c1}, ${c2})`)
+  }
+  // Yey, currying!
+  showLinks(debugLink: string): ((r: ExternalResult) => void) {
+    this.cleanUp()
+    this.externalDivs.prepend(span("Fetching links..."))
+    const self = this
+
+    function externalLinks(result: ExternalResult) {
+      self.cleanUp()
+      self.artistReconBox.val("")
+      self.albumReconBox.val("")
+      Object.entries(result).forEach(([entityName, externalLinksForEntity]) => {
+        const ul = elem('ul', {'class': 'external-links'})
+        const finalLine: string =
+          match(externalLinksForEntity)
+            .returnType<string>()
+            .when(isLinks, l => {
+              $.each(l.links, (linkName, link) => {
+                const extensions = getExtensions(link)
+                const links = href(link.main, link.host) + (extensions ? ` (${extensions})` : "")
+                const imageIcon = `"list-style-image: url('assets/images/${link.host.replace(/[*?].*$/g, "")}_icon.png')"`
+                ul.append($(`<li style=${imageIcon}>${links}</li>`))
+              })
+              return l.timestamp
+            })
+            .when(isError, e => href(debugLink, e.error))
+            .exhaustive()
+        const fieldset = $(`#external-${entityName.split(" ")[0].toLowerCase()}`)
+        // TODO this shouldn't really be created every time
+        fieldset.prepend(ul)
+        fieldset.children('legend').remove()
+        fieldset.prepend($(`<legend>${entityName} (${finalLine})</legend>`))
+        self.setLinkColor(fieldset)
+      })
+    }
+
+    return externalLinks
+  }
+  updateRecon(): void {
+    const json: Record<string, string> = {}
+
+    function addIfNotEmpty(elem: JQuery<HTMLElement>) {
+      const id = (elem[0] as any).placeholder.split(" ")[0].toLowerCase()
+      const text = (elem.val() as string).takeAfterLast("/")
+      if (text.length !== 0) {
+        assert(RECON_REGEX.test(text))
+        json[id] = text
+      }
+    }
+
+    addIfNotEmpty(this.artistReconBox)
+    addIfNotEmpty(this.albumReconBox)
+    if (!isEmptyObject(json)) {
+      const songPath = gplaylist.currentPlayingSong().file
+      postJson(remotePath + "recons/" + songPath, json, this.showLinks(remotePath + songPath))
+    }
+  }
+}
+
+interface Links {
+  timestamp: string
+  links: Record<string, Link>
+}
+
+interface ExternalError {
+  readonly error: string
+}
+
+type Result = Links | ExternalError
+
+function isError(r: Result): r is ExternalError {return 'error' in r}
+
+function isLinks(r: Result): r is Links {return 'timestamp' in r}
+
+type ExternalResult = Record<string, Result>
+
+interface Link {
+  extensions: Record<string, string>
+  main: string
+  host: string
+}
+
+function getExtensions(link: Link): string {
+  const $ = Object.keys(link.extensions).map(k => href(link.extensions[k], k)).join(", ")
+  return $ + (link.host.includes("*") ?
+    `${$ === '' ? '' : ', '}<a class='copy-to-clipboard' href='javascript:void(0)' url='${link.main}'>copy</a>` :
+    "")
+}
