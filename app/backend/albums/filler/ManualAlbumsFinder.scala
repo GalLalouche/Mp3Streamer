c36078b0fc8888c4d9dd11b2873650423a78d165
Gal (Windows) <>
2024-12-08 11:33:48 +0200

    Extract ManualAlbumsFinder actor

---
+++ app/backend/albums/filler/ManualAlbumsFinder.scala
@@ -0,0 +1,35 @@
+package backend.albums.filler
+
+import javax.inject.{Inject, Singleton}
+
+import backend.recon.{Album, ReconcilableFactory}
+import models.MusicFinder
+import models.TypeAliases.ArtistName
+
+import scala.concurrent.Future
+
+import common.TimedLogger
+import common.concurrency.SimpleTypedActor
+import common.io.DirectoryRef
+import common.rich.RichT.richT
+
+@Singleton private class ManualAlbumsFinder @Inject() (
+    timed: TimedLogger,
+    mf: MusicFinder,
+    reconcilableFactory: ReconcilableFactory,
+) extends SimpleTypedActor[ArtistName, Option[Set[Album]]] {
+  override def !(m: => ArtistName): Future[Option[Set[Album]]] = delegate ! m
+
+  private val delegate = SimpleTypedActor("ManualAlbumsFinder", fallback)
+
+  private def fallback(artistName: ArtistName): Option[Set[Album]] = timed(
+    s"Cannot find directory for <$artistName>, falling back to manual album search",
+    scribe.warn(_),
+  ) {
+    mf.albumDirs
+      .filter(e => mf.getSongsInDir(e).head.artistName.toLowerCase == artistName)
+      .toSet
+      .map((e: DirectoryRef) => reconcilableFactory.toAlbum(e).get)
+      .optFilter(_.nonEmpty)
+  }
+}
--- app/backend/albums/filler/RealTimeExistingAlbums.scala
+++ app/backend/albums/filler/RealTimeExistingAlbums.scala
@@ -6,16 +6,20 @@ import backend.recon.{Album, Artist, ReconcilableFactory}
 import models.MusicFinder
 import models.TypeAliases.ArtistName
 
+import scala.concurrent.ExecutionContext
+
 import common.TimedLogger
-import common.io.DirectoryRef
-import common.rich.RichT.richT
+import common.rich.RichFuture.richFuture
 import common.rich.primitives.RichOption.richOption
 
 private class RealTimeExistingAlbums @Inject() (
     reconcilableFactory: ReconcilableFactory,
     mf: MusicFinder,
     timed: TimedLogger,
+    manualAlbumsFinder: ManualAlbumsFinder,
+    ec: ExecutionContext,
 ) extends ExistingAlbums {
+  private implicit val iec: ExecutionContext = ec
   override def artists: Iterable[Artist] = timed("Fetching artists (lazy)", scribe.info(_)) {
     reconcilableFactory.artistDirectories.flatMap { artistDir =>
       val dirs = artistDir.dirs
@@ -38,21 +42,9 @@ private class RealTimeExistingAlbums @Inject() (
 
   override def albums: Artist => Set[Album] = artist =>
     getAlbums(artist.normalize)
-      .orElse(fallback(artist.normalize))
+      .orElse(manualAlbumsFinder.!(artist.normalize).get)
       .getOrThrow(s"Could not find albums for artist $artist")
 
   private def getAlbums(artistName: ArtistName): Option[Set[Album]] =
     mf.findArtistDir(artistName).map(_.dirs.map(reconcilableFactory.toAlbum(_).get).toSet)
-
-  // TODO This should use an actor, to avoid thread hogging.
-  private def fallback(artistName: ArtistName): Option[Set[Album]] = timed(
-    s"Cannot find directory for <$artistName>, falling back to manual album search",
-    scribe.warn(_),
-  ) {
-    mf.albumDirs
-      .filter(e => mf.getSongsInDir(e).head.artistName.toLowerCase == artistName)
-      .toSet
-      .map((e: DirectoryRef) => reconcilableFactory.toAlbum(e).get)
-      .optFilter(_.nonEmpty)
-  }
 }
