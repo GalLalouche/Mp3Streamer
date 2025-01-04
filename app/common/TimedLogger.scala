bdf4ed1596fbefb9039c33f6fe49b62576d16c08
Gal (Windows) <>
2025-01-04 21:08:10 +0200

    TimedLogger: log timing of failures

--- app/common/TimedLogger.scala
+++ app/common/TimedLogger.scala
@@ -5,9 +5,15 @@ import javax.inject.Inject
 class TimedLogger @Inject() {
   def apply[T](task: String, logger: String => Unit = scribe.trace(_))(f: => T): T = {
     val start = System.currentTimeMillis
-    logger(s"starts $task")
-    val $ = f
-    logger(s"$task took ${System.currentTimeMillis - start} ms")
-    $
+    try {
+      logger(s"starts $task")
+      val $ = f
+      logger(s"$task took ${System.currentTimeMillis - start} ms")
+      $
+    } catch {
+      case e: Throwable =>
+        logger(s"$task FAILED after ${System.currentTimeMillis - start} ms")
+        throw e
+    }
   }
 }
