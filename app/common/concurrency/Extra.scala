7cf9071288647a2fcdc6658e0a9b72fbaeb29ad7
Gal (Windows) <>
2025-01-20 15:16:56 +0200

    Extras now print stacktrace on errors

--- app/common/concurrency/Extra.scala
+++ app/common/concurrency/Extra.scala
@@ -2,14 +2,22 @@ package common.concurrency
 
 import scala.concurrent.Future
 
+import common.rich.func.BetterFutureInstances._
+import common.rich.func.ToMoreMonadErrorOps.toMoreMonadErrorOps
+
 /**
  * An actor with neither input nor output, i.e., an object you can tell "go do that thing you do".
  */
 sealed trait Extra extends SimpleActor[Unit] {
-  final def !(): Future[Unit] = this.!(())
+  def !(): Future[Unit]
 }
 
 object Extra {
   def apply(name: String)(f: => Any): Extra =
-    new SimpleTypedActorImpl[Unit, Unit](name, _ => f) with Extra
+    new SimpleTypedActorImpl[Unit, Unit](name, _ => f) with Extra {
+      final override def !(): Future[Unit] = this
+        .!(())
+        // Since it's possible no one will use the Extra output, it's important to log this!
+        .listenError(scribe.error(s"Extra <$name> failed", _))
+    }
 }
--- app/common/concurrency/SimpleTypedActorImpl.scala
+++ app/common/concurrency/SimpleTypedActorImpl.scala
@@ -8,7 +8,7 @@ import scalaz.syntax.functor.ToFunctorOps
 /** It's a single threaded future factory basically. */
 private class SimpleTypedActorImpl[Msg, +Result](name: String, f: Msg => Result)
     extends SimpleTypedActor[Msg, Result] {
-  private implicit val ec: ExecutionContext = SingleThreadedJobQueue.executionContext(name)
+  protected implicit val ec: ExecutionContext = SingleThreadedJobQueue.executionContext(name)
   def !(m: => Msg): Future[Result] = Future(f(m))
 
   def void: SimpleActor[Msg] = SimpleTypedActorImpl.this.!(_).void
