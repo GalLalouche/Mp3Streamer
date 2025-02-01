04983d5a5e1b2432916e508b98f8aa5ef7dfffad
Gal (Windows) <>
2025-02-01 20:09:46 +0200

    Added Album test

--- test/backend/recon/ReconcilablesTest.scala
+++ test/backend/recon/ReconcilablesTest.scala
@@ -15,6 +15,16 @@ class ReconcilablesTest extends FreeSpec with AuxSpecs {
       a1.toString shouldNot be(a2.toString)
     }
   }
+  "Album" - {
+    "Comparison is based on name" in {
+      val a1 = Album("MOoO", 2003, Artist("FooOBAr"))
+      val a2 = Album("MoOO", 2003, Artist("foOoBAr"))
+      a1 shouldReturn a2
+      a1.hashCode shouldReturn a2.hashCode
+      a1.toString shouldNot be(a2.toString)
+    }
+  }
+}
 
 object ReconcilablesTest {
   implicit val arbitraryArtist: Arbitrary[Artist] = Arbitrary(Gen.alphaNumStr.map(Artist))
