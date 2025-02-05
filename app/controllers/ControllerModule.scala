b1dd6767be81f0ed5e52e6cf164fa08d93e36f0c
Gal (Windows) <>
2025-02-05 17:05:37 +0200

    Inject Poster (and rename to PosterLookup)

--- app/controllers/ControllerModule.scala
+++ app/controllers/ControllerModule.scala
@@ -1,11 +1,14 @@
 package controllers
 
 import com.google.inject.Provides
+import models.PosterLookup
 import net.codingwell.scalaguice.ScalaModule
 
 object ControllerModule extends ScalaModule {
-  override def configure(): Unit =
+  override def configure(): Unit = {
     bind[UrlPathUtils].toInstance(PlayUrlPathUtils)
+    bind[PosterLookup].toInstance(PosterLookup.IOPosterLookup)
+  }
 
   @Provides private def provideUrlDecoder(u: UrlPathUtils): UrlDecodeUtils = u
 }
---
+++ app/models/PosterLookup.scala
@@ -0,0 +1,21 @@
+package models
+
+import java.util.regex.Pattern
+
+import common.io.{DirectoryRef, FileRef}
+import common.rich.primitives.RichString._
+
+trait PosterLookup {
+  def getCoverArt(s: Song): FileRef
+
+}
+object PosterLookup {
+  object IOPosterLookup extends PosterLookup {
+    private val FolderImagePattern =
+      Pattern.compile("folder\\.(jpg)|(png)", Pattern.CASE_INSENSITIVE)
+    /** Searches for a folder image recursively up the folder tree. */
+    private def getCoverArt(dir: DirectoryRef): FileRef =
+      dir.files.find(_.name.matches(FolderImagePattern)).getOrElse(getCoverArt(dir.parent))
+    def getCoverArt(s: Song): FileRef = getCoverArt(s.asInstanceOf[IOSong].file.parent)
+  }
+}
