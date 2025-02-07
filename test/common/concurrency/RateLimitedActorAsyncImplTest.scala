f2a5e63137e20e899ed7114410365436b6e1b2ca
Gal (Windows) <>
2025-02-07 14:01:54 +0200

    Fix RateLimitedActorAsyncImpl
    Honestly, mostly fix the test since it's been flakey for years! Issue
    was it actually measured the time between the last request *starting*
    instead of finishing.

--- test/common/concurrency/RateLimitedActorAsyncImplTest.scala
+++ test/common/concurrency/RateLimitedActorAsyncImplTest.scala
@@ -20,15 +20,17 @@ import common.test.AsyncAuxSpecs
 class RateLimitedActorAsyncImplTest extends AsyncFreeSpec with AsyncAuxSpecs with Matchers {
   "rate limited" in {
     val queue = new LinkedBlockingQueue[(Int, Long)]()
+    val limit = 10L
     val $ = SimpleTypedActor.asyncRateLimited[Int, Unit](
       "name",
       i => Future.successful(queue.put(i, System.currentTimeMillis())),
-      10.millis,
+      limit.millis,
     )
-    1.to(5).toVector.traverse($ ! _) >| {
+    val vector = 1.to(5).toVector
+    vector.traverse($ ! _) >| {
       val v = queue.asScala.toVector
-      v.map(_._1) shouldReturn 1.to(5).toVector
-      all(v.map(_._2).pairSliding.map(_.swap.fold(_ - _)).toVector) should be >= 10L
+      v.map(_._1) shouldReturn vector
+      all(v.map(_._2).pairSliding.map(_.swap.fold(_ - _)).toVector) should be >= limit
     }
   }
 }
