197d7263fecef1a8a2de31632300d6412bb13992
Gal (Windows) <>
2025-03-05 22:26:50 +0200

    BrowserUtils: Replace Chrome with Firefox
    For some reason, Chrome decided to apparently require admin access to be
    run during one of its updates I guess? So I'm switching to Firefox for
    now.

--- app/mains/BrowserUtils.scala
+++ app/mains/BrowserUtils.scala
@@ -7,10 +7,9 @@ import scala.sys.process.Process
 import common.Urls
 
 private object BrowserUtils {
-  def pointBrowserTo(query: Url): Unit =
-    Process(
-      """C:\Program Files\Google\Chrome\Application\chrome.exe """ + query.toStringPunycode,
-    ).!!
+  def pointBrowserTo(query: Url): Unit = Process(
+    """"C:\Program Files\Mozilla Firefox\firefox.exe" -new-tab """ + query.toStringPunycode,
+  ).!!
   def searchFor(query: String): Unit = pointBrowserTo(Urls.googleSearch(query))
   def searchForLucky(query: String): Unit = pointBrowserTo(Urls.searchForLucky(query))
 }
