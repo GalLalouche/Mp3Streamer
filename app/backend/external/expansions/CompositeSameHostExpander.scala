6aa0e4180b0eb09cf93a297c5edc4b498cb807a0
Gal (Windows) <>
2025-03-09 15:07:41 +0200

    Disable AllMusic discography loading.
    GitHub: Relates to #145

--- app/backend/external/expansions/CompositeSameHostExpander.scala
+++ app/backend/external/expansions/CompositeSameHostExpander.scala
@@ -12,12 +12,14 @@ import common.rich.collections.RichTraversableOnce._
 /** E.g., from an artist's wikipedia page, to that artists' wikipedia pages of her albums. */
 private[external] class CompositeSameHostExpander @Inject() (
     wiki: WikipediaAlbumFinder,
-    am: AllMusicAlbumFinder,
+    // This is turned off since the discography loads dynamically and this is not something the
+    // InternetTalker currently supports. See https://github.com/GalLalouche/Mp3Streamer/issues/145.
+    // am: AllMusicAlbumFinder,
     ec: ExecutionContext,
 ) {
   private implicit val iec: ExecutionContext = ec
 
-  private val expanders: Map[Host, SameHostExpander] = Iterator(wiki, am).mapBy(_.host)
+  private val expanders: Map[Host, SameHostExpander] = Iterator(wiki).mapBy(_.host)
   def toReconcilers(ls: BaseLinks[Artist]): LinkRetrievers[Album] = {
     val availableHosts = ls.toMultiMap(_.host).mapValues(_.head)
     LinkRetrievers(expanders.flatMap(e => availableHosts.get(e._1).map(e._2.toReconciler)))
