19321737a22cd1a14fadcb716758485cafb6d7d6
Gal (Windows) <>
2025-04-10 17:09:27 +0300

    Slightly more efficient unique actor implementation
    Avoid multiple map accesses by using compute.

--- app/common/concurrency/SimpleTypedActorImpl.scala
+++ app/common/concurrency/SimpleTypedActorImpl.scala
@@ -8,7 +8,7 @@ import scalaz.syntax.functor.ToFunctorOps
 private class SimpleTypedActorImpl[Msg, +Result](name: String, f: Msg => Result)
     extends SimpleTypedActor[Msg, Result] {
   protected implicit val ec: ExecutionContext = SingleThreadedJobQueue.executionContext(name)
-  def !(m: => Msg): Future[Result] = Future(f(m))
+  override def !(m: => Msg): Future[Result] = Future(f(m))
 
   def void: SimpleActor[Msg] = SimpleTypedActorImpl.this.!(_).void
 }
--- app/common/concurrency/UniqueSimpleTypedActorImpl.scala
+++ app/common/concurrency/UniqueSimpleTypedActorImpl.scala
@@ -8,7 +8,6 @@ import common.rich.func.BetterFutureInstances._
 import common.rich.func.ToMoreFunctorOps._
 
 import common.rich.RichT._
-import common.rich.collections.RichMap._
 
 private class UniqueSimpleTypedActorImpl[Msg, Result](
     name: String,
@@ -17,11 +16,18 @@ private class UniqueSimpleTypedActorImpl[Msg, Result](
   private val messages: util.Map[Msg, Future[Result]] = new util.HashMap()
   private implicit val service: ExecutionContext = SingleThreadedJobQueue.executionContext(name)
   private def clear(m: Msg): Unit = synchronized(messages.remove(m).ensuring(_ != null))
-  def !(m: => Msg): Future[Result] = synchronized {
+  override def !(m: => Msg): Future[Result] = synchronized {
     lazy val msg = m
-    if (messages.containsKey(msg))
-      scribe.trace(s"Ignoring non-unique msg <$msg>")
-    // TODO clear in cases of failures too
-    messages.getOrPutIfAbsent(msg, Future(f(msg)).listen(clear(msg).const))
+    messages.compute(
+      msg,
+      (_, result) =>
+        if (result == null)
+          // TODO clear in cases of failures too
+          Future(f(msg)).listen(clear(msg).const)
+        else {
+          scribe.trace(s"Ignoring non-unique msg <$msg>")
+          result
+        },
+    )
   }
 }
