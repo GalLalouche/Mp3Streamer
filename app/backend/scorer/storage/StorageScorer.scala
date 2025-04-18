a525306417b194a50e84eb5cf83fff7546a6f56f
Gal (Windows) <>
2025-04-18 21:11:40 +0300

    StorageScorer: Add signatures

--- app/backend/scorer/storage/StorageScorer.scala
+++ app/backend/scorer/storage/StorageScorer.scala
@@ -2,17 +2,18 @@ package backend.scorer.storage
 
 import backend.scorer.{ModelScore, OptionalModelScore}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 import common.rich.func.BetterFutureInstances._
+import scalaz.OptionT
 import scalaz.Scalaz.ToFunctorOps
 
 import common.storage.StorageTemplate
 
 private[scorer] trait StorageScorer[A] { self: StorageTemplate[A, ModelScore] =>
   protected implicit def ec: ExecutionContext
-  final def apply(a: A) = load(a)
-  final def updateScore(a: A, score: OptionalModelScore) = (score.toModelScore match {
+  final def apply(a: A): OptionT[Future, ModelScore] = load(a)
+  final def updateScore(a: A, score: OptionalModelScore): Future[Unit] = (score.toModelScore match {
     case None => delete(a)
     case Some(s) => replace(a, s)
   }).run.void
