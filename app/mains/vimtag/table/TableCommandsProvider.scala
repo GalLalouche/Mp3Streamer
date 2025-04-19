08d105b9282ce6ca7102ee9106b926b7a6d3554b
Gal (Windows) <>
2025-04-19 14:34:17 +0300

    Extract VimLauncher

--- app/mains/vimtag/table/TableCommandsProvider.scala
+++ app/mains/vimtag/table/TableCommandsProvider.scala
@@ -8,6 +8,6 @@ private object TableCommandsProvider extends CommandsProvider {
   override val get = Vector(
     NormalCommand("G"), // Go to the end of the file
     ExecutionCommand("TableModeRealign"), // The realign has to invoked at the end for some reason
-    ExecutionCommand("silent! TableModeEnable"), // If not silent, it'll just echo it annoyingly
+    ExecutionCommand("silent!TableModeEnable"), // If not silent, it'll just echo it annoyingly
   )
 }
