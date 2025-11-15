1d44dd0178b5fcd139b61b694ad9f3f3d20d2214
Gal (Windows) <>
2025-11-15 14:26:16 +0200

    Small refactors

--- app/backend/mb/AlbumReconciler.scala
+++ app/backend/mb/AlbumReconciler.scala
@@ -7,10 +7,12 @@ import play.api.libs.json.{JsObject, JsValue}
 
 import scala.concurrent.ExecutionContext
 
-import cats.implicits.{toFoldableOps, toFunctorOps}
+import cats.syntax.functor.toFunctorOps
+import common.rich.func.kats.IteratorInstances.iteratorInstances
+import common.rich.func.kats.ToMoreUnorderedFoldableOps.toMoreUnorderedFoldableOps
 
 import common.json.RichJson._
-import common.rich.RichT._
+import common.rich.RichT.richT
 
 private class AlbumReconciler @Inject() (
     ec: ExecutionContext,
@@ -30,6 +32,7 @@ private class AlbumReconciler @Inject() (
     Album(title = js.str("title"), year = js.str("first-release-date").take(4).toInt, artist = a)
   private def parse(js: JsValue, a: Album): Option[ReconID] = js
     .objects("release-groups")
+    .iterator
     .filter(_.has("first-release-date"))
     .filter(_.ostr("primary-type").exists(Set("Album", "EP")))
     // TODO topByFilter?
