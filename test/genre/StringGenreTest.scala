15bfeb784da54df364efa06dcc245f427cf46067
Gal (Windows) <>
2025-11-20 13:42:27 +0200

    TODOs: StringGenreTest.forArtist

--- test/genre/StringGenreTest.scala
+++ test/genre/StringGenreTest.scala
@@ -1,11 +1,13 @@
 package genre
 
 import backend.module.{FakeMusicFinder, TestModuleConfiguration}
+import backend.recon.Artist
 import com.google.inject.Guice
-import common.io.MemoryRoot
-import common.test.AuxSpecs
 import net.codingwell.scalaguice.InjectorExtensions.ScalaInjector
 import org.scalatest.freespec.AnyFreeSpec
+
+import common.io.MemoryRoot
+import common.test.AuxSpecs
 
 class StringGenreTest extends AnyFreeSpec with AuxSpecs {
   private val root = new MemoryRoot
@@ -18,17 +20,17 @@ class StringGenreTest extends AnyFreeSpec with AuxSpecs {
   private val $ =
     Guice.createInjector(TestModuleConfiguration(_mf = mf).module).instance[StringGenreFinder]
 
-  // TODO test forArtist
+  private val baseDir = root.getDir("a").get
+  private val d = root.getDir("d").get
+  private val d2 = d.addSubDir("d2")
+  private val c = baseDir.addSubDir("b").addSubDir("c")
   "forDir" - {
-    val a = root.getDir("a").get
-    val d = root.getDir("d").get
-    "flat genre" in {
-      val d2 = d.addSubDir("d2")
-      $.forDir(d2) shouldReturn StringGenre.Flat("d")
-    }
-    "nested genre" in {
-      val c = a.addSubDir("b").addSubDir("c")
-      $.forDir(c) shouldReturn StringGenre.Nested("a", "b")
-    }
+    "flat genre" in { $.forDir(d2) shouldReturn StringGenre.Flat("d") }
+    "nested genre" in { $.forDir(c) shouldReturn StringGenre.Nested("a", "b") }
+  }
+  "forArtist" - {
+    "flat genre" in { $.forArtist(Artist("d2")).get shouldReturn StringGenre.Flat("d") }
+    "nested genre" in { $.forArtist(Artist("c")).get shouldReturn StringGenre.Nested("a", "b") }
+    "not found" in { $.forArtist(Artist("foobar")) shouldBe empty }
   }
 }
