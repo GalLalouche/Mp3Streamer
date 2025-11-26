d295cef8d2f15e3ef867b772e5fa5d3cc829e33c
Gal (Windows) <>
2025-11-27 01:41:18 +0200

    ActorState

--- app/backend/search/SearchState.scala
+++ app/backend/search/SearchState.scala
@@ -5,6 +5,8 @@ import models.{AlbumDir, ArtistDir, Song}
 
 import scala.concurrent.{ExecutionContext, Future}
 
+import cats.implicits.toFunctorOps
+
 import common.concurrency.{UpdatableProxy, UpdatableProxyFactory}
 
 @Singleton
@@ -16,8 +18,8 @@ private class SearchState @Inject() (
   private implicit val iec: ExecutionContext = ec
   private val updater: Future[UpdatableProxy[CompositeIndex]] =
     proxyFactory.initialize(() => index.create())
-  def update(): Future[Unit] = updater.flatMap(_.update())
+  def update(): Future[Unit] = updater.flatMap(_.update()).void
 
   def search(terms: Seq[String]): Future[(Seq[Song], Seq[AlbumDir], Seq[ArtistDir])] =
-    updater.map(_.current.search(terms))
+    updater.map(_.get.search(terms))
 }
--- app/common/json/saver/JsonableCOWFactory.scala
+++ app/common/json/saver/JsonableCOWFactory.scala
@@ -3,17 +3,32 @@ package common.json.saver
 import alleycats.Zero
 import com.google.inject.Inject
 
+import common.concurrency.ActorState
 import common.json.Jsonable
 
+/**
+ * {{{
+ *  ___________________________
+ * < Writes to JSON on changes >
+ *  ---------------------------
+ *         \   ^__^
+ *          \  (oo)\_______
+ *             (__)\       )\/\
+ *                 ||----w |
+ *                 ||     ||
+ * }}}
+ *
+ * On [[set]], saves itself using `JsonableSaver`. On initial [[get]], returns the loaded value.
+ */
 class JsonableCOWFactory @Inject() (saver: JsonableSaver) {
   def apply[Input, Internal: JsonableSaveable: Manifest, Output](
       i2i: Input => Internal,
       i2o: Internal => Output,
-  ): JsonableCOW[Input, Output] = new JsonableCOWImpl[Input, Internal, Output](saver, i2i, i2o)
+  ): ActorState[Input, Output] = new JsonableCOWImpl[Input, Internal, Output](saver, i2i, i2o)
   /**
    * A very simplified overload of the above, when there is intermediate internal type, and there is
    * no need for special [[JsonableSaver]] loading and saving logic.
    */
-  def apply[A <: AnyRef: Manifest: Jsonable: Zero]: JsonableCOW[A, A] =
+  def apply[A <: AnyRef: Manifest: Jsonable: Zero]: ActorState[A, A] =
     new JsonableCOWImpl[A, A, A](saver, identity, identity)
 }
