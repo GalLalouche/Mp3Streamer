87bd0a52bf750e7d28f638fe3dd4b29735b263f1
Gal (Windows) <>
2025-11-29 20:09:50 +0200

    TODOs: Remove "Add to ScalaGuice", since it's no longer maintained
    Also, use ModuleUtils for factory injection.

--- app/mains/cover/CoverModule.scala
+++ app/mains/cover/CoverModule.scala
@@ -1,14 +1,13 @@
 package mains.cover
 
-import com.google.inject.assistedinject.FactoryModuleBuilder
 import net.codingwell.scalaguice.ScalaModule
 
+import common.guice.ModuleUtils
 import common.io.google.GoogleModule
 
-private[mains] object CoverModule extends ScalaModule {
+private[mains] object CoverModule extends ScalaModule with ModuleUtils {
   override def configure(): Unit = {
     install(GoogleModule)
-    // TODO add to scala guice.
-    install(new FactoryModuleBuilder().build(classOf[AsyncFolderImagePanelFactory]))
+    install[AsyncFolderImagePanelFactory]
   }
 }
