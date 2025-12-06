50ca66b344924841b8b053b8a12184580b3578aa
Gal (Windows) <>
2025-12-06 17:47:30 +0200

    Move MemoryRef to test package

--- app/backend/module/NonPersistentModule.scala
+++ app/backend/module/NonPersistentModule.scala
@@ -9,12 +9,13 @@ import slick.jdbc.{H2Profile, JdbcProfile}
 
 import scala.concurrent.ExecutionContext
 
-import common.io.{DirectoryRef, MemoryRoot, RootDirectory}
+import common.io.{DirectoryRef, IODirectory, RootDirectory}
+import common.rich.path.TempDirectory
 
 object NonPersistentModule extends ScalaModule {
   override def configure(): Unit = {
     bind[Clock].toInstance(Clock.systemDefaultZone)
-    bind[DirectoryRef].annotatedWith[RootDirectory].toInstance(new MemoryRoot)
+    bind[DirectoryRef].annotatedWith[RootDirectory].toInstance(IODirectory(TempDirectory()))
     bind[DbProvider].toInstance(new DbProvider {
       override lazy val profile: JdbcProfile = H2Profile
       override lazy val db: profile.backend.DatabaseDef =
