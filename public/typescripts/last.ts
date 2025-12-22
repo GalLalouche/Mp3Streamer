69c1a1618728719b32cf16dcf6e4120c23e8dc0f
Gal (Windows) <>
2025-12-22 17:37:42 +0200

    Faster dequeue last album if the last response wasn't empty

--- public/typescripts/last.ts
+++ public/typescripts/last.ts
@@ -4,8 +4,13 @@ import {Album, gplaylist} from "./types.js"
 
 export namespace LastAlbum {
   export async function addNextNewAlbum(): Promise<void> {
-    await updateLatestAlbum()
-    return dequeue()
+    if (nonEmptyQueue) { // If the queue isn't empty, dequeue first for faster update time.
+      await dequeue()
+      await updateLatestAlbum()
+    } else {
+      await updateLatestAlbum()
+      await dequeue()
+    }
   }
 
   export async function updateLatestAlbum(): Promise<void> {
@@ -35,12 +40,15 @@ function write(text: string): JQuery<HTMLElement> {
   return $("#last_album").empty().append(span(text))
 }
 
+let nonEmptyQueue: boolean = false
+
 function updateAlbums(albums: Album[]): void {
   if (albums.length == 0) {
     write("No new albums")
     return
   }
   const firstAlbum = albums[0]
+  nonEmptyQueue = true
   const extra = albums.length > 1 ? ` (+ ${albums.length - 1} more)` : ""
   const formatAlbum = (album: Album) => `${album.artistName}: ${album.title}`
   const text = `${icon(ADD)} ${formatAlbum(firstAlbum)}${extra}`
@@ -48,15 +56,16 @@ function updateAlbums(albums: Album[]): void {
   const albumElement = write(text)
   albumElement.find(".fa-" + ADD).on("click", (() => dequeue()))
   if (albumElement.custom_overflown() || extra.length > 0) {
-    var formatted = albums.map(formatAlbum)
+    const formatted = albums.map(formatAlbum)
     const extraOverflowText = albums.length > 1 ? ` (+ ${formatted.slice(1).join(", ")})` : ""
     albumElement.custom_tooltip(`${formatted[0]}${extraOverflowText}`)
   }
 }
 
 async function dequeue(): Promise<void> {
-  const [head, _] = await $.post(prefix + "/dequeue").toPromise()
+  const [head, tail] = await $.post(prefix + "/dequeue").toPromise()
   await $.get("data/album/" + head.dir, e => gplaylist.add(e, false)).toPromise()
+  nonEmptyQueue = tail.length > 0
   return LastAlbum.updateLatestAlbum()
 }
 
