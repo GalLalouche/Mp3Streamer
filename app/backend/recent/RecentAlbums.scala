cd5f5238da4bd6f60cde13680598b3a51d53a266
Gal (Windows) <>
2026-02-10 11:20:27 +0200

    RecentAlbums optimization (sort before filter)

--- app/backend/recent/RecentAlbums.scala
+++ app/backend/recent/RecentAlbums.scala
@@ -5,13 +5,12 @@ import java.time.{Clock, LocalDate, LocalDateTime}
 import com.google.inject.Inject
 import models.{AlbumDir, AlbumDirFactory, SongTagParser}
 import musicfinder.{MusicFiles, SongFileFinder}
-import rx.lang.scala.Observable
 
+import scala.collection.mutable
 import scala.math.Ordering.Implicits.infixOrderingOps
 
 import cats.syntax.apply.catsSyntaxApplyOps
 
-import common.TopKBuilder
 import common.path.ref.{DirectoryRef, FileRef}
 import common.rich.RichT._
 import common.rich.RichTime
@@ -26,8 +25,8 @@ private class RecentAlbums @Inject() (
     albumFactory: AlbumDirFactory,
     clock: Clock,
 ) extends LastAlbumProvider {
-  def all(amount: Int): Seq[AlbumDir] = go(amount, mf.albumDirs)
-  def double(amount: Int): Seq[AlbumDir] = go(amount, mf.albumDirs.filter(isDoubleAlbum))
+  def all(amount: Int): Seq[AlbumDir] = sortedDirs().take(amount).toVector
+  def double(amount: Int): Seq[AlbumDir] = sortedDirs().filter(isDoubleAlbum).take(amount).toVector
   override def since(lastDuration: LocalDateTime): Seq[AlbumDir] =
     mf.albumDirsWithAttributes
       .filter(_._2.lastModifiedTime.toLocalDateTime >= lastDuration)
@@ -38,14 +37,18 @@ private class RecentAlbums @Inject() (
   def sinceMonths(m: Int): Seq[AlbumDir] = since(_.minusMonths(m))
   private def since(f: LocalDate => LocalDate): Seq[AlbumDir] =
     since(f(clock.getLocalDate).atStartOfDay)
-  private def isDoubleAlbum(dir: DirectoryRef): Boolean = {
-    val songs = finder.getSongFilesInDir(dir).sortBy(_.name)
+  private def isDoubleAlbum(albumDir: AlbumDir): Boolean = {
+    val songs = finder.getSongFilesInDir(albumDir.dir).sortBy(_.name)
     def discNumber(s: FileRef) = songTagParser(s).discNumber
     discNumber(songs.head).map2(discNumber(songs.last))(_ != _).getOrElse(false)
   }
-  private def go(amount: Int, dirs: Observable[DirectoryRef]) =
-    dirs
-      .buildBlocking(TopKBuilder[DirectoryRef](amount)(Ordering.by(_.lastModifiedTime)))
+  private def sortedDirs(): Iterator[AlbumDir] =
+    mf.albumDirsWithAttributes
+      .buildBlocking(mutable.ArrayBuilder.make)
+      .sortInPlaceBy(_._2.lastModifiedTime.toLocalDateTime)
+      .map(_._1)
+      .iterator
+      // TODO this could be sped up even further with a heap, since it's O(n) for building!
       .map(makeAlbum)
   // recent doesn't care about songs.
   private def makeAlbum(dir: DirectoryRef) = albumFactory.fromDirWithoutSongs(dir)
