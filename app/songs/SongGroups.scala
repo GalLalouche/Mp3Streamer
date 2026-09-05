75d6bf634060ac04edf8254a4dc4181975cc3614
Gal (Windows) <>
2026-09-05 22:37:45 +0300

    SongGroups: Inject ModelJsonable

--- app/songs/SongGroups.scala
+++ app/songs/SongGroups.scala
@@ -1,26 +1,25 @@
 package songs
 
 import com.google.inject.Inject
-import models.Song
+import models.{ModelJsonable, Song}
 
 import cats.syntax.functor.toFunctorOps
 
 import common.io.RootDirectory
-import common.json.Jsonable
 import common.json.ToJsonableOps._
 import common.path.ref.DirectoryRef
 import common.rich.RichT._
 
-private class SongGroups @Inject() (@RootDirectory rootDirectory: DirectoryRef) {
+private class SongGroups @Inject() (
+    @RootDirectory rootDirectory: DirectoryRef,
+    mj: ModelJsonable,
+) {
   private lazy val jsonFile = rootDirectory.addFile("song_groups.json")
+  import mj.songJsonifier
 
-  def save(groups: Iterable[SongGroup])(implicit songJsonable: Jsonable[Song]): Unit = groups
-    .map(_.songs.jsonify)
-    .map(_.toString)
-    .mkString("\n") |> jsonFile.write
-  def load(implicit songJsonable: Jsonable[Song]): Set[SongGroup] = jsonFile.lines
-    .map(_.parseJsonable[Seq[Song]] |> SongGroup.apply)
-    .toSet
+  def save(groups: Iterable[SongGroup]): Unit =
+    groups.map(_.songs.jsonify).map(_.toString).mkString("\n") |> jsonFile.write
+  def load: Set[SongGroup] = jsonFile.lines.map(_.parseJsonable[Seq[Song]] |> SongGroup.apply).toSet
 }
 
 private object SongGroups {
