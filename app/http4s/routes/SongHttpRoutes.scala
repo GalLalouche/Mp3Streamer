929fa81e789ad78a03f592919b93de015ca98ccd
Gal (Windows) <>
2026-09-05 22:29:18 +0300

    Get rid of forced MP3 encoding
    Chrome can handle flac files just fine, and with the change to in-memory
    client-side reading, all bugs seem to have been fixed.

--- app/http4s/routes/SongHttpRoutes.scala
+++ app/http4s/routes/SongHttpRoutes.scala
@@ -1,26 +1,22 @@
 package http4s.routes
 
 import com.google.inject.Inject
-import http4s.routes.Http4sUtils.{decodePath, jsonEncoder, shouldEncodeMp3}
-import org.http4s.{HttpRoutes, Request}
+import http4s.routes.Http4sUtils.{decodePath, jsonEncoder}
+import org.http4s.HttpRoutes
 import org.http4s.dsl.io._
 import songs.SongFormatter
 
-import cats.data.Reader
 import cats.effect.IO
 
 /** Handles fetch requests of JSON information. */
 private class SongHttpRoutes @Inject() ($ : SongFormatter) {
   val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
-    case req @ GET -> Root / "randomSong" / "mp3" => Ok(run(req, $.randomMp3Song()))
-    case req @ GET -> Root / "randomSong" / "flac" => Ok(run(req, $.randomFlacSong()))
-    case req @ GET -> Root / "randomSong" => Ok(run(req, $.randomSong()))
-    case req @ GET -> "album" /: path => Ok(run(req, $.album(decodePath(path))))
-    case req @ GET -> "disc" /: disc /: path => Ok(run(req, $.discNumber(decodePath(path), disc)))
-    case req @ GET -> "song" /: path => Ok(run(req, $.song(decodePath(path))))
-    case req @ GET -> "nextSong" /: path => Ok(run(req, $.nextSong(decodePath(path))))
+    case GET -> Root / "randomSong" / "mp3" => Ok($.randomMp3Song())
+    case GET -> Root / "randomSong" / "flac" => Ok($.randomFlacSong())
+    case GET -> Root / "randomSong" => Ok($.randomSong())
+    case GET -> "album" /: path => Ok($.album(decodePath(path)))
+    case GET -> "disc" /: disc /: path => Ok($.discNumber(decodePath(path), disc))
+    case GET -> "song" /: path => Ok($.song(decodePath(path)))
+    case GET -> "nextSong" /: path => Ok($.nextSong(decodePath(path)))
   }
-
-  private def run[A](req: Request[IO], reader: Reader[Boolean, A]): A =
-    reader.run(shouldEncodeMp3(req))
 }
